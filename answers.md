ac4fde5b6de029d804c92be16cb0ebd18e605752
Jamie Domingo <jaybmw@aol.com>
2018-02-02 09:16:45 -0800

    updated server test

---
+++ answers.md
@@ -0,0 +1,11 @@
+In Mocha, what are the differences between before after beforeEach afterEach? When do they run? What are they used for?
+
+Hooks are used to run data before and after that help with setting up preconditions before a test and cleaning up after. Before/after are run before and after all tests in a blocks while beforeEach/afterEach are run before and after each individual test. 
+
+
+What is the point of Test Driven Development? What do you personally think about this approach?
+
+
+
+
+What is a spy in sinon? How do we use it to effectively test a callback?
--- server.test.js
+++ server.test.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
+const server = require('./server');
+const chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
 
 const Game = require('./models');
 
@@ -25,21 +28,86 @@ describe('Games', () => {
   });
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+  let gameId;
+
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    new Game({
+      title: 'The Legend of Zelda',
+      genre: 'action-adventure',
+      date: 'November 21, 1991'
+    }).save((err, savedGame) => {
+      if (err) {
+        console.log(err);
+        // return done();
+      }
+      gameId = savedGame.id;
+      done();
+    });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, (err) => {
+      done();
+    });
   });
 
   // test the POST here
+  describe('[POST]/api/game/create', () => {
+    it('should create a new game', (done) => {
+      const game = {
+        title: 'Super Mario World',
+        genre: 'platform',
+      }
+      chai.
+        request(server)
+        .post('/api/game/create')
+        .send(game)
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal('Super Mario World');
+          expect(res.body.genre).to.equal('platform');
+          done();
+        })
+    })
+  })
 
   // test the GET here
+  describe('[GET]/api/game/get', (req, res) => {
+    it('should get all games', (done) => {
+      chai.request(server)
+        .get('/api/game/get')
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(200);
+          expect(Array.isArray(res.body)).to.equal(true);
+          expect(res.body.length).to.equal(1);
+          expect(res.body[0].title).to.equal('The Legend of Zelda');
+          done();
+        })
+    })
+  })
 
   // test the PUT here
-
+  describe('[PUT]/api/game/update', () => {
+    it('should update game entry based on id', (done) => {
+      const update = {
+        id: gameId,
+        title: 'Super Mario World'
+      };
+      chai.request(server)
+        .put('/api/game/update')
+        .send(update)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body.title).to.equal('Super Mario World');
+          done();
+        });
+    });
+  })
   // --- Stretch Problem ---
   // Test the DELETE here
 });
