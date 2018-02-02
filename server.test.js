0f916af87fc1a8cfc58dbb2953435e46bd0139ed
Mark Marchant <jtla3@yahoo.com>
2018-02-02 10:02:24 -0800

    beforeEach hook and afterEach hook

--- server.test.js
+++ server.test.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
+const chaiHTTP = require('chai-http');
 
 const Game = require('./models');
+chai.use(chaiHTTP);
+const server = require('./server');
 
 describe('Games', () => {
   before(done => {
@@ -25,13 +28,29 @@ describe('Games', () => {
   });
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+  let nesGameId;
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
-    // by saving the document you'll be able to use it in each of your `it` blocks
+    // by saving the document you'll be able to use it in each of your `it` 
+    const newGame = new Game({
+      title: 'Zelda',
+      genre: 'RPG',
+      releaseDate: '02/21/86'
+    });
+  });
+    newGame.save((error, game) => {
+      if (error) return done(error);
+      gameId = game.id;
+      done();
+    });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove((err) => {
+      if (err) return done(err);
+      done();
+    });
   });
 
   // test the POST here
