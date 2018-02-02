ae2a41260939086b45c26d6db483dd9c487da09b
Ting Wang <felix.wang.1026@gmail.com>
2018-02-02 12:15:40 -0500

    fixed model and added test for [POST]

--- server.test.js
+++ server.test.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
-
+const chaiHTTP = require('chai-http');
 const Game = require('./models');
+chai.use(chaiHTTP);
+const server = require('./server');
 
 describe('Games', () => {
   before(done => {
@@ -25,16 +27,50 @@ describe('Games', () => {
   });
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+  let gameId;
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: 'WOW',
+      genre: 'MMO',
+      date: '2004'
+    });
+    newGame.save((err, game) => {
+      if (err) return done(err);
+      gameId = game.id;
+      done();
+    });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove((err) => {
+      if (err) return done(err);
+      done();
+    });
   });
 
   // test the POST here
+  describe(`[POST] /api/game/create`, () => {
+    it('should create a new game', done => {
+      const gameData = {
+        title: 'WarCraft',
+        genre: 'Real-time strategy',
+        date: '2003'    
+      };
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(gameData)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body.title).to.eql('WarCraft');
+          expect(res.body.date).to.eql('2003');
+          done();
+        });
+    });
+  });
 
   // test the GET here
 
