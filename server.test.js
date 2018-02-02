096cc1f0b86d935a561910b55d01c0e135294410
Yasin Shuman <email@yasin.io>
2018-02-02 11:08:09 -0800

    GET tests working

--- server.test.js
+++ server.test.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const chai = require("chai");
+const chaiHTTP = require("chai-http");
 const { expect } = chai;
 const sinon = require("sinon");
-
+const server = require("./server");
+chai.use(chaiHTTP);
 const Game = require("./models");
 
 describe("Games", () => {
@@ -25,16 +27,16 @@ describe("Games", () => {
   });
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
-  let myGame = new Game({
-    name: "yasins awesome game",
-    date: Date.now,
-    genre: "RPG"
-  });
+
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
-
+    let myGame = new Game({
+      title: "yasins awesome game",
+      date: Date.now,
+      genre: "RPG"
+    });
     myGame
       .save()
       .then(game => {
@@ -49,16 +51,32 @@ describe("Games", () => {
   });
   afterEach(done => {
     // simply remove the collections from your DB.
-    game.remove({}, err => {
-      if (err) console.error(err);
-      done();
+    mongoose.connection.db.dropCollection("games", function(err, result) {
+      if (err) {
+        console.log("error removing collections, what did you break?");
+        done();
+      }
+      if (result) done();
     });
   });
 
   // test the POST here
 
   // test the GET here
-
+  it("should get all bands", done => {
+    chai
+      .request(server)
+      .get("/api/game/get")
+      .end((err, res) => {
+        if (err) {
+          console.log(err);
+          done();
+        }
+        expect(res.status).to.equal(200);
+        expect(res.body[0].title).to.equal("yasins awesome game");
+        done();
+      });
+  });
   // test the PUT here
 
   // --- Stretch Problem ---
