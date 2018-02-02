6f1033edb475e0e3971e7b7d99b5a8ef87c3926c
Emma Drueke <emma.drueke@gmaila.com>
2018-02-02 12:26:59 -0600

    First Push

--- server.test.js
+++ server.test.js
@@ -3,7 +3,11 @@ const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
 
+const ChaiHTTP = require('chai-http');
+
 const Game = require('./models');
+
+chai.use(ChaiHTTP);
 
 describe('Games', () => {
   before(done => {
@@ -23,18 +27,53 @@ describe('Games', () => {
       console.log('we are disconnected');
     });
   });
+
+
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+  let gameId;
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
-  });
+    new Game({
+      title: 'Dark Souls'
+    }).save((err, savedGame) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      gameId = savedGame.id;
+      done();
+    });
+  });//beforeEach test it needs to create a new instance of a new Game
+
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, (err) => {
+      if (err) console.log(err);
+      done();
+    });
   });
 
   // test the POST here
+  describe('[POST] /game/create', ()=> {
+    it('should add a new game', (done) => {
+      const game = {
+        title: 'Blood Born'
+      };
+
+      chai.request(server)
+        .post('/game/create')
+        .send(game)
+        .end((err, res) => {
+          if (err) return console.log(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal('Blood Born');
+          done();
+        });
+    });
+  });
 
   // test the GET here
 
