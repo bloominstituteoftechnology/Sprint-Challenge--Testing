6bd275dcf0e5968e8481e2323d96450adfdf0ffb
Clara Andrew <candrewwani@gmail.com>
2018-03-16 12:19:44 -0400

    [POST] tests added
    server.test.js: Added a test for successful and unsuccesful post requests.

--- server.test.js
+++ server.test.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
-const chai = require('chai');
+const mongoose = require("mongoose");
+const chai = require("chai");
 const { expect } = chai;
-const sinon = require('sinon');
+const sinon = require("sinon");
+const chaihtttp = require("chai-http");
 
-const Game = require('./models');
+const Game = require("./models");
+const server = require("./server");
+chai.use(chaihtttp);
 
-describe('Games', () => {
+describe("Games", () => {
   before(done => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/test');
+    mongoose.connect("mongodb://localhost/test");
     const db = mongoose.connection;
-    db.on('error', () => console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('we are connected');
+    db.on("error", () => console.error.bind(console, "connection error"));
+    db.once("open", () => {
+      console.log("we are connected");
       done();
     });
   });
@@ -20,21 +23,77 @@ describe('Games', () => {
   after(done => {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
-      console.log('we are disconnected');
+      console.log("we are disconnected");
     });
   });
-  // declare some global variables for use of testing
-  // hint - these wont be constants because you'll need to override them.
+
+  let gameId;
+  let marioGame;
+  
   beforeEach(done => {
-    // write a beforeEach hook that will populate your test DB with data
-    // each time this hook runs, you should save a document to your db
-    // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: "California Games",
+      genre: "Sports",
+      date: "June 1987"
+    });
+
+    newGame
+      .save()
+      .then(game => {
+        gameId = game.id;
+        marioGame = game;
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
   afterEach(done => {
-    // simply remove the collections from your DB.
+    Game.remove({}, error => {
+      if (error) console.error(error);
+      done();
+    });
   });
 
   // test the POST here
+  describe("[POST] /api/game/create", () => {
+    it("should add a new game", done => {
+      const myGame = new Game({
+        title: "Super Mario Bros.",
+        genre: "Action",
+        date: "September 1985"
+      });
+      chai
+        .request(server)
+        .post("/api/game/create")
+        .send(myGame)
+        .end((err, res) => {
+          expect(res.body.title).to.equal("Super Mario Bros.");
+          expect(res.body).to.be.a("object");
+          done();
+        });
+    });
+    it("should return a 422 error", done => {
+      const myGame = {
+        genre: "Sport",
+        date: "June 1987"
+      };
+      chai
+        .request(server)
+        .post("/api/game/create")
+        .send(myGame)
+        .end((err, res) => {
+          if (err) {
+            expect(err.status).to.equal(422);
+            expect(err.response.body.error).to.equal(
+              "Error saving data to the DB"
+            );
+            done();
+          }
+        });
+    });
+  });
 
   // test the GET here
 
