b3424378b4028a2d8d7bf548017b9a41f2ddd544
Your Name <haywood.d.johnson@gmail.com>
2018-03-16 12:28:53 -0500

    Fumbling

--- server.test.js
+++ server.test.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
+const chaihttp = require('chai-http');
 
+const server = require('./server');
 const Game = require('./models');
+
+chai.use(chaihttp);
 
 describe('Games', () => {
   before(done => {
@@ -29,12 +33,48 @@ describe('Games', () => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const game = new Game({
+      title: 'Destiny 2',
+      genre: 'ShootEmUp'
+    });
+    game
+      .save()
+      .then(_ => {
+        newFile = game.id;
+        done();
+      })
+      .catch(err => ({ err: `game saveFile corrupted` }));
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove()
+      .then(_ => {
+        done();
+      })
+      .catch(err => {
+        done(err);
+      });
   });
 
   // test the POST here
+  describe('[POST] /api/game/create', _ => {
+    it(`should post correctly to database`, _ => {
+      const newGame = new Game({
+        title: 'California Games',
+        genre: 'Sports',
+        date: 'June 1987'
+      });
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(newGame)
+        .end((err, res) => {
+          if (err) console.log(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal('California Games');
+        });
+    });
+  });
 
   // test the GET here
 
