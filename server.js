e1577dfb246b178b8a0fe50b24429615ce2d4c96
Samuel Cha <sunchipssam@gmail.com>
2018-03-16 11:24:46 -0700

    Added route to just /api/game/destroy

--- server.js
+++ server.js
@@ -85,4 +85,32 @@ server.delete('/api/game/destroy/:id', (req, res) => {
   });
 });
 
+server.delete('/api/game/destroy', (req, res) => {
+  // to delete a game you can send up an id on the request body or the params
+  let id = undefined;
+  if (req.params.id) {
+    // if it's on the params set it.
+    id = req.params.id;
+  }
+  if (req.body.id) {
+    // if it's on the body set it.
+    id = req.body.id;
+  }
+  if (id === undefined) {
+    // if it's undefined throw error back to client
+    res.status(422);
+    res.json({ error: 'You need to give me an ID' });
+    return;
+  }
+  Game.findByIdAndRemove(id, (err, removedGame) => {
+    // search for game by that id and remove it
+    if (err) {
+      res.status(422);
+      res.json({ error: 'Cannot find game by that id' });
+      return;
+    }
+    res.json({ success: `${removedGame.title} was removed from the DB` });
+  });
+});
+
 module.exports = server;
