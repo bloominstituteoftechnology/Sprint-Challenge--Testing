da3ad3958201fc03517b2786d8b64feba2d36ebf
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-03-16 13:17:57 -0400

    Hooks complete

--- server.test.js
+++ server.test.js
@@ -29,16 +29,39 @@ describe('Games', () => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: 'Final Fantasy VII',
+      releaseDate: 'January 31, 1997',
+      genre: 'JRPG'
+    });
+    newGame
+      .save()
+      .then(game => {
+        testGame = game;
+        gameId = game._id;
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.error(err);
+      done();
+    })
   });
 
   // test the POST here
+  describe(`[POST]`, () => {});
 
   // test the GET here
+  describe(`[GET]`, () => {});
 
   // test the PUT here
+  describe(`[PUT]`, () => {});
 
   // --- Stretch Problem ---
   // Test the DELETE here
