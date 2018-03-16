0362cb2fbefbd210804ebf3e02c64d670fb3b5b8
Troy Bradley <me@bitcointroy.co>
2018-03-16 13:07:19 -0500

    All required tests written and passing. Docs done.

--- server.test.js
+++ server.test.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
+const chaiHTTP = require('chai-http');
 const { expect } = chai;
 const sinon = require('sinon');
+
+const server = require('./server');
+chai.use(chaiHTTP);
 
 const Game = require('./models');
 
@@ -34,8 +38,8 @@ describe('Games', () => {
 		// by saving the document you'll be able to use it in each of your `it` blocks
 		const myGame = new Game({
 			title: 'California Games',
-			genre: 'Sports',
-			date: 'June 1987'
+			releaseDate: 'June 1987',
+			genre: 'Sports'
 		});
 		myGame
 			.save()
@@ -58,10 +62,80 @@ describe('Games', () => {
 	});
 
 	// test the POST here
+	describe(`[POST] /game`, () => {
+		it('should add a new game', (done) => {
+			const myGame = {
+				title: 'Mario Brothers',
+				releaseDate: '1981',
+				genre: 'Karting'
+			};
+			chai.request(server).post('/api/game/create').send(myGame).end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body.title).to.equal('Mario Brothers');
+				done();
+			});
+		});
+		it('should send back 422 upon bad data', (done) => {
+			const myGame = {
+				ditle: 'Mario Brothers', // ditle
+				genre: 'Karting',
+				date: '1981'
+			};
+			chai.request(server).post('/api/game/create').send(myGame).end((err, res) => {
+				if (err) {
+					expect(err.status).to.equal(422);
+					done();
+				} //handle error
+			});
+		});
+	});
 
 	// test the GET here
+	describe(`[GET] /api/game/get`, () => {
+		it('should get all games', (done) => {
+			chai.request(server).get('/api/game/get').end((err, res) => {
+				if (err) {
+					throw new Error(err);
+					done();
+				}
+				expect(res.body[0].title).to.eql(testGame.title);
+				expect(res.body[0]._id).to.equal(gameId.toString());
+				done();
+			});
+		});
+	});
 
 	// test the PUT here
+	describe(`[PUT] /api/game/update`, () => {
+		it('update a game given an id and some text', (done) => {
+			const gameUpdate = {
+				id: gameId,
+				title: 'Mario Brothers'
+			};
+			chai.request(server).put('/api/game/update').send(gameUpdate).end((err, res) => {
+				if (err) {
+					throw new Error(err);
+					done();
+				}
+				expect(res.body.title).to.equal(gameUpdate.title);
+				done();
+			});
+		});
+		it('handles the error if a bad gameId sent', (done) => {
+			const gameUpdate = {
+				id: 'asdfasdf',
+				title: 'Mario Brothers'
+			};
+			chai.request(server).put('/api/game/update').send(gameUpdate).end((err, res) => {
+				if (err) {
+					expect(err.status).to.equal(422);
+					const { error } = err.response.body;
+					expect(error).to.eql('Cannot find game by that id');
+				}
+				done();
+			});
+		});
+	});
 
 	// --- Stretch Problem ---
 	// Test the DELETE here
