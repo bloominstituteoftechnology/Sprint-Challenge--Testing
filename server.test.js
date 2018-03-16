f2d29407adb6339da713a967f2ca57943a1d19b8
Troy Bradley <me@bitcointroy.co>
2018-03-16 11:13:59 -0500

    beforeEach, afterEach, variables

--- server.test.js
+++ server.test.js
@@ -6,40 +6,63 @@ const sinon = require('sinon');
 const Game = require('./models');
 
 describe('Games', () => {
-  before(done => {
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/test');
-    const db = mongoose.connection;
-    db.on('error', () => console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('we are connected');
-      done();
-    });
-  });
+	before((done) => {
+		mongoose.Promise = global.Promise;
+		mongoose.connect('mongodb://localhost/test');
+		const db = mongoose.connection;
+		db.on('error', () => console.error.bind(console, 'connection error'));
+		db.once('open', () => {
+			console.log('we are connected');
+			done();
+		});
+	});
 
-  after(done => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(done);
-      console.log('we are disconnected');
-    });
-  });
-  // declare some global variables for use of testing
-  // hint - these wont be constants because you'll need to override them.
-  beforeEach(done => {
-    // write a beforeEach hook that will populate your test DB with data
-    // each time this hook runs, you should save a document to your db
-    // by saving the document you'll be able to use it in each of your `it` blocks
-  });
-  afterEach(done => {
-    // simply remove the collections from your DB.
-  });
+	after((done) => {
+		mongoose.connection.db.dropDatabase(() => {
+			mongoose.connection.close(done);
+			console.log('we are disconnected');
+		});
+	});
+	// declare some global variables for use of testing
+	// hint - these wont be constants because you'll need to override them.
+	let gameId = null;
+	let testGame = null;
 
-  // test the POST here
+	beforeEach((done) => {
+		// write a beforeEach hook that will populate your test DB with data
+		// each time this hook runs, you should save a document to your db
+		// by saving the document you'll be able to use it in each of your `it` blocks
+		const myGame = new Game({
+			title: 'California Games',
+			genre: 'Sports',
+			date: 'June 1987'
+		});
+		myGame
+			.save()
+			.then((game) => {
+				testGame = game;
+				gameId = game._id;
+				done();
+			})
+			.catch((err) => {
+				console.error(err);
+				done();
+			});
+	});
+	afterEach((done) => {
+		// simply remove the collections from your DB.
+		Game.remove({}, (err) => {
+			if (err) console.error(err);
+			done();
+		});
+	});
 
-  // test the GET here
+	// test the POST here
 
-  // test the PUT here
+	// test the GET here
 
-  // --- Stretch Problem ---
-  // Test the DELETE here
+	// test the PUT here
+
+	// --- Stretch Problem ---
+	// Test the DELETE here
 });
