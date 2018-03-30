bf7f61ef37959a1b5c849acd80e857bc273f21a7
Christopher Beards <beardscm@gmail.com>
2018-03-30 11:32:43 -0400

    beforeEach and afterEach Hooks; Written and untested

--- server.test.js
+++ server.test.js
@@ -25,13 +25,36 @@ describe('Games', () => {
   });
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+
+  let testGame, testGameID;
+
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
-  });
+    new Game({
+      title: 'SinonSpies',
+      releaseDate: 'March 30',
+      genre: 'Adventure'
+    })
+    .save()
+    .then(game => {
+      testGame = game;
+      testGameID = game._id;
+      done();
+    })
+    .catch(err => {
+      console.error(err);
+      done();
+    })
+});
+
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.error(err);
+      done();
+    });
   });
 
   // test the POST here
