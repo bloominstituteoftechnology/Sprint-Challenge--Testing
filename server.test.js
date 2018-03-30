d4757ccfce371b2288699b4f227fa2c64c3746ba
Peter Gray <peter@petergraycreative.com>
2018-03-30 11:18:38 -0500

    Add working POST test
    Also have non-working get request

--- server.test.js
+++ server.test.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
+const chaiHTTP = require('chai-http');
 const { expect } = chai;
 const sinon = require('sinon');
+const server = require('./server.js');
+
+chai.use(chaiHTTP);
 
 const Game = require('./models');
 
 describe('Games', () => {
-  before(done => {
+  before((done) => {
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://localhost/test');
     const db = mongoose.connection;
@@ -17,24 +21,80 @@ describe('Games', () => {
     });
   });
 
-  after(done => {
+  after((done) => {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
       console.log('we are disconnected');
     });
   });
-  // declare some global variables for use of testing
-  // hint - these wont be constants because you'll need to override them.
-  beforeEach(done => {
-    // write a beforeEach hook that will populate your test DB with data
-    // each time this hook runs, you should save a document to your db
-    // by saving the document you'll be able to use it in each of your `it` blocks
-  });
-  afterEach(done => {
-    // simply remove the collections from your DB.
+
+  let gameId = null;
+  let testGame = null;
+
+  beforeEach((done) => {
+    const newGame = new Game({
+      title: 'California Games',
+      genre: 'Sports',
+      date: 'June 1987',
+    });
+    newGame
+      .save()
+      .then((game) => {
+        testGame = game;
+        gameId = game._id;
+        done();
+      })
+      .catch((err) => {
+        console.error(err);
+        done();
+      });
   });
 
-  // test the POST here
+  afterEach((done) => {
+    Game.remove({}, (err) => {
+      if (err) console.error(err);
+      done();
+    });
+  });
+
+  describe('[POST] /api/game/create', () => {
+    it('should save a new game to the db', (done) => {
+      const newGame = {
+        title: 'Another Sweet game',
+        genre: 'Fighting',
+        date: 'June 1989',
+      };
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(newGame)
+        .end((err, res) => {
+          if (err) {
+            console.error(err);
+            done();
+          }
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal('Another Sweet game');
+        });
+      done();
+    });
+  });
+
+  describe('[GET] /api/game/get', () => {
+    it('should return all games from the db', (done) => {
+      chai.request(server)
+        .get('/api/game/get')
+        .end((err, res) => {
+          if (err) {
+            console.error(err);
+            done();
+          }
+          expect(res.status).to.equal(200);
+          expect(res.body[0].title).to.equal('Another Sweet game');
+        })
+      done();
+    })
+  });
 
   // test the GET here
 
