4200cb3b56dd971c376899121da040fd6cdf0889
Amanda Phillips <amanda@phillipsdev.org>
2018-03-30 11:24:46 -0500

    POST done

--- server.test.js
+++ server.test.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
+const server = require('./server');
+
+const chaiHTTP = require('chai-http');
 
 const Game = require('./models');
+chai.use(chaiHTTP);
 
 describe('Games', () => {
   before(done => {
@@ -25,16 +29,60 @@ describe('Games', () => {
   });
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+  let gameId = null;
+  let testGame = null;
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const myGame = new Game({
+      title: 'Tales of Vesperia',
+      genre: 'RPG',
+      releaseDate: 'Aug 2008',
+    });
+    myGame
+      .save()
+      .then(game => {
+        testGame = game;
+        gameId = game.id;
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.error(err);
+      done();
+    });
   });
 
   // test the POST here
+  describe('[POST] /api/game/create', () => {
+    it('should add a new game', done => {
+      const myGame = {
+        title: 'Tales of Symphonia',
+        genre: 'RPG',
+        releaseDate: 'Aug 2003',
+      };
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(myGame)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            done();
+          }
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal(myGame.title);
+        });
+      done();
+    });
+  });
 
   // test the GET here
 
