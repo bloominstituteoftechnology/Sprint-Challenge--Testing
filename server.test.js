af09e5c21b093a9da747860359838a6a49f692f2
Aaron Burk <aarondburk@gmail.com>
2018-03-30 11:28:57 -0500

    update server.test.js
    - add test data to before each
    - remove test data in the after each method.

--- server.test.js
+++ server.test.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
+const chaihttp = require('chai-http');
+
 const { expect } = chai;
 const sinon = require('sinon');
 
 const Game = require('./models');
+chai.use(chaihttp);
 
 describe('Games', () => {
   before(done => {
+    console.log('running `before` method');
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://localhost/test');
     const db = mongoose.connection;
@@ -18,21 +22,54 @@ describe('Games', () => {
   });
 
   after(done => {
+    console.log('running `after` method');
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
       console.log('we are disconnected');
     });
   });
+  let firstGameId;
+  let secondGameId;
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
   beforeEach(done => {
+    console.log('running `beforeEach` method')
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    new Game({
+      name: 'The Rest Of Us',
+      genre: 'Zombie',
+      releaseDate: 'June 2011',
+    }).save((err, saved) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      firstGameId = saved._id;
+      done();
+    });
+    new Game({
+      name: 'Madden NFL 2027',
+      genre: 'Sports'
+    }).save((err, saved) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      secondGameId = saved._id;
+      done();;
+    });
   });
   afterEach(done => {
+    console.log('running `afterEach` method');
     // simply remove the collections from your DB.
-  });
+      Game.remove({}, err => {
+        if (err) console.error(err);
+      done();
+      });
+    });
+
 
   // test the POST here
 
