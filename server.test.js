2f5cc39c829c28521d2d603e283e7ab72951be1f
Michael Marshal <mikemarshalman@gmail.com>
2018-03-30 14:03:33 -0500

    Post and node_modules folder (sorry Patrick)

--- server.test.js
+++ server.test.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
+const { assert } = chai;
 const sinon = require('sinon');
+const chaiHTTP = require('chai-http');
 
 const Game = require('./models');
 
+const server = require('./server');
+chai.use(chaiHTTP);
+
 describe('Games', () => {
   before(done => {
+    let gameId = null;
+    let gameName = null;
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://localhost/test');
     const db = mongoose.connection;
@@ -29,13 +36,53 @@ describe('Games', () => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: 'Half Life 2',
+      releaseDate: 'November 2004',
+      genre: 'First Person Shooter'
+    });
+    newGame
+      .save()
+      .then(game => {
+        gameName = game;
+        gameId = game._id;
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
   });
-  afterEach(done => {
+  afterEach((done) => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.log(err);
+      done();
+    });
   });
 
   // test the POST here
-
+  describe('[Post] /api/game/create', () => {
+    it('should post to database', done => {
+      const newGame = new Game({
+        title: 'Portal',
+        date: 'July 2004',
+        genre: 'Puzzle'
+      });
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(newGame)
+        .end((err, res) => {
+          if(err) {
+            return console.log(err)
+            done();
+          }
+          assert.equal(res.body.title, 'Portal');
+          done();
+        })
+    })
+  })
   // test the GET here
 
   // test the PUT here
