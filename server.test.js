c02aa40c0388f815ab5b4688e022289ea1155531
german gonzalez <gcomlnk@gmail.com>
2018-03-30 15:10:08 -0400

    reformatting spaces

--- server.test.js
+++ server.test.js
@@ -1,45 +1,47 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
-const { expect } = chai;
+const {expect} = chai;
 const sinon = require('sinon');
 
 const Game = require('./models');
 
 describe('Games', () => {
-  before(done => {
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/test');
-    const db = mongoose.connection;
-    db.on('error', () => console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('we are connected');
-      done();
+    before(done => {
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/test');
+        const db = mongoose.connection;
+        db.on('error', () => console.error.bind(console, 'connection error'));
+        db.once('open', () => {
+            console.log('we are connected');
+            done();
+        });
     });
-  });
 
-  after(done => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(done);
-      console.log('we are disconnected');
+    after(done => {
+        mongoose.connection.db.dropDatabase(() => {
+            mongoose.connection.close(done);
+            console.log('we are disconnected');
+        });
     });
-  });
-  // declare some global variables for use of testing
-  // hint - these wont be constants because you'll need to override them.
-  beforeEach(done => {
-    // write a beforeEach hook that will populate your test DB with data
-    // each time this hook runs, you should save a document to your db
-    // by saving the document you'll be able to use it in each of your `it` blocks
-  });
-  afterEach(done => {
-    // simply remove the collections from your DB.
-  });
+    // declare some global variables for use of testing
+    // hint - these wont be constants because you'll need to override them.
+    let
+        beforeEach
+    (done => {
+        // write a beforeEach hook that will populate your test DB with data
+        // each time this hook runs, you should save a document to your db
+        // by saving the document you'll be able to use it in each of your `it` blocks
+    });
+    afterEach(done => {
+        // simply remove the collections from your DB.
+    });
 
-  // test the POST here
+    // test the POST here
 
-  // test the GET here
+    // test the GET here
 
-  // test the PUT here
+    // test the PUT here
 
-  // --- Stretch Problem ---
-  // Test the DELETE here
+    // --- Stretch Problem ---
+    // Test the DELETE here
 });
