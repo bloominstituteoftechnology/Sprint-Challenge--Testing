43a9679817cd33faedddb49cd60b36f82c04eade
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-03-30 11:11:00 -0400

    GET Test passing all tests, but not everytime
    -Async issues with beforeEach hook timing out on GET request
    -Test functions sometimes but not always

--- server.test.js
+++ server.test.js
@@ -21,44 +21,37 @@ describe('Games', () => {
     });
   });
 
-  after(done => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(done);
-      console.log('we are disconnected');
-    });
-  });
-  
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
-  
+
   let testGame = null;
   let gameId = null;
-  
+
   // write a beforeEach hook that will populate your test DB with data
   // each time this hook runs, you should save a document to your db
   // by saving the document you'll be able to use it in each of your `it` blocks
-  
+
   beforeEach(done => {
     const testerGame = new Game({
       title: 'Contra',
       genre: `Shoot 'em Up`,
-      releaseDate: 'February, 1987'
+      releaseDate: 'February, 1987',
     });
     testerGame
-    .save()
-    .then(game => {
-      testGame = game;
-      gameId = game._id;
-      done();
-    })
-    .catch(err => {
-      console.error(err);
-      done();
-    });
+      .save()
+      .then(game => {
+        testGame = game;
+        gameId = game._id;
+        done();
+      })
+      .catch(err => {
+        console.error(err);
+        done();
+      });
   });
-  
+
   // simply remove the collections from your DB.
-  
+
   afterEach(done => {
     Game.remove({}, err => {
       if (err) console.error(err);
@@ -66,47 +59,70 @@ describe('Games', () => {
     });
   });
 
+  after(done => {
+    mongoose.connection.db.dropDatabase(() => {
+      mongoose.connection.close(done);
+      console.log('we are disconnected');
+    });
+  });
   // test the POST here
   describe(`[POST] /api/game/create`, () => {
-    it ('should add a new game to the collection', done => {
+    it('should add a new game to the collection', done => {
       const testGame = {
         title: 'Contra',
         genre: `Shoot 'em Up`,
-        releaseDate: 'February, 1987'
+        releaseDate: 'February, 1987',
       };
       chai
-      .request(server)
-      .post('/api/game/create')
-      .send(testGame)
-      .end((err, res) => {
-        console.log(res.body);
-        expect(res.status).to.equal(200);
-        expect(res.body.title).to.equal('Contra')
-        done();
-      });
-    });
-    it (`should send back '422: Invalid input data sent to Server' upon bad data`, () => {
-      const testGame = {
-        title: 'Contra',
-        genre: `Shoot 'em Up`,
-        releaseDate: 'February, 1987'
-      };
-      chai
-      .request(server)
-      .post('/api/game/create')
-      .send(testGame)
-      .end((err, res) => {
-        if (err) {
-          expect(err.status).to.equal(422);
-          const { error } = err.response.body;
-          expect(error).to.equal('Invalid input data sent to Server')
+        .request(server)
+        .post('/api/game/create')
+        .send(testGame)
+        .end((err, res) => {
+          console.log(res.body);
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal('Contra');
           done();
-        }
-      });
+        });
+    });
+    it(`should send back '422: Invalid input data sent to Server' upon bad data`, () => {
+      const testGame = {
+        title: 'Contra',
+        genre: `Shoot 'em Up`,
+        releaseDate: 'February, 1987',
+      };
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(testGame)
+        .end((err, res) => {
+          if (err) {
+            expect(err.status).to.equal(422);
+            const { error } = err.response.body;
+            expect(error).to.equal('Invalid input data sent to Server');
+            done();
+          }
+        });
     });
   });
   // test the GET here
-
+  describe(`[GET] /api/game/get`, () => {
+    it('should get a list of all the games in the collection', done => {
+      chai
+        .request(server)
+        .get('/api/game/get')
+        .end((err, res) => {
+          if (err) {
+            throw new Error(err);
+            done();
+          }
+          console.log(res.body[1]._id);
+          console.log(gameId.toString());
+          expect(res.body[0].title).to.equal(testGame.title);
+          expect(res.body[1]._id).to.equal(gameId.toString());
+          done();
+        });
+    });
+  });
   // test the PUT here
 
   // --- Stretch Problem ---
