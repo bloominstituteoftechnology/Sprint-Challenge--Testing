fd43dd7638e6d7ca3a80abdb2fc1818b93dc09d4
TERR1E <terrieb.kim@gmail.com>
2018-03-30 10:33:03 -0700

    POST test working

--- server.test.js
+++ server.test.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
+const chaiHTTP = require('chai-http');
 const { expect } = chai;
 const sinon = require('sinon');
+const server = require('./server');
 
 const Game = require('./models');
+
+chai.use(chaiHTTP);
 
 describe('Games', () => {
   before(done => {
@@ -28,14 +32,16 @@ describe('Games', () => {
   let gameId = null;
   let testGame = null;
 
+  const STATUS_OK = 200;
+  const STATUS_UNPROCESSABLE = 422;
+
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
     const myGame = new Game({
       title: 'Kirby\'s Adventure',
-      genre: 'Adventure',
-      releaseDate: '1993'
+      genre: 'Adventure'
     });
     myGame
       .save()
@@ -58,6 +64,42 @@ describe('Games', () => {
   });
 
   // test the POST here
+  describe(`[POST] /api/game/create`, () => {
+    it('should add a new game', done => {
+      const myGame = {
+        title: 'Tetris',
+        genre: 'Puzzle',
+        releaseDate: '1984'
+      };
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(myGame)
+        .end((err, res) => {
+          expect(res.status).to.equal(STATUS_OK);
+          expect(res.body.title).to.equal('Tetris');
+          done();
+        });
+    });
+    it('should send back 422 upon bad data', done => {
+      const myGame = {
+        character: 'Kirby',
+        genre: 'Adventure'
+      };
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send(myGame)
+        .end((err, res) => {
+          if (err) {
+            expect(err.status).to.equal(STATUS_UNPROCESSABLE);
+            const { error } = err.response.body;
+            expect(error).to.eql('Error saving data to the DB');
+            done();
+          }
+        });
+    });
+  });
 
   // test the GET here
 
