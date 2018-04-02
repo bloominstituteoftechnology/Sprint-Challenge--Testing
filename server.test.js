3f09817e89ccec8222817750e4e00d8647ea1bd8
unknown <davsoudry@gmail.com>
2018-04-01 23:37:27 -0500

    Added beforeEach, afterEach, POST test

--- server.test.js
+++ server.test.js
@@ -23,20 +23,70 @@ describe('Games', () => {
       console.log('we are disconnected');
     });
   });
+
   // declare some global variables for use of testing
   // hint - these wont be constants because you'll need to override them.
+  let id = null;
+  let game = null;
+
   beforeEach(done => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const testGame = new Game({
+      title: 'Go Fish',
+      genre: 'Card Games',
+      releaseDate: 'April 1, 1978'
+    });
+    testGame.save((error, saved) => {
+      if (error) {
+        console.error(err);
+        return done();
+      }
+      id = saved._id;
+      game = saved;
+      done();
+    });
   });
+
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, (error, removed) => {
+      if (error) {
+        console.error(err);
+        return done();
+      }
+      done();
+    });
   });
 
   // test the POST here
+  describe('[POST] /api/game/create', () => {
+    it('should add a new game', (done) => {
+      const newGame = new Game({
+        title: 'California Games',
+        genre: 'Sports',
+        date: 'June 1987'
+      });
+      chai.request(server)
+        .post('/api/game/create')
+        .send(newGame)
+        .end((err, res) => {
+          if (err) {
+            console.error(err);
+            done();
+          }
+          expect(res.status).to.equal(200);
+          expect(res.body.title).to.equal('California Games');
+        });
+        done();
+    });
+  });
 
   // test the GET here
+  describe('[GET] /api/game/get', () => {
+    it('should return')
+  })
 
   // test the PUT here
 
