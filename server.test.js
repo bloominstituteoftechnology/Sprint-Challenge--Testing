0f7d61dd6b7c0f481d5d7332c666cd1f76238d2a
Khepf <jeremy1975@gmail.com>
2018-04-27 11:37:31 -0400

    added documentation and set beforeEach, afterEach

--- server.test.js
+++ server.test.js
@@ -25,14 +25,29 @@ describe('Games', () => {
   });
   let gameId;
   // hint - these wont be constants because you'll need to override them.
-  beforeEach(done => {
-
+  beforeEach(() => {
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      name: 'Radiohead',
+      genre: 'Alt-rock'
+    });
+    return newGame
+      .save()
+      .then(savedGame => {
+        gameId = savedGame._id.toString();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
-  afterEach(done => {
+
+  afterEach(() => {
     // simply remove the collections from your DB.
+    return Game.remove({}, err => {
+      if (err) console.log(err);
+    });
   });
 
   // test the POST here
@@ -40,7 +55,7 @@ describe('Games', () => {
   // test the GET here
 
   // Test the DELETE here
-  
+
   // --- Stretch Problem ---
   // test the PUT here
 });
