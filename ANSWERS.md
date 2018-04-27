0c183a4807ebaa70350616e5cc5178680f8ec58f
arcadia-ego <arcadia-ego@github.com>
2018-04-27 08:51:56 -0700

    post test added

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,6 +2,15 @@
 
 1. In Mocha, what are the differences between `before`, `after`, `beforeEach`, and `afterEach`? When do they run? What are they used for?
 
+* before: Runs a particular function before an entire describe block.
+* after: runs a particular function after an entire describe block
+* beforeEach: runs a particular function before every single test in a describe block.
+* afterEach: runs a particular function after every single test in a describe block.
+
 2. What is the point of Test Driven Development? What do you personally think about this approach?
 
+* Test driven development is an approach to writing code that values passing tests first, above all else. I think that in certain cases it may be useful, perhaps extremely large applications that are being added to constantly, but unnecessary for smaller projects.
+
 3. What is a `spy` in `sinon`? How do we use it to effectively test a `callback`?
+
+* A sinon spy is a function that is able to record lots of information on how it was called. It can effectively be used to test a callback function as it can tell you how many times that callback was called, how it was called, when it was called, and arguments that it was called with.
--- server.test.js
+++ server.test.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
-const chai = require('chai');
-const { expect } = chai;
-const sinon = require('sinon');
+const mongoose = require("mongoose");
+const chai = require("chai");
+const { expect, assert } = chai;
+const sinon = require("sinon");
+const server = require("./server");
+const Game = require("./models");
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
-const Game = require('./models');
-
-describe('Games', () => {
+describe("Games", () => {
   before(done => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/test');
+    mongoose.connect("mongodb://localhost/test");
     const db = mongoose.connection;
-    db.on('error', () => console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('we are connected');
+    db.on("error", () => console.error.bind(console, "connection error"));
+    db.once("open", () => {
+      console.log("we are connected");
       done();
     });
   });
@@ -20,27 +22,63 @@ describe('Games', () => {
   after(done => {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
-      console.log('we are disconnected');
+      console.log("we are disconnected");
     });
   });
   let gameId;
+  let outerGame;
   // hint - these wont be constants because you'll need to override them.
   beforeEach(done => {
-
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: "Master Blaster",
+      genre: "Platformer",
+      releaseDate: "1988"
+    });
+    newGame.save((err, savedGame) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      gameId = savedGame._id.toString();
+      outerGame = newGame;
+      done();
+    });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.log(err);
+      return done();
+    });
   });
 
   // test the POST here
-
+  describe("POST a new game in database using /api/game/create", () => {
+    it("should add a new game to the DB.", done => {
+      // const games = Game.find({});
+      chai.request(server)
+      .post('/api/game/create')
+      .send(outerGame)
+      .end((err, response) => {
+        if (err) {
+          console.log(err);
+          return done();
+        }
+        expect(response.status).to.equal(200);
+        expect(response.body.title).equal("Master Blaster");
+        expect(response.body.genre).to.equal("Platformer");
+        expect(response.body.releaseDate).equal("1988");
+        return done();
+      });
+    });
+  });
   // test the GET here
 
   // Test the DELETE here
-  
+
   // --- Stretch Problem ---
   // test the PUT here
 });
