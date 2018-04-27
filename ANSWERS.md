1e65a521132aa3e4c1ea0699ee2e6d0c029fbb8f
YFN Lucci <anderson56ak@gmail.com>
2018-04-27 13:32:31 -0500

    AAndersonSprintTesting

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,7 +1,12 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
-1. In Mocha, what are the differences between `before`, `after`, `beforeEach`, and `afterEach`? When do they run? What are they used for?
+1. In Mocha, what are the differences between `before`, `after`, `beforeEach`, and `afterEach`? When do they run? What are they used for? 
+ - `before` - Runs before  all test in said block.
+ - `after` - Runs after all test in said block.
+ - `beforeEach` - Runs before eact test in said block.
+ - `afterEach` - Runs after each test in said block.
 
 2. What is the point of Test Driven Development? What do you personally think about this approach?
+- Test-driven development is a development technique where you must first write a test that fails before you write new functional code. I mean i dont mind it as it makes for good practice for me to put out the most efficient code.
 
 3. What is a `spy` in `sinon`? How do we use it to effectively test a `callback`?
--- server.test.js
+++ server.test.js
@@ -1,46 +1,99 @@
-const mongoose = require('mongoose');
-const chai = require('chai');
+const mongoose = require("mongoose");
+const chai = require("chai");
 const { expect } = chai;
-const sinon = require('sinon');
+const sinon = require("sinon");
+const chaiHttp = require("chai-http");
+const Game = require("./models");
+const server = require("./server");
 
-const Game = require('./models');
+chai.use(chaiHttp);
 
-describe('Games', () => {
+describe("Games", () => {
   before(done => {
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/test');
-    const db = mongoose.connection;
-    db.on('error', () => console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('we are connected');
-      done();
-    });
-  });
-
-  after(done => {
-    mongoose.connection.db.dropDatabase(() => {
-      mongoose.connection.close(done);
-      console.log('we are disconnected');
-    });
-  });
+    
   let gameId;
-  // hint - these wont be constants because you'll need to override them.
   beforeEach(done => {
-
-    // write a beforeEach hook that will populate your test DB with data
-    // each time this hook runs, you should save a document to your db
-    // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: "Shaq-Fu",
+      genre: "Fighting",
+      releaseDate: "October 28, 1994"
+    });
+    newGame.save((err, savedGame) => {
+      if (err) {
+        console.log(err);
+        return done();
+      }
+      gameId = newGame._id.toString();
+      return done();
+    });
   });
+
   afterEach(done => {
-    // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.log(err);
+      return done();
+    });
   });
 
-  // test the POST here
+  describe(`[POST] /api/game/create`, () => {
+    it('Should save a new game to the database', done => {
+      chai
+        .request(server)
+        .post("/api/game/create")
+        .send({
+          title: "PaperBoy",
+          releaseDate: "1985",
+          genre: "Action Game"
+        })
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          expect(response.status).to.equal(200);
+          expect(response.body).to.be.an('object');
+          expect(response.body.title).to.equal('Paperoy');
+          return done();
+        });
+    });
+  });
 
-  // test the GET here
+  describe(`[GET] /api/game/get`, () => {
+    it("Should get a list of all games in the database", done => {
+      chai
+        .request(server)
+        .get("/api/game/get")
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            return done();
+          }
+          expect(response.status).to.equal(200);
+          expect(response.body[0].title).to.equal('Shaq-Fu');
+          expect(response.body).to.be.an('Array');
+          return done();
+        });
+    });
+  });
 
-  // Test the DELETE here
-  
+  describe(`[DELETE] /api/game/destroy/:id`, () => {
+    it("Should delete a game from the database", done => {
+      chai
+        .request(server)
+        .delete(`/api/game/destroy/${gameId}`)
+        .end((err, res) => {
+          if (err) {
+            console.og(err);
+            return done();
+          }
+          expect(res.status).to.equal(200);
+          expect(res.body.success).to.equal('Shaq-Fu was removed from the DB');
+          return done();
+        });
+    });
+  });
+
   // --- Stretch Problem ---
   // test the PUT here
+})
 });
