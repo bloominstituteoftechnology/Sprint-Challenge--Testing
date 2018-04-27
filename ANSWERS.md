131d7394d2a0633524423f995d6eab2f3b7c82f3
William <wtpwinberg@gmail.com>
2018-04-27 11:48:14 -0700

    Completed GET and POST tests

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,7 +1,11 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
-1. In Mocha, what are the differences between `before`, `after`, `beforeEach`, and `afterEach`? When do they run? What are they used for?
+1.  In Mocha, what are the differences between `before`, `after`, `beforeEach`, and `afterEach`? When do they run? What are they used for?
 
-2. What is the point of Test Driven Development? What do you personally think about this approach?
+`before` is run once before all the tests in a `describe` and `after` is run once after all the tests in a `describe`, whereas `beforeEach` is run before each test in a describe, and `afterEach` after each test.
 
-3. What is a `spy` in `sinon`? How do we use it to effectively test a `callback`?
+2.  What is the point of Test Driven Development? What do you personally think about this approach?
+    It's a good way of making sure things are working properly and continue to as a project scales. It may seem tedious but can save much time in the long run.
+
+3.  What is a `spy` in `sinon`? How do we use it to effectively test a `callback`?
+    A spy is a function that records all the activity and traits of itself when used in place of data
--- server.test.js
+++ server.test.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
+const chaiHTTP = require('chai-http');
 const { expect } = chai;
+const server = require('./server');
 const sinon = require('sinon');
 
 const Game = require('./models');
+
+chai.use(chaiHTTP);
 
 describe('Games', () => {
   before(done => {
@@ -26,21 +30,84 @@ describe('Games', () => {
   let gameId;
   // hint - these wont be constants because you'll need to override them.
   beforeEach(done => {
-
+    const newGame = new Game({
+      title: 'Skyrim',
+      genre: 'RPG',
+      releaseDate: 'June 2011'
+    });
+    newGame
+      .save()
+      .then(savedGame => {
+        gameId = savedGame._id.toString();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    done();
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.log(err);
+      return done();
+    });
   });
 
   // test the POST here
-
+  describe(`[POST] /api/game/create`, () => {
+    it('should save a game to the db', done => {
+      chai
+        .request(server)
+        .post('/api/game')
+        .send({ title: 'Skyrim', genre: 'RPG', date: 'June 2011' })
+        .then(response => {
+          // is res.body.length === 2?
+          // console.log(response.body);
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it(`Should fail if title or genre aren't provided`, () => {
+      return chai
+        .request(server)
+        .post('/api/game')
+        .send({ poo: 'sry' })
+        .then(res => {
+          const genreMessage = res.body.errors.genre.message;
+          const titleMessage = res.body.errors.title.message;
+          expect(res.status).to.equal(422);
+          expect(genreMessage).to.equal('Path `genre` is required.');
+          expect(titleMessage).to.equal('Path `title` is required.');
+        });
+    });
+  });
   // test the GET here
-
+  describe(`[GET] /api/game/get`, () => {
+    it('should get a list of all the games in db', done => {
+      chai
+        .request(server)
+        .get('/api/game/get')
+        .then(response => {
+          const { _id, title, genre } = response.body[0];
+          expect(response.status).to.equal(200);
+          expect(response.body).to.be.an('array');
+          expect(_id).to.equal(gameId);
+          expect(title).to.equal('Skyrim');
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it.skip('Should fail if bad URL is provided', () => { });
+  });
   // Test the DELETE here
-  
+
   // --- Stretch Problem ---
   // test the PUT here
 });
