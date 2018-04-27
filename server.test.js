83fc7d2a2d6cb8bccae2e6d56ba8e966ec4aafb4
Kevin Jolley <serrowpls@gmail.com>
2018-04-27 12:55:22 -0500

    3 passing 1 failing, bad info doesn't work on POST

--- server.test.js
+++ server.test.js
@@ -3,7 +3,11 @@ const chai = require('chai');
 const { expect } = chai;
 const sinon = require('sinon');
 
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
 const Game = require('./models');
+const server = require('./server');
 
 describe('Games', () => {
   before(done => {
@@ -26,21 +30,68 @@ describe('Games', () => {
   let gameId;
   // hint - these wont be constants because you'll need to override them.
   beforeEach(done => {
-
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const newGame = new Game({
+      title: 'Game',
+      genre: 'RPG',
+      releaseDate: 'yes',
+    });
+    newGame
+      .save()
+      .then(savedGame => {
+        gameId = savedGame._id;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    done();
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if (err) console.log(err);
+      return done();
+    });
   });
 
   // test the POST here
+  describe('[POST] /api/game/create', () => {
+    it('should save the information to the database', done => {
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send({
+          title: 'Game',
+          genre: 'RPG',
+          releaseDate: 'yes',
+        })
+        .then(response => {
+          done();
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+    it('Should fail if bad information is provided', done => {
+      chai
+        .request(server)
+        .post('/api/game/create')
+        .send({ data: 'bad' })
+        .then(response => {
+          console.log(response.body.errors);
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+  });
 
   // test the GET here
 
   // Test the DELETE here
-  
+
   // --- Stretch Problem ---
   // test the PUT here
 });
