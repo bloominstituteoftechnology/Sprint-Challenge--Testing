7b250c237138c9827a29929fa47de6def42c422d
Cristina Laryea <crispad06@gmail.com>
2018-04-27 12:50:52 -0400

    beforeEach and afterEach added and passing tests

--- server.test.js
+++ server.test.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
 const { expect } = chai;
+const chaiHTTP = require('chai-http');
 const sinon = require('sinon');
 
 const Game = require('./models');
+const server = require('./server');
+
+chai.use(chaiHTTP);
 
 describe('Games', () => {
   before(done => {
@@ -23,18 +27,39 @@ describe('Games', () => {
       console.log('we are disconnected');
     });
   });
-  let gameId;
+  let gameId = null;
+  let testGame = null;
   // hint - these wont be constants because you'll need to override them.
   beforeEach(done => {
-
     // write a beforeEach hook that will populate your test DB with data
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
+    const testGame = new Game({
+      title: 'Hello Kitty',
+      genre: 'Kitty World',
+      releaseDate: 'January, 2001'
+    });
+    testGame
+      .save()
+      .then(game => {
+        testGame = game;
+        gameId = game._id;
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
   });
   afterEach(done => {
     // simply remove the collections from your DB.
+    Game.remove({}, err => {
+      if(err) console.log(err);
+      done();
+    });
   });
 
+  
   // test the POST here
 
   // test the GET here
