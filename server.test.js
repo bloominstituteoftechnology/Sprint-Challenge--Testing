b9c227f515a6a53ee17f6baf8a06cd0e73687a93
Khepf <jeremy1975@gmail.com>
2018-04-27 12:06:58 -0400

    added post, still working on get

--- server.test.js
+++ server.test.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const chai = require('chai');
+const chaiHTTP = require('chai-http');
 const { expect } = chai;
 const sinon = require('sinon');
 
+const server = require('./server');
 const Game = require('./models');
+
+chai.use(chaiHTTP);
 
 describe('Games', () => {
   before(done => {
@@ -30,8 +34,9 @@ describe('Games', () => {
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
     const newGame = new Game({
-      name: 'Radiohead',
-      genre: 'Alt-rock'
+      title: 'Test Games',
+      genre: 'Test',
+      releaseDate: 'June 1975'
     });
     return newGame
       .save()
@@ -51,8 +56,42 @@ describe('Games', () => {
   });
 
   // test the POST here
+  describe(`[POST] /api/game/create`, () => {
+    it('should save a game to the db', () => {
+      return chai
+        .request(server)
+        .post('/api/game/create')
+        .send({
+          title: 'California Games',
+          genre: 'Sports',
+          releaseDate: 'June 1987'
+        })
+        .then(response => {
+          expect(response.status).to.equal(200);
+          expect(response.body).to.be.an('object');
+          console.log('Game added!');
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+  });
 
   // test the GET here
+  describe(`[GET] /api/game/get`, () => {
+    it('should get the games', () => {
+      return chai
+        .request(server)
+        .get('/api/game/get')
+        .then(response => {
+          expect(response.status).to.equal(200);
+          expect(response.body).to.be.an('array');
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+  });
 
   // Test the DELETE here
 
