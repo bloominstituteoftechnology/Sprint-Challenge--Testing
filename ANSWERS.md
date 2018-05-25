d06614b49f7a9fc7de817ae741132fd8f4d97665
ajf412 <aaronjfaulkner@yahoo.com>
2018-05-25 14:21:57 -0400

    Requirements met except delete.  Almost there, but not quite.

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,7 +2,16 @@
 
 
 1. In Jest, what are the differences between beforeAll, afterAll, beforeEach, and afterEach? When do they run? What are they used for?
-1. What is the point of Test Driven Development? What do you personally think about this approach?
-1. What is a mock function? How do we use it to test a callback passed to a function?
-1. Mention three types of tests.
-1. What type of test performs database operations against a real server.
+- The run when they say they'll fun.  Before all tests, after all tests, before each test, and after each test.  They are used to set up requirements for the tests, like connecting to a database, and then for cleaning up after a/all test(s), by doing things like disconnecting from a database, or deleting temporary data.
+
+2. What is the point of Test Driven Development? What do you personally think about this approach?
+- It allows you to get what you need working up off the ground quicker, and get it testing, so you know what works, and what doesn't work, quickly.
+
+3. What is a mock function? How do we use it to test a callback passed to a function?
+- It emulates a function, so you can use it in testing.  Use it in place of the callback.
+
+4. Mention three types of tests.
+- Component tests, snapshot tests, and integration tests.
+
+5. What type of test performs database operations against a real server.
+- supertest.
--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const request = require('supertest');
 
 const Game = require('./games/Game');
+const server = require('./server');
+
+// ______________VARIABLES______________
+let gameId;
+let game;
 
 describe('Games', () => {
   beforeAll(() => {
@@ -15,24 +21,68 @@ describe('Games', () => {
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  let gameId;
+
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
+  beforeEach( async () => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+    game = {
+      title: 'Total War: WARHAMMER II',  // required
+      genre: 'Strategy Games',  // required
+      releaseDate: 'Sep 28, 2017',  // not required
+    }
+    gameId = await Game.create(game);
   });
 
   afterEach(() => {
     //   // clear collection.
+    return Game.remove({}, function (err) {
+      console.log('Database cleared');
+    });
   });
 
-  it('runs the tests', () => {});
-
+  it('runs the tests', async () => {
+    let response;
   // test the POST here
+    game = {
+      title: 'Total War: WARHAMMER',  // required
+      genre: 'Strategy Games',  // required
+      releaseDate: 'May 24, 2016',  // not required
+    }
+
+    response = request(server)
+      .post('/api/games')
+      .send(game)
+      .expect(201)
+      .end(function(err, res) {
+        if(err) throw err;
+      });
+
+    expect(response._data.title).toEqual(game.title);
+    expect(response._data.genre).toEqual(game.genre);
+    expect(response._data.releaseDate).toEqual(game.releaseDate);
+
+    console.log('_id', response._data);
+    console.log('\n YAYAYAYAYAYAAYAY \n')
+      
 
   // test the GET here
-
+    response = await request(server).get('/api/games');
+    expect(response.status).toEqual(200);
+    console.log(response.body);
+    let idToDelete = response.body[1]._id;
+    console.log(idToDelete);
   // Test the DELETE here
+    request(server)
+      .del(`/api/games/${idToDelete}`)
+      .expect(500)
+      .end(function(err, res) {
+        if(err) throw err;
+      })
+      
+      response = await request(server).get('/api/games');
+      console.log(response.body);
+  });
 });
