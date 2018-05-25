be84401972219b659b4b3ce46d645e5e7f7933ea
tanking <tanking@github.com>
2018-05-25 14:18:06 -0400

    Added test for PUT

--- server.spec.js
+++ server.spec.js
@@ -20,15 +20,15 @@ describe('Games', () => {
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach( async () => {
-    //   // write a beforeEach hook that will populate your test DB with data
-    //   // each time this hook runs, you should save a document to your db
-    //   // by saving the document you'll be able to use it in each of your `it` blocks
-    const newGame = { title: "Sonic", genre: "actions", releaseDate: "A long time ago" }
+  // beforeEach( async () => {
+  //   //   // write a beforeEach hook that will populate your test DB with data
+  //   //   // each time this hook runs, you should save a document to your db
+  //   //   // by saving the document you'll be able to use it in each of your `it` blocks
+  //   const newGame = { title: "Sonic", genre: "actions", releaseDate: "A long time ago" }
 
-    const game = await Game.create(newGame);
+  //   const game = await Game.create(newGame);
     
-  })
+  // })
 
   afterEach(() => {
     //   // clear collection.
@@ -37,73 +37,87 @@ describe('Games', () => {
 
   it('runs the tests', () => {});
 
-  // test the POST here
-  describe('POST /games', () => {
-    it('should check if games are being properly posted',  () => {
+  // // test the POST here
+  // describe('POST /games', () => {
+  //   it('should check if games are being properly posted',  () => {
       
-      // arrange
+  //     // arrange
 
-      const newGame = { title: "Sonic", genre: "actions", releaseDate: "A long time ago" }
+  //     const newGame = { title: "Sonic", genre: "actions", releaseDate: "A long time ago" }
 
-      // assert
+  //     // assert
 
-      expect(newGame.title).toBe("Sonic");
-    })
+  //     expect(newGame.title).toBe("Sonic");
+  //   })
 
-    it('should return an error if title or genre is missing' , async () => { 
-      // arrange
-      const noGenreGame = { title: "Sonic", releaseDate: "A long time ago" }
-      const noTitleGame = { genre: "action", releaseDate: "A long time ago" }
+  //   it('should return an error if title or genre is missing' , async () => { 
+  //     // arrange
+  //     const noGenreGame = { title: "Sonic", releaseDate: "A long time ago" }
+  //     const noTitleGame = { genre: "action", releaseDate: "A long time ago" }
 
-      // act
-      const ggame = request(server).post('/api/games').send(noGenreGame);
-      const tgame = request(server).post('/api/games').send(noTitleGame);
+  //     // act
+  //     const ggame = request(server).post('/api/games').send(noGenreGame);
+  //     const tgame = request(server).post('/api/games').send(noTitleGame);
 
-      // assert
-      expect(500);
-      expect(500);
+  //     // assert
+  //     expect(500);
+  //     expect(500);
 
-    })
-  })
+  //   })
+  // })
 
-  // test the GET here
-  describe('GET /games' , () => {
+  // // test the GET here
+  // describe('GET /games' , () => {
 
-    it('should show there are games in the database', () => {
+  //   it('should show there are games in the database', () => {
 
-      request(server)
-        .get('/api/games')
-        .expect(res => res.length > 0)
+  //     request(server)
+  //       .get('/api/games')
+  //       .expect(res => res.length > 0)
        
-    })
+  //   })
 
-    it('should return the proper status code', () => {
+  //   it('should return the proper status code', () => {
       
-      request(server)
-          .get('/api/games')
-          .expect(200)
-        })
-  })
+  //     request(server)
+  //         .get('/api/games')
+  //         .expect(200)
+  //       })
+  // })
 
-  // Test the DELETE here
-  describe('DELETE /game', () => {
-    it('should delete a game given its id', async () => {
-      gameId = '5b083df427909f205c605369';
+  // // Test the DELETE here
+  // describe('DELETE /game', () => {
+  //   it('should delete a game given its id', async () => {
+  //     gameId = '5b084ed8f9faa32ddf519a3f';
 
-      await Game.findByIdAndRemove(gameId);
+  //     await Game.findByIdAndRemove(gameId);
 
-      expect(204);
+  //     expect(204);
 
-    })
+  //   })
 
-    it('should return an error for a invalid ID', () => {
+  //   it('should return an error for a invalid ID', () => {
 
-      gameId = 'zzzzz';
+  //     gameId = 'zzzzz';
 
-      request(server).delete(`/api/game/${gameId}`)
+  //     request(server).delete(`/api/games/${gameId}`).expect(404);
 
-      expect(404);
+  //   })
+  // })
 
+  // TEST for PUT here
+  describe('PUT /game', () => {
+    it('should rewrite a game\'s information', async () => {
+      const updatedGame = { title: "Sonics", genre: "horror", releaseDate: "Never" }
+      gameId = '5b084ed9f9faa32ddf519a40';
+
+      const gameNow = await Game.findByIdAndUpdate(gameId, updatedGame);
+
+      expect(gameNow.title).toEqual('Sonics')
+      
+
+
+      // request(server).put(`/api/games/${gameId}`).send(gameNow).expect(204)
     })
   })
 });
