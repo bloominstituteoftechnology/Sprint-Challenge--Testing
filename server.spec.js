4d703ac23a5aca13985f2857980db1b08127d7da
cgoez <goez2001@gmail.com>
2018-05-25 09:35:56 -0700

    Added beforeEach, afterEach functions

--- server.spec.js
+++ server.spec.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Game = require('./games/Game');
+const Game = require("./games/Game");
 
-describe('Games', () => {
+describe("Games", () => {
   beforeAll(() => {
     return mongoose
-      .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='));
+      .connect("mongodb://localhost/test")
+      .then(() => console.log("\n=== connected to TEST DB ==="));
   });
 
   afterAll(() => {
     return mongoose
       .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
+      .then(() => console.log("\n=== disconnected from TEST DB ==="));
   });
 
   let gameId;
@@ -22,13 +22,28 @@ describe('Games', () => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+    const zelda = new Game({
+      title: "The Legend of Zelda",
+      genre: "Action-Adventure",
+      releaseDate: "1986"
+    });
+
+    zelda
+      .save()
+      .then(game => {
+        gameId = game._id.toString();
+      })
+      .catch(error => {
+        console.log(error)
+      })
   });
 
   afterEach(() => {
     //   // clear collection.
+    Game.remove()
   });
 
-  it('runs the tests', () => {});
+  it("runs the tests", () => {});
 
   // test the POST here
 
