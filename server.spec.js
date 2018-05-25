14a19451a5ff0e1b12be21e12b722b93932178b6
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-25 10:41:44 -0700

    beforeEach server.spec.js

--- server.spec.js
+++ server.spec.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const supertest = require('supertest');
+const server = require('./server');
 
 const Game = require('./games/Game');
 
@@ -19,20 +21,42 @@ describe('Games', () => {
   // // hint - these wont be constants because you'll need to override them.
 
   beforeEach(() => {
+    const newGame = new Game({
+      title: 'Zelda',
+      genre: 'Classic',
+      releaseDate: 'Jun 1 1980',
+    });
+
+    newGame.save((err, savedGame) => {
+      if (err) {
+        console.log(err);
+        done();
+      }
+      gameID = savedGame._id.toString();
+      done();
+    });
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
   });
 
   afterEach(() => {
+    Game.remove({}, err => {
+      if (err) console.log(err);
+      return();
+    });
     //   // clear collection.
   });
 
-  it('runs the tests', () => {});
+describe('POST /api/game/create', () => {
+  it('POST a new game to the database', () => {
+
+  });
 
   // test the POST here
 
   // test the GET here
 
   // Test the DELETE here
+})
 });
