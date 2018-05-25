6a7090915ffc01deb44e23a97b76b3c486263625
Ashiko124 <greeneselectronics@gmail.com>
2018-05-25 10:10:47 -0600

    Complted the post test

--- server.spec.js
+++ server.spec.js
@@ -17,11 +17,12 @@ describe('Games', () => {
 
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
-
+  
   beforeEach(() => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+
   });
 
   afterEach(() => {
@@ -31,6 +32,15 @@ describe('Games', () => {
   it('runs the tests', () => {});
 
   // test the POST here
+  it('should verify the info is sent to the database' , async () => {
+    const game = {genre: 'Action', title: 'SuperCops!', releaseDate: '04-23-1990'}
+    const savedGame = await Game.create(game); // new + save
+
+    expect(savedGame.title).toEqual('SuperCops!');
+    expect(savedGame.title).not.toEqual('Tommys Train Adventure!');
+    expect(savedGame.title).not.toEqual(456);
+
+  })
 
   // test the GET here
 
