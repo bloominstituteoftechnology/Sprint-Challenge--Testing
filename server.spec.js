ec137e57cdd34330b8274885386feb0f75d1916b
Nedu Robert <nedumrobert@gmail.com>
2018-05-25 17:47:49 +0100

    wrote tests for the POST route

--- server.spec.js
+++ server.spec.js
@@ -42,7 +42,37 @@ describe('Games', () => {
   it('runs the tests', () => {});
 
   // test the POST here
-  
+  describe('[POST] /api/games', () => {
+    it('should create a new game', async () => {
+      const game = new Game({
+        title: 'California Games',
+        genre: 'Sports',
+        releaseDate: 'June 1987',
+      });
+      const response = await request(server).post(`/api/games`).send(game);
+
+      expect(response.status).toEqual(201);
+      expect(response.type).toEqual('application/json');
+      expect(response.body).toMatchObject({
+        title: game.title,
+        genre: game.genre,
+        releaseDate: game.releaseDate
+      });
+      expect(response.body).toHaveProperty('_id');
+    })
+
+    it('should ensure that title and genre fields are provided', async () => {
+      const game = new Game()
+      game.validate((err) => {
+        expect(err.errors.title.kind).toBe('required')
+      });
+      game.validate((err) => {
+        expect(err.errors.genre.kind).toBe('required')
+      })
+    })
+
+  })
+
   // test the GET here
 
   // Test the DELETE here
