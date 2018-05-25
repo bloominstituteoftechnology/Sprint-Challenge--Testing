8bebf0336ddf03783b21ed8928e01d9203bc85f0
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-25 13:52:09 -0400

    revisions

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -3,17 +3,9 @@ const mongoose = require('mongoose');
 const Game = require('./Game');
 
 describe('NESGames Model', () => {
-  beforeAll(() => {
-    return mongoose
-      .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='));
-  });
+  beforeAll(() => mongoose.connect('mongodb://localhost/test').then(() => console.log('\n=== connected to TEST DB ===')));
 
-  afterAll(() => {
-    return mongoose
-      .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
-  });
+  afterAll(() => mongoose.disconnect().then(() => console.log('\n=== disconnected from TEST DB ===')));
 
   describe('#getGameTitle', () => {
     it('should return the proper game.title', () => {
--- server.spec.js
+++ server.spec.js
@@ -17,98 +17,64 @@ describe('Games', () => {
 
   describe('POST', () => {
     it('should create a new game', async () => {
-      request(server)
-        .post('/api/games')
-        .send(newGame)
-        .expect('Content-Type', /json/)
-        .expect(201)
+      const response = await request(server).post('/api/games').send(newGame)
+      expect(response.status).toBe(201)
+      expect(response.type).toBe('application/json')
     })
   
-    it('should throw an error when a new game POST does not meet all requirements', () => {
+    it('should throw an error when a new game POST does not meet all requirements', async () => {
       const noTitle = { genre: 'sci-fi', releaseDate: '3141' }
       const noGenre = { title: 'messing with mongoose', releaseDate: '3141' }
-  
-      request(server)
-        .post('/api/games')
-        .send(noTitle)
-        .expect('Content-Type', /json/)
-        .expect(500)
-  
-      request(server)
-        .post('/api/games')
-        .send(noGenre)
-        .expect('Content-Type', /json/)
-        .expect(500)
+      await request(server).post('/api/games').send(noTitle).then(res => expect(res.status).toBe(500))
+      await request(server).post('/api/games').send(noGenre).then(res => expect(res.status).toBe(500))
     })
   
-    it('should allow a user to POST a game without a release date', () => {
+    it('should allow a user to POST a game without a release date', async () => {
       const noRelease = { title: 'messing with mongoose', genre: 'sci-fi' }
-      request(server)
-        .post('/api/games')
-        .send(noRelease)
-        .expect('Content-Type', /json/)
-        .expect(201)
+      await request(server).post('/api/games').send(noRelease).then(res => expect(res.status).toBe(201))
     })
   })
 
   describe('GET', () => {
     it('should fetch all games from database', async () => {
-      request(server)
-        .get('/api/games')
-        .expect(200)
-        .expect(res => res.length === 0)
-  
       const savedGame = await Game.create(newGame)
       const anotherGame = await Game.create({ title: 'jeffrey', genre: 'flynn' })
-  
-      request(server)
-        .get('/api/games')
-        .expect(200)
-        .expect(res => res.length === 2)
+
+      await request(server).get('/api/games').then(res => {
+        expect(res.status).toBe(200)
+        expect(res.type).toBe('application/json')
+      })
     })
 
     it('should fetch a game with provided ID', async () => {
       const savedGame = await Game.create(newGame)
-      request(server)
-        .get(`/api/games/${savedGame._id}`)
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(res => res.body.title === newGame.title)
-        .expect(res => res.body.genre === newGame.genre)
-        .expect(res => res.body.releaseDate === newGame.releaseDate)
+      const response = await request(server).get(`/api/games/${savedGame._id}`)
+      expect(response.status).toBe(200)
+      expect(response.body.title).toEqual(newGame.title)
+      expect(response.body.genre).toEqual(newGame.genre)
+      expect(response.body.releaseDate).toEqual(newGame.releaseDate)
     })
 
     it('should return an error if an invalid ID is provided', async () => {
-      request(server)
-        .get('/api/games/123')
-        .expect(404)
-        .expect('Content-Type', /json/)
-        .expect(res => res.message === 'Game not found')
+      await request(server).get('/api/games/123').then(res => {
+        expect(res.status).toBe(500)
+        expect(res.type).toBe('application/json')
+      })
     })
   })
 
   describe('DELETE', () => {
     it('should delete an existing game if a proper ID is provided', async () => {
       const savedGame = await Game.create(newGame)
-      request(server)
-        .delete(`/api/games/${savedGame._id}`)
-        .expect(204) // game exists and has been removed test
+      await request(server).delete(`/api/games/${savedGame._id}`).then(res => expect(res.status).toBe(204))
     })
   
     it('should return an error if no ID is provided for DELETE', async () => {
-      request(server)
-        .delete('/api/games')
-        .expect('Content-Type', /json/)
-        .expect(res => res.message === 'You need to give me an ID')
-        .expect(422)
+      await request(server).delete('/api/games').then(res => expect(res.status).toBe(404))
     })
   
     it('should return an error if an invalid ID is provided for DELETE', async () => {
-      request(server)
-        .delete('/api/game/123')
-        .expect('Content-Type', /json/)
-        .expect(res => res.message === 'Game not found')
-        .expect(404)
+      await request(server).delete('/api/game/123').then(res => expect(res.status).toBe(404))
     })
   })
 
@@ -116,32 +82,27 @@ describe('Games', () => {
     it('should be able to update an existing user', async () => {
       const savedGame = await Game.create(newGame)
       const updates = { title: 'jeffrey', genre: 'flynn' }
-      request(server)
-        .put(`/api/games/${savedGame._id}`)
-        .send(updates)
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(res => res.body.title === 'jeffrey')
-        .expect(res => res.body.genre === 'flynn')
+      await request(server).put(`/api/games/${savedGame._id}`).send(updates).then(res => {
+        expect(res.status).toBe(200)
+        expect(res.type).toBe('application/json')
+        expect(res.text).toContain('jeffrey')
+      })
     })
   
     it('should return an error for a PUT request with no title', async () => {
       const savedGame = await Game.create(newGame)
       const updates = { genre: 'jeffrey' }
-      request(server)
-        .put(`/api/games/${savedGame._id}`)
-        .send(updates)
-        .expect(422)
-        .expect('Content-Type', /json/)
-        .expect(res => res.message === 'Must Provide a title && Id')
+      await request(server).put(`/api/games/${savedGame._id}`).send(updates).then(res => {
+        expect(res.status).toBe(422)
+        expect(res.type).toBe('application/json')
+      })
     })
   
     it('should return an error for a PUT request with an invalid ID', async () => {
-      request(server)
-        .put('/api/games/123')
-        .expect(404)
-        .expect('Content-Type', /json/)
-        .expect(res => res.message === 'Game not found')
+      await request(server).put('/api/games/123').then(res => {
+        expect(res.status).toBe(422)
+        expect(res.type).toBe('application/json')
+      })
     })
   })
 });
