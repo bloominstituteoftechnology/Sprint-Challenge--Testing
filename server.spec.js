cfe34f687bcaff16d4057c8d4e2f4969d85deaa9
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-25 11:44:19 -0700

    Fixed POST & GET, couldn't get DELETE to pass

--- server.spec.js
+++ server.spec.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
-const supertest = require('supertest');
+const request = require('supertest');
 const server = require('./server');
 
 const Game = require('./games/Game');
 
 describe('Games', () => {
+  let newGame = {
+    title: 'Zelda',
+    genre: 'Classic',
+    releaseDate: 'Jun 1 1980'
+  }
+
   beforeAll(() => {
     return mongoose
       .connect('mongodb://localhost/test')
@@ -18,60 +24,53 @@ describe('Games', () => {
   });
 
   let gameId;
-  // // hint - these wont be constants because you'll need to override them.
+  //   // hint - these wont be constants because you'll need to override them.
 
   beforeEach(() => {
-    const newGame = new Game({
-      title: 'Zelda',
-      genre: 'Classic',
-      releaseDate: 'Jun 1 1980',
-    });
-
-    newGame.save((err, savedGame) => {
-      if (err) {
-        console.log(err);
-        done();
-      }
-      gameID = savedGame._id.toString();
-      done();
-    });
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
   });
 
-  afterEach(() =>
-    Game.remove())
+  afterEach(() => Game.remove())
     //   // clear collection.
-})
 
+  it('runs the tests', () => {});
+  
   // test the POST here
-describe('POST /api/game/create', () => {
-  it('POST a new game to the database', async () => {
-    request(server)
-      .post('/api/game/create')
-      .send(newGame)
-      .expect('Content-Type', /json/)
-      .expect(201)
-  });
-})
+  describe('POST', () => {
+    it('POST a new game to the database', async () => {
+      await request(server)
+        .post('/api/games')
+        .send(newGame)
+        .then(res => {
+          expect(res.status).toBe(201)
+          expect(res.type).toBe('application/json')
+        })
+    })
+  })
 
   // test the GET here
-describe('GET /api/game', () => {
-  it('GET the list of games from database', async () => {
-    request(server)
-      .get('/api/game')
-      .expect('Content-Type', /json/)
-      .expect(200)
-  });
-})
+  describe('GET', () => {
+    it('GET the list of games from database', async () => {
+      await request(server)
+        .get('/api/games')
+        .then(res => {
+          expect(res.status).toBe(200)
+          expect(res.type).toBe('application/json')
+
+        })
+    })
+  })
 
   // Test the DELETE here
-describe('DELETE /api/game/remove/:id', () => {
-  it('DELETE game from database', async () => {
-    const savedGame = await Game.create(newGame)
-    request(server)
-      .delete(`api/game/${savedGame._id}`)
-      .expect(204)
+  describe('DELETE', () => {
+    it('DELETE game from database', async () => {
+      const savedGame = await Game.create(newGame)
+      await request(server)
+        .delete(`api/games/${savedGame._id}`)
+        .then(res =>
+          expect(res.status).toBe(204))
+    })
   })
-});
+})
