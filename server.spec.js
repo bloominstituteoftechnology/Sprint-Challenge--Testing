b5a3ed57c56727a9de169dcbb290c75182434d8e
Devon Bacon <devon.bacon@gmail.com>
2018-05-25 09:10:48 -0700

    server.spec.js - set up describes for each end point

--- server.spec.js
+++ server.spec.js
@@ -1,5 +1,7 @@
+const request = require('supertest');
 const mongoose = require('mongoose');
 
+const server = require('./server');
 const Game = require('./games/Game');
 
 describe('Games', () => {
@@ -18,21 +20,36 @@ describe('Games', () => {
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
 
+ 
+  
   beforeEach(() => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+
   });
 
   afterEach(() => {
     //   // clear collection.
+
   });
 
   it('runs the tests', () => {});
 
   // test the POST here
+  describe('[POST] /api/games', () => {
+
+  });
+  
 
   // test the GET here
+  describe('[GET] /api/games', () => {
 
+  })
+  
   // Test the DELETE here
+  describe('[DELETE] /api/games/:id', () => {
+
+  });
+ 
 });
