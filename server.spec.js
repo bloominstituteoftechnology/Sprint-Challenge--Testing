052007b9ae26c841815008dadb4943bb5d6a3712
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-25 11:10:06 -0400

    test for creating new game

--- server.spec.js
+++ server.spec.js
@@ -1,35 +1,27 @@
 const mongoose = require('mongoose');
-
+const request = require('supertest');
+const server = require('./server');
 const Game = require('./games/Game');
 
 describe('Games', () => {
-  beforeAll(() => {
-    return mongoose
-      .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='));
-  });
 
-  afterAll(() => {
-    return mongoose
-      .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
-  });
+  const newGame = { title: 'messing with mongoose', genre: 'sci-fi', releaseDate: '3141' }
 
-  let gameId;
-  // // hint - these wont be constants because you'll need to override them.
+  beforeAll(() => mongoose.connect('mongodb://localhost/test').then(() => console.log('\n=== connected to TEST DB ===')))
 
-  beforeEach(async () => {
-    const newGame = { title: 'messing with mongoose', genre: 'sci-fi', releaseDate: '3141' }
-    const savedGame = await Game.create(newGame);
-  });
+  afterAll(() => mongoose.disconnect().then(() => console.log('\n=== disconnected from TEST DB ===')))
 
-  afterEach(() => {
-    //   // clear collection.
-  });
+  afterEach(() => Game.remove())
 
   it('runs the tests', () => {});
 
-  // test the POST here
+  it('should create a new game', async () => {
+    request(server)
+      .post('/api/games')
+      .send(newGame)
+      .expect('Content-Type', /json/)
+      .expect(201)
+  })
 
   // test the GET here
 
