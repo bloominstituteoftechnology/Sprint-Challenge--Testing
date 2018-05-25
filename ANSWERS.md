71463d2a50f1c07541979921a4f61fadd017e5de
Henry K <mykim1891@hotmail.com>
2018-05-25 09:41:21 -0700

    defined beforeAll, afterAll, beforeEach, afterEach

--- ANSWERS.md
+++ ANSWERS.md
@@ -18,6 +18,6 @@ Kind of like a quality assurance test where you repeat tests in order to meet sp
 Mock function is self-explanatory. It simulates a function by capturing calls to the function and instances of constructor functions, returning test-time configuration of return value, rather than relying on acutal implementation of a function.
 
 1. Mention three types of tests.
-
+Unit test, Integration test, 
 
 1. What type of test performs database operations against a real server.
--- server.spec.js
+++ server.spec.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Game = require('./games/Game');
+const Game = require("./games/Game");
 
-describe('Games', () => {
+const faker = describe("Games", () => {
   beforeAll(() => {
     return mongoose
-      .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='));
+      .connect("mongodb://localhost/test")
+      .then(() => console.log("\n=== connected to TEST DB ==="));
   });
 
   afterAll(() => {
     return mongoose
       .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
+      .then(() => console.log("\n=== disconnected from TEST DB ==="));
   });
 
   let gameId;
@@ -22,13 +22,16 @@ describe('Games', () => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+    const game = { title: "Starcraft", genre: "RTS", releaseDate: "1998" };
+    const newGame = await User.create(game); // new + save
   });
 
   afterEach(() => {
     //   // clear collection.
+    return Game.remove();
   });
 
-  it('runs the tests', () => {});
+  it("runs the tests", () => {});
 
   // test the POST here
 
