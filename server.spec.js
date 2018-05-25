4eb1b14282f27da66bcb7fdf0a0fcac0900ff1a8
harrisonbrock <harrison@harrisonbrock.com>
2018-05-25 14:08:02 -0400

    Added Test cases for delete

--- server.spec.js
+++ server.spec.js
@@ -21,11 +21,22 @@ describe('Games', () => {
     let gameId;
     // // hint - these wont be constants because you'll need to override them.
 
-    beforeEach(() => {
-        const newGame = new Game ({title: 'Buster', genre: 'RST', releaseDate: 'May 25 1948'}).save();
-        const newGame2 = new Game ({title: 'Buster', genre: 'RST', releaseDate: 'May 25 1948'}).save();
 
-
+    beforeEach(done => {
+        const newGame = new Game({
+            title: "Home movie",
+            genre: "None",
+            releaseDate: "2017"
+        });
+        newGame.save((err, savedGame) => {
+            if (err) {
+                console.log(err);
+            } else {
+                gameId = savedGame._id;
+                console.log(gameId);
+            }
+            done();
+        });
     });
 
     afterEach(() => {
@@ -60,9 +71,20 @@ describe('Games', () => {
         let response = await request(server)
             .get('/api/games');
         expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(2);
+        expect(response.body).toHaveLength(1);
 
     });
 
     // Test the DELETE here
+    describe('DELETE', () => {
+        it('should delete game', async () => {
+            const response = await request(server).delete(`/api/games/${gameId}`);
+            expect(response.status).toBe(204);
+        });
+
+        it('should get an error deleting with wrong id', async () => {
+            const response = await request(server).delete(`/api/games/`);
+            expect(response.status).toBe(404);
+        });
+    });
 });
