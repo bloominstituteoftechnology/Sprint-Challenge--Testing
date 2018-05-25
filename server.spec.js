dffeeef7bb76a90de86acae767240d553125480d
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-25 09:17:14 -0700

    filled out POST tests/ got runs the tests to pass

--- server.spec.js
+++ server.spec.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-
+const request = require('supertest');
+const server = require('./server');
 const Game = require('./games/Game');
 
 describe('Games', () => {
+  const newGame = { title: 'Harry Potter', genre: 'sci-fi', releaseDate: '021208'};
   beforeAll(() => {
     return mongoose
       .connect('mongodb://localhost/test')
@@ -26,12 +28,44 @@ describe('Games', () => {
 
   afterEach(() => {
     //   // clear collection.
+    Game.remove();
   });
 
   it('runs the tests', () => {});
 
   // test the POST here
+  describe('POST', () => {
+    it('should create a new game', async() => {
+      request(server)
+        .post('/api/games')
+        .send(newGame)
+        .expect('Content-Type', /json/)
+        .expect(201)
+    });
+    it('should throw an error when a new game POST does not meet all requirements', () => {
+      const noTitle = { genre: 'sci-fi', releaseDate: '021208'};
+      const noGenre = { title: 'Harry Potter', releaseDate: '021208'};
+      request(server)
+        .post('/api/games')
+        .send(noTitle)
+        .expect('Content-Type', /json/)
+        .expect(500)
 
+      request(server)
+        .post('/api/games')
+        .send(noGenre)
+        .expect('Content-Type', /json/)
+        .expect(500)
+    });
+    it('should allow a user to POST a game without a release date', () => {
+      const noRelease = { title: 'Harry Potter', genre: 'sci-fi'};
+      request(server)
+        .post('/api/games')
+        .send(noRelease)
+        .expect('Content-Type', /json/)
+        .expect(201)
+    });
+  })
   // test the GET here
 
   // Test the DELETE here
