765f851bbbf2b0ffef61bf64f12da458b3d08468
Herbert Hodgson <herberthodgson@hotmail.com>
2018-06-22 14:14:52 -0400

    Added tests to Game.spec.js

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -1,24 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Game = require('./Game');
+const Game = require("./Game");
 
-describe('The Game Model', () => {
+const game = {
+  title: "Super Mario 3",
+  genre: "Platforming",
+  releaseDate: "October 1988"
+};
+
+describe("The Game Model", () => {
   beforeAll(() => {
     return mongoose
-      .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='))
+      .connect("mongodb://localhost/test")
+      .then(() => console.log("\n=== connected to TEST DB ==="))
       .catch(err => {
-        console.log('error connecting to TEST database, is MongoDB running?');
+        console.log("error connecting to TEST database, is MongoDB running?");
       });
   });
 
   afterAll(() => {
     return mongoose
       .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
+      .then(() => console.log("\n=== disconnected from TEST DB ==="));
   });
 
-  it('runs the tests', () => {});
+  it("runs the tests", () => {});
 
   // test away!
+  it("Should return a title, genre, and release date", async () => {
+    const savedGame = await Game.create(game);
+
+    expect(savedGame.title).toEqual(game.title);
+    expect(savedGame.genre).toEqual(game.genre);
+    expect(savedGame.releaseDate).toEqual(game.releaseDate);
+  });
+
+  it("title, genre, and release date should have a type of string", async () => {
+    const savedGame = await Game.create(game);
+
+    expect(typeof savedGame.title).toBe("string");
+    expect(typeof savedGame.genre).toBe("string");
+    expect(typeof savedGame.releaseDate).toBe("string");
+  });
 });
