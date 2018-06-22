2d8589a416bcd573f984460bc0f08ad84f54840f
jesuarva <jesuarva.developer@gmail.com>
2018-06-22 18:22:18 +0200

    GAMES MODEL: Implement test case to validate required fields

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -13,12 +13,39 @@ describe('The Game Model', () => {
   });
 
   afterAll(() => {
-    return mongoose
-      .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
+    return mongoose.disconnect().then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  it('runs the tests', () => {});
+  describe('Test validators', () => {
+    const testData = new Game({
+      title: 'testing required fields',
+      releaseDate: 'June 2018',
+    });
+
+    afterEach(() => {
+      return Game.deleteOne({ title: testData.title })
+        .then(response => {
+          console.log('response', response);
+        })
+        .catch(e => {
+          console.log('error', e);
+        });
+    });
+
+    it('If required fields are no provided return an error', async () => {
+      return Game.create(testData)
+        .then(response => {
+          expect('if we are here the test fails').toEqual(
+            'the document was created even though there were missing required-params'
+          );
+        })
+        .catch(e => {
+          expect(e._message).toEqual('Game validation failed');
+          expect(e.name).toEqual('ValidationError');
+          expect(e.errors.genre.kind).toEqual('required');
+        });
+    });
+  });
 
   // test away!
 });
