4fd9f6867754075f510a1575ca24e83a88622089
A99 <A99@99s-macbook-pro.home>
2018-06-22 21:21:31 +0200

    Add game title test (not working)

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 const Game = require('./Game');
 
-const game = { title: 'Lord of the Rings', genre: 'fantasy', releaseDate: 'Nov-1-2001' }
+const game = { title: 'Lord of the Rings', genre: 'fantasy', releaseDate: 'Nov-1-2001' };
+
+const gameNoTitle = { title: '', genre: 'fantasy', releaseDate: 'Nov-1-2001' };
+
+const gameNoGenre = { title: 'Lord of the Rings', genre: '', releaseDate: 'Nov-1-2001' };
+
+const gameNoReleaseDate = { title: 'Lord of the Rings', genre: 'fantasy', releaseDate: '' };
+
+const emptyGame = { title: '', genre: '', releaseDate: '' };
+
+// const response = (game) => Game.create(game);
+
+// function response(game) {
+//    Game.create(game)
+//     .then(res => res)
+//     .catch(err => {
+//       throw new Error(err);
+//     });
+// }
+
 
 describe('The Game Model', () => {
   beforeAll(() => {
@@ -13,16 +32,28 @@ describe('The Game Model', () => {
       });
   });
 
-  afterAll(() => {
+  afterAll(async () => {
+    const { title } = game;
+    await Game.findOneAndDelete(title);
     return mongoose
       .disconnect()
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  it('creates a new user', async () => {;
-    const newGame = await Game.create(game);
-    expect(newGame.title).toEqual(game.title);
-    expect(newGame.genre).toEqual(game.genre);
-    expect(newGame.releaseDate).toEqual(game.releaseDate);
+  it('creates a new game', async () => {;
+    const response = await Game.create(game);
+    expect(response.title).toEqual(game.title);
+    expect(response.genre).toEqual(game.genre);
+    expect(response.releaseDate).toEqual(game.releaseDate);
   });
+
+  it('returns the game title', async () => {;
+    const response = await Game.find(game);
+    const title = await response.getGameTitle;
+    expect(title).toEqual(game.title);
+  });
+
+  // it('rejects a new game without a title', async () => {;
+  //   const response = await Game.create(gameNoTitle);
+  // });
 });
