5794c6f86f04e603f1dde1702971af624f3e8a38
Chris Honda <christopher.honda92@gmail.com>
2018-06-22 09:39:00 -0700

    Added tests for get and post

--- api/server.spec.js
+++ api/server.spec.js
@@ -15,13 +15,47 @@ describe('Game', () => {
         .then(() => console.log('Disconnected from database'));
     });
 
-    beforeEach(() => {});
-
-    afterEach(() => {
-        return Game.remove();
+    beforeEach(() => {
+        const savedGame = await Game.create({
+            title: 'California Games',
+            genre: 'Sports',
+            releaseDate: 'June 1987'
+        });
     });
 
-    afterAll(() => {
-        return mongoose.disconnect();
-    })
+    afterEach(() => {
+        await Game.remove();
+        return Game.remove();
+    });
+//Does it run tests?
+
+    it('runs tests', () => {});
+
+//Does it run GET requests?
+
+    it('should return status 200', async () => {
+        const game = await request(server).get('/api/games')
+        expect(game.status).toBe(200)
+    });
+
+    it('should return an array with game objects, and the first object should be the initial gameObj object', async () => {
+        const game = await request(server).get('/api/games')
+        expect(games.body[0]).toMatchObject(gameObj)
+    });
+
+//Does it run POST requests?
+
+    it('should return status 201', async () => {
+        const game = await request(server).post('/api/games').send(gameObj)
+        expect(game.status).toBe(201)
+    });
+
+    it('should return status 500 if request is incomplete', async () => {
+        const game = await request(server).post('/api/games').send({
+            title: 'Monopoly', genre: 'How-to-ruin-a-friendship'
+        })
+        expect(game.status).toBe(500)
+    });
+
+//
 })
