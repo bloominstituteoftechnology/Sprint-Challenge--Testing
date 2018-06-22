8242b55dc574229702af3237323b2effe167b8de
A99 <A99@99s-macbook-pro.home>
2018-06-22 18:10:47 +0200

    Add create user test

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
 const Game = require('./Game');
+
+const game = { title: 'Lord of the Rings', genre: 'fantasy', releaseDate: 'Nov-1-2001' }
 
 describe('The Game Model', () => {
   beforeAll(() => {
@@ -18,7 +19,10 @@ describe('The Game Model', () => {
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  it('runs the tests', () => {});
-
-  // test away!
+  it('creates a new user', async () => {;
+    const newGame = await Game.create(game);
+    expect(newGame.title).toEqual(game.title);
+    expect(newGame.genre).toEqual(game.genre);
+    expect(newGame.releaseDate).toEqual(game.releaseDate);
+  });
 });
