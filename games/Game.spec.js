b007e1febf750d428729d3f44bd4c7e483357011
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-06-22 14:08:49 -0400

    Wrote code to test game creation in the Game.spec.js file.

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -5,7 +5,7 @@ const Game = require('./Game');
 describe('The Game Model', () => {
   beforeAll(() => {
     return mongoose
-      .connect('mongodb://localhost/test')
+      .connect('mongodb://localhost/games')
       .then(() => console.log('\n=== connected to TEST DB ==='))
       .catch(err => {
         console.log('error connecting to TEST database, is MongoDB running?');
@@ -18,7 +18,22 @@ describe('The Game Model', () => {
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  it('runs the tests', () => {});
+  it('It should return the title, genre, and release date of the game.', async () => {
+    const fortnite = { title: 'Fortnite', genre: 'Survival', releaseDate: 'July 25, 2017' };
+    const godofwar = { title: 'God of War 4', genre: 'Action-adventure', releaseDate: 'April 20, 2018' };
 
-  // test away!
+    const savedGameOne = await Game.create(fortnite);
+    const savedGameTwo = await Game.create(godofwar);
+
+    expect(savedGameOne.title).toEqual(fortnite.title);
+    expect(savedGameOne.genre).toEqual(fortnite.genre);
+    expect(savedGameOne.releaseDate).toEqual(fortnite.releaseDate);
+
+    expect(savedGameTwo.title).toEqual(godofwar.title);
+    expect(savedGameTwo.genre).toEqual(godofwar.genre);
+    expect(savedGameTwo.releaseDate).toEqual(godofwar.releaseDate);
+
+    await Game.deleteOne({ username: fortnite.username });
+    await Game.deleteOne({ username: godofwar.username }); 
+  });
 });
