b8dc7b0ca32f072a5fa4d1a27ab8a93dd8dfd507
Bradiowave <divebombwalker@gmail.com>
2018-06-22 10:04:25 -0700

    added 2 tests for both GET and DELETE methods

--- index.spec.js
+++ index.spec.js
@@ -20,18 +20,20 @@ describe('The API Server', () => {
   });
 
   let gameId;
+  let game = {};
   // // hint - these wont be constants because you'll need to override them.
 
   beforeEach(() => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
-    const game = {
+    game = {
       title: 'California Games',
       genre: 'Sports',
       releaseDate: 'June 1987'
     };
-    return Game.create(game);
+    Game.create(game);
+    return;
   });
 
   afterEach(() => {
@@ -52,9 +54,9 @@ describe('The API Server', () => {
 
     const newGame = await request(server).post('/api/games').send(expectedNewGame);
 
-    expect(newGame.body.title).toBe('Uncharted');
-    expect(newGame.body.genre).toBe('Adventure');
-    expect(newGame.body.releaseDate).toBe('Nov 2007');
+    expect(newGame.body.title).toBe(expectedNewGame.title);
+    expect(newGame.body.genre).toBe(expectedNewGame.genre);
+    expect(newGame.body.releaseDate).toBe(expectedNewGame.releaseDate);
     expect(newGame.status).toBe(expectedStatusCode);
   })
 
@@ -68,27 +70,51 @@ describe('The API Server', () => {
 
     const newGame = await request(server).post('/api/games').send(expectedNewGame);
 
-    expect(newGame.text).toContain(expectedErrorMessage);
+    expect(newGame.body.message).toBe(expectedErrorMessage);
     expect(newGame.status).toBe(expectedStatusCode);
   })
 
 
   // test the GET here
-  it('Returns a JSON object of the existing games and an OK status code of 200', async () => {
-    const expectedStatusCode = 201;
-    const expectedNewGame = {
-      title: 'Uncharted',
-      genre: 'Adventure',
-      releaseDate: 'Nov 2007'
-    };
+  it('Returns a JSON object of the existing game and an OK status code of 200', async () => {
+    const expectedStatusCode = 200;
 
-    const newGame = await request(server).post('/api/games').send(expectedNewGame);
+    const gameArray = await request(server).get('/api/games');
 
-    expect(newGame.body.title).toBe('Uncharted');
-    expect(newGame.body.genre).toBe('Adventure');
-    expect(newGame.body.releaseDate).toBe('Nov 2007');
-    expect(newGame.status).toBe(expectedStatusCode);
+    expect(gameArray.body[0].title).toBe(game.title);
+    expect(gameArray.body[0].genre).toBe(game.genre);
+    expect(gameArray.body[0].releaseDate).toBe(game.releaseDate);
+    expect(gameArray.status).toBe(expectedStatusCode);
   })
 
+  it('Returns an error when endpoint is incorrect', async () => {
+    const expectedStatusCode = 404;
+
+    const gameArray = await request(server).get('/api/semag');
+
+    expect(gameArray.status).toBe(expectedStatusCode);
+  })
+
+
   // Test the DELETE here
+  it('Returns an empty JSON object and a No Content status code of 204', async () => {
+    const expectedStatusCode = 204;
+
+    const existingGame = await request(server).get('/api/games');
+    const existingGameId = existingGame.body[0]._id;
+    const deletedGame = await request(server).delete(`/api/games/${existingGameId}`);
+
+    expect(deletedGame.body).toEqual({});
+    expect(deletedGame.status).toEqual(expectedStatusCode);
+  })
+
+  it('Returns an error message if the id is not found', async () => {
+    const expectedStatusCode = 404;
+    const expectedErrorMessage = 'Game not found';
+    const existingGameId = 'thisaintreal';
+    const deletedGame = await request(server).delete(`/api/games/${existingGameId}`);
+
+    expect(deletedGame.body.message).toBe(expectedErrorMessage);
+    expect(deletedGame.status).toBe(expectedStatusCode);
+  })
 });
