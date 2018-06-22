437ccceb2e69fba8d268e9bbb5e1d2c1f790c6d8
Xang Thao <xangthao24@yahoo.com>
2018-06-22 09:27:04 -0700

    Able to test get all games

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -49,5 +49,14 @@ describe('The Game Model', () => {
     expect(createdGame.body.title).toEqual("First game");
   })
 
-  
+  it("should be able to get all the games", async () => {
+    let createdGame1 = await superTestRequest(server).post("/api/games").send(game1).set('Accept', 'application/json');
+    let createdGame2 = await superTestRequest(server).post("/api/games").send(game2).set('Accept', 'application/json');
+
+    let allGames = await superTestRequest(server).get("/api/games").set('Accept', 'application/json')
+    
+    allGames = JSON.parse(allGames.text); // parse to json
+    expect(allGames[0].title).toEqual("First game");
+    expect(allGames[1].title).toEqual("Second game");
+  })  
 });
