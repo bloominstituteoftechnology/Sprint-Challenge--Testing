057ce34ce28da02a83dcf4967bd0c8e40e315cae
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-06-22 15:53:44 -0400

    Wrote code in the index.spec.js file to test the post endpoint for creating a game.

--- index.spec.js
+++ index.spec.js
@@ -1,24 +1,25 @@
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const request = require('supertest');
+const server = require('./api/server');
 
 describe('The API Server', () => {
   beforeAll(() => {
     return mongoose
-      .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='))
+      .connect('mongodb://localhost/games')
+      .then(() => console.log('\n=== connected to games DB ==='))
       .catch(err => {
-        console.log('error connecting to TEST database, is MongoDB running?');
+        console.log('error connecting to games database, is MongoDB running?');
       });
   });
 
   afterAll(() => {
     return mongoose
       .disconnect()
-      .then(() => console.log('\n=== disconnected from TEST DB ==='));
+      .then(() => console.log('\n=== disconnected from games DB ==='));
   });
 
-  let gameId;
+  let gameOneId;
+  let gameTwoId;
   // // hint - these wont be constants because you'll need to override them.
 
   beforeEach(() => {
@@ -31,9 +32,30 @@ describe('The API Server', () => {
     //   // clear the games collection.
   });
 
-  it('runs the tests', () => {});
+  it('It should return a status code of 201 and return the title, genre, and release date of the game created.', async () => {
 
-  // test the POST here
+    // test the POST here
+
+    const expectedStatusCode = 201;
+
+    const fortnite = { title: 'Fortnite', genre: 'Survival', releaseDate: 'July 25, 2017' };
+    const responseOne = await request(server).post('/api/games').send(fortnite);
+
+    const godofwar = { title: 'God of War 4', genre: 'Action-adventure', releaseDate: 'April 20, 2018' };
+    const responseTwo = await request(server).post('/api/games').send(godofwar);
+
+    expect(responseOne.status).toEqual(expectedStatusCode);
+    expect(responseOne.body.title).toEqual(fortnite.title);
+    expect(responseOne.body.genre).toEqual(fortnite.genre)
+    expect(responseOne.body.releaseDate).toEqual(fortnite.releaseDate)
+    expect(responseOne.type).toEqual('application/json');
+
+    expect(responseTwo.status).toEqual(expectedStatusCode);
+    expect(responseTwo.body.title).toEqual(godofwar.title);
+    expect(responseTwo.body.genre).toEqual(godofwar.genre);
+    expect(responseTwo.body.releaseDate).toEqual(godofwar.releaseDate);
+    expect(responseTwo.type).toEqual('application/json');
+  });
 
   // test the GET here
 
