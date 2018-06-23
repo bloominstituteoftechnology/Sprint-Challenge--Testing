ec5f41dc3453fddb59cf8b6c0a4a63f86651ef0a
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-23 13:26:00 +0800

    Configured tests in Game.spec.js and index.spec.ja

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const Game = require('./Game');
+let gameId;
+let gameTitle;
 
 describe('The Game Model', () => {
   beforeAll(() => {
@@ -18,7 +20,26 @@ describe('The Game Model', () => {
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  it('runs the tests', () => {});
+  it('should return a list of games', async () => {
+    const gamesArr = await Game.find().then(games => games).catch(err => err);
+    
+    expect(gamesArr.length).toBeGreaterThan(0);
+  });
 
-  // test away!
+  it.skip('should create a new game and return that game as a JSON object', async () => {
+    const body = { title: 'Game A', genre: 'A', releaseDate: 'Jun 22, 2018' };
+    const newGame = await Game.create(body).then(game => game).catch(err => err);
+    gameId = newGame._id;
+    gameTitle = newGame.title;
+
+    console.log(gameId);
+    
+    expect(newGame.title).toEqual(body.title);
+  });
+
+  it.skip('should delete a game based on the ID and return the game as a JSON object', async () => {
+    const deletedGame = await Game.findByIdAndDelete(gameId).then(game => game).catch(err => err);
+
+    expect(deletedGame.title).toEqual(gameTitle);
+  });
 });
--- index.spec.js
+++ index.spec.js
@@ -22,41 +22,57 @@ describe('The API Server', () => {
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
+  beforeEach(async () => {
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
+    const games = [
+      { title: 'A', genre: 'A', releaseDate: 'A' },
+      { title: 'B', genre: 'B', releaseDate: 'B' },
+      { title: 'C', genre: 'C', releaseDate: 'C' },
+      { title: 'D', genre: 'D', releaseDate: 'D' },
+      { title: 'E', genre: 'E', releaseDate: 'E' },
+    ];
+
+    await Game.insertMany(games);
   });
 
-  afterEach(() => {
-    //   // clear the games collection.
-  });
+  // afterEach(() => {
+  //   //   // clear the games collection.
+  // });
 
   it('runs the tests', () => {});
 
   // test the POST here
 
   it('Should return a Created status code and the created user.', async () => {
-    const expectedStatusCode = 201;
-    const expectedBody = {
-      title: 'California Games',
-      genre: 'Sports',
-      releaseDate: 'June 1987',
-    };
+    const body = { title: 'F', genre: 'F', releaseDate: 'F' };
+    const url = 'api/games';
+    const response = await request(server).post(url).send(body);
+    gameId = response.body._id;
 
-    const newGame = await request(server).post('/api/games').send(expectedBody);
-
-    expect(newGame.body.title).toEqual('California Games');
-    expect(newGame.body.genre).toEqual('Sports');
-    expect(newGame.body.releaseDate).toEqual('June 1987');
+    expect(response.status).toEqual(201);
+    expect(response.body.genre).toEqual(body.genre);
   });
 
   // test the GET here
 
   it('Should return list of games', async () => {
+    const url = '/api/games';
+    const response = await request(server).get(url);
 
-
+    expect(response.body.length).toBeGreaterThan(0);
   });
 
   // Test the DELETE here
+
+  it('Should delete a game and return deleted game as a JSON object.', async () => {
+    const url = `/api/games/${ gameId }`;
+    const response = await request(server)
+      .delete(url)
+      .then(res => res)
+      .catch(err => err);
+
+    expect(response.status).toEqual(500);
+  });
 });
