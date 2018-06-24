2001ca1b0547beb575e0f9a7fa3b53b94318e2cd
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-06-23 20:26:21 -0700

    write 2 tests for POST - return correct status code when game is created - return a new game object with an id

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const Game = require('../games/Game');
+const server = require('./server');
+const request = require('supertest');
 
 describe('The API Server', () => {
   beforeAll(() => {
@@ -19,21 +20,59 @@ describe('The API Server', () => {
   });
 
   let gameId;
-  // // hint - these wont be constants because you'll need to override them.
+  // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
-    //   // write a beforeEach hook that will populate your test DB with data
-    //   // each time this hook runs, you should save a document to your db
-    //   // by saving the document you'll be able to use it in each of your `it` blocks
+  beforeEach(async () => {
+      // write a beforeEach hook that will populate your test DB with data
+      // each time this hook runs, you should save a document to your db
+      // by saving the document you'll be able to use it in each of your `it` blocks
+    const californiaGames = {
+      title: 'California Games',
+      genre: 'Sports',
+      releaseDate: 'June 1987'
+    };
+
+    const game = await Game.create(californiaGames);
+    gameId = game._id;
   });
 
   afterEach(() => {
-    //   // clear the games collection.
+      // clear the games collection.
+    return Game.remove();
   });
 
-  it('runs the tests', () => {});
+  describe('POST to /api/games', () => {
+    it('returns status code 201 when a new game is added to the db', async () => {
+      const mineCraft = {
+        title: 'Minecraft',
+        genre: 'sandbox',
+        releaseDate: 'May 2009'
+      };
 
-  // test the POST here
+      const response = await request(server)
+        .post('/api/games')
+        .send(mineCraft);
+      
+      expect(response.status).toBe(201)
+    });
+
+    it('returns a game object with an id, title, genre, and releaseDate', async () => {
+      const WoW = {
+        title: 'World of Warcraft',
+        genre: 'MMORPG',
+        releaseDate: 'November 2004'
+      };
+
+      const response = await request(server)
+        .post('/api/games')
+        .send(WoW);
+
+      expect(response.body._id).toBeTruthy();
+      expect(response.body.title).toBe('World of Warcraft');
+      expect(response.body.genre).toBe('MMORPG');
+      expect(response.body.releaseDate).toBe('November 2004');
+    })
+  });
 
   // test the GET here
 
