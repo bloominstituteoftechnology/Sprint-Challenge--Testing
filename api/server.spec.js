5d1a3739dbb44e62c53a56429274d45bb84b6c8c
Usman Javed <ujaved931@gmail.com>
2018-06-29 22:58:54 +0500

    Finished POST and GET for server.spec

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const Game = require('../games/Game');
 const server = require('./server');
 const request = require('supertest')
 
@@ -22,31 +21,71 @@ describe('The API Server', () => {
   });
 
   let gameId;
-  // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
-    //   // write a beforeEach hook that will populate your test DB with data
-    //   // each time this hook runs, you should save a document to your db
-    //   // by saving the document you'll be able to use it in each of your `it` blocks
+  beforeEach(async ()  => {
     const superMarioOdyssey = { title: 'Super Mario Odyssey', genre: 'Action/Platformer', releaseDate: '29/Dec/2017' }
     const savedGame = await Game.create(superMarioOdyssey);
+    gameId = savedGame._id;
   });
 
   afterEach(() => {
-    //   // clear the games collection.
     return Game.remove();
   });
 
-  describe('post to /api/games', async () = {
-    const geow = {
-      title: 'Gears of War 4',
-      genre: 'Cover Shooter',
-      releaseDate: '03/04/2017'
-    };
-
-    const response = await request(server)
-      .post('api/games')
-      .send(geow)
-
-      expect(response.status).toBe(201)
+  describe('POST to /api/games', () => {
+    it('return 201 status code when game created', async () => {
+      const geow = {
+        title: 'Gears of War 4',
+        genre: 'Cover Shooter',
+        releaseDate: '03/04/2017'
+      };
+          const response = await request(server)
+            .post('/api/games')
+            .send(geow)
+      
+            expect(response.status).toBe(201)
+    })
+    it('returns object with id/title/genre/releaseDate', async () => {
+      const gow4 = {
+        title: 'God of War 4',
+        genre: 'Action/Adventure',
+        releaseDate: '01/05/2018'
+      };
+          const response = await request(server)
+            .post('/api/games')
+            .send(gow4)
+      
+            expect(response.body._id).toBeTruthy()
+            expect(response.body.title).toBe('God of War 4')
+            expect(response.body.genre).toBe('Action/Adventure')
+            expect(response.body.releaseDate).toBe('01/05/2018')
+    })
   })
+
+  describe('GET to /api/games', () => {
+    it('returns 200 status code when games are accessed', async () => {
+      const response = await request(server).get('/api/games');
+      
+      expect(response.status).toBe(200);
+    })
+    it("returns 200 status code when games can't be accessed", async () => {
+      
+      const response = await request(server).get('/api/games/' + gameId);
+      
+      
+      expect(response.body.title).toBe('Super Mario Odyssey')
+      expect(response.body.genre).toBe('Action/Platformer')
+      expect(response.body.releaseDate).toBe('29/Dec/2017')
+    })
+    
+    it('returns status code 404 when no game found', async () => {
+      const response = await request(server).get('/api/games/1');
+
+      expect(response.status).toEqual(404);
+    })
+  })
+
+  describe('DELETE to /api/games', () => {
+    it('returns ')
+  })
+})
