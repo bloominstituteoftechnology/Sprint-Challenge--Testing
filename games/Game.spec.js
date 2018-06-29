a846bc2e71499bebb5d32e9aef6b709b29338809
FireInjun <mk@fireinjun.com>
2018-06-29 14:06:58 -0400

    modified game test

--- games/Game.spec.js
+++ games/Game.spec.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-
+const server = require('../api/server');
 const Game = require('./Game');
+const tester = require('supertest');
 
 describe('The Game Model', () => {
+  let g1;
+  let g2;
+
   beforeAll(() => {
     return mongoose
       .connect('mongodb://localhost/test')
@@ -12,13 +16,37 @@ describe('The Game Model', () => {
       });
   });
 
+  beforeEach((() => {
+    g1 = {
+      title: 'Moises',
+      genre: 'Hacker',
+      releaseDate: 'unknown'
+    },
+      gamer2 = {
+      title: 'Myke',
+      genre: 'N00b',
+      releaseDate: 'December 1989'
+      }
+  }))
+
+  afterEach(() => {
+    return Game.remove();
+  })
+
   afterAll(() => {
     return mongoose
       .disconnect()
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  it('runs the tests', () => {});
+  it('runs the tests', async () => {
 
-  // test away!
+    // test away!
+    const player = await tester(server).post('/api/games').send(g1);
+    expect(player.status).toEqual(201);
+    expect(player.body.title).toEqual('Moises');
+    // expect(res.data[0].Game).toEqual(Game.title);
+
+
+  });
 });
