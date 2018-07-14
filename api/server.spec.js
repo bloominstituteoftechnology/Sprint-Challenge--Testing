04a492a54b95d2ba887f4de3b8e4a44e08aad14e
unknown <jesse.reichel@live.com>
2018-07-13 20:48:44 -0700

    progress

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const request = require('supertest');
 
-const Game = require('./games/Game');
+const Game = require('../games/Game');
+const server = require('./server');
 
 describe('The API Server', () => {
   beforeAll(() => {
@@ -18,22 +20,92 @@ describe('The API Server', () => {
       .then(() => console.log('\n=== disconnected from TEST DB ==='));
   });
 
-  let gameId;
+  let gameId = 'test Id'
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
+  beforeEach((done,gameId) => {
+
+    const newGame = { 
+      title: 'Final Fantasy',
+      genre: 'RPG',
+      releaseDate: 'before games were cool' }
+
+    request(server)
+      .post('/api/games')
+      .send(newGame)
+      .end(function (err, res) {
+        if(err) return done(err);
+        let gameId = res.body._id
+        done()
+      })
+
     //   // write a beforeEach hook that will populate your test DB with data
     //   // each time this hook runs, you should save a document to your db
     //   // by saving the document you'll be able to use it in each of your `it` blocks
   });
 
   afterEach(() => {
+    return Game.remove()
     //   // clear the games collection.
   });
 
   it('runs the tests', () => {});
 
-  // test the POST here
+  it('successfully returns posted status', (done) => {
+    const newGame = { 
+      title: 'Final Fantasy',
+      genre: 'RPG',
+      releaseDate: 'before games were cool' }
+      console.log(gameId)
+
+    request(server)
+      .post('/api/games')
+      .send(newGame)
+      .end(function (err, res) {
+        if(err) return done(err);
+        expect(res.status).toBe(201)
+        done()
+      })
+  })
+
+  it('successfully returns games db', (done, newGame) => {
+    request(server)
+      .get('/api/games')
+      .end(function (err, res) {
+        if(err) return done(err);
+        expect(res.status).toBe(200)
+        expect(res.json).toEqual(newGame)
+        done()
+      })
+  })
+
+  // it('successfully returns games db', (done, gameId) => {
+  //   let gameIdTest = 12
+  //   expect(gameId).toBe(gameIdTest)
+  // })
+
+
+  // it('successfully deletes posted game', (done, gameId) => {
+  //  let gameIdTest = 12
+    
+    // const newGame = { 
+    //   title: 'Final Fantasy',
+    //   genre: 'RPG',
+    //   releaseDate: 'before games were cool' }
+      // expect(gameIdTest).toBe(gameId)
+    // request(server)
+    //   .post('/api/games')
+    //   .send(newGame)
+    //   .end(function (err, res) {
+    //     if(err) return done(err);
+    //     done()
+    //   })
+
+    // request(server)
+    // .del('/api/games')
+  // })
+
+
 
   // test the GET here
 
