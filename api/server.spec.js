0aa1faaca3359a2a03b80de103d041ac228beff4
Alexander <alexander.botello@g.austincc.edu>
2018-07-14 12:28:27 -0500

    Add tests for GET/POST endpoints

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,12 +1,13 @@
+const request = require('supertest');
+const server = require('./server');
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const Game = require('../games/Game');
 
 describe('The API Server', () => {
   beforeAll(() => {
     return mongoose
       .connect('mongodb://localhost/test')
-      .then(() => console.log('\n=== connected to TEST DB ==='))
+      .then(() => {})
       .catch(err => {
         console.log('error connecting to TEST database, is MongoDB running?');
       });
@@ -21,19 +22,64 @@ describe('The API Server', () => {
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
-    //   // write a beforeEach hook that will populate your test DB with data
-    //   // each time this hook runs, you should save a document to your db
-    //   // by saving the document you'll be able to use it in each of your `it` blocks
+  beforeEach(async () => {
+    await Game.create({
+      title: 'Hollow Knight',
+      genre: 'Metroidvania',
+      releaseDate: 'June 2018',
+    });
+
+    await Game.create({
+      title: 'Ocotopath Traveler',
+      genre: 'JRPG',
+      releaseDate: 'July 2018',
+    })
   });
 
-  afterEach(() => {
-    //   // clear the games collection.
+  afterEach(async () => {
+    await Game.remove();
   });
 
   it('runs the tests', () => {});
 
-  // test the POST here
+  it('GET endpoint successfully returns 200', async () => {
+    await request(server)
+      .get('/api/games')
+      .expect(200)
+  })
+
+  it('GET endpoint should return a list of games', async () => {
+    const response = await request(server).get('/api/games')
+    const expected = {
+      title: 'Hollow Knight',
+      genre: 'Metroidvania',
+      releaseDate: 'June 2018',
+    }
+    expect(response.body).toHaveLength(2);
+    expect(response.body[0]).toMatchObject(expected);
+  })
+
+  it('POST endpoint should successfully return 201', async () => {
+    const newGame = {
+      title: 'Pokemon',
+      genre: 'JRPG',
+      releaseDate: 'i dont remember lol',
+    };
+    await request(server)
+      .post('/api/games')
+      .send(newGame)
+      .expect(201);
+  })
+
+  it('POST endpoint should return 500 if invalid data is given', async () => {
+    const newGame = {
+      releaseDate: 'July 2018'
+    }
+    await request(server)
+      .post('/api/games')
+      .send(newGame)
+      .expect(500)
+  })
 
   // test the GET here
 
