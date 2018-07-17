3aa2d92cc96ba6cb550b91f1c168246c1eb5755d
Charlie <ccariste@gmail.com>
2018-07-16 17:29:17 -0700

    CRC: passing initial tests for server

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-const Game = require('./games/Game');
+const request = require('supertest');
+const server = require('./server');
+const Game = require('../games/Game');
 
 describe('The API Server', () => {
   beforeAll(() => {
@@ -21,21 +22,66 @@ describe('The API Server', () => {
   let gameId;
   // // hint - these wont be constants because you'll need to override them.
 
-  beforeEach(() => {
-    //   // write a beforeEach hook that will populate your test DB with data
-    //   // each time this hook runs, you should save a document to your db
-    //   // by saving the document you'll be able to use it in each of your `it` blocks
+  beforeEach(async () => {
+    await Game.create({
+      title: 'Mario',
+      genre: 'Adventure',
+      releaseDate: 'May 1980'
+    });
+
+    await Game.create({
+      title: 'PacMan',
+      genre: 'Arcade',
+      releaseDate: 'June 1981'
+    })
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     //   // clear the games collection.
+    await Game.remove();
+
   });
 
-  it('runs the tests', () => {});
 
-  // test the POST here
+  it('has a GET / endpoint', async () => {
+    await request(server)
+        .get('/api/games')
+        .expect(200)
+  })
 
-  // test the GET here
+  it('has a GET / endpoint that returns 200', async () => {
+      await request(server)
+          .get('/api/games')
+          .expect(200)
+  })
 
-  // Test the DELETE here
+  it('returns a list of games', async () => {
+      const response = await request(server).get('/api/games')
+      const expected = {
+        title: 'Mario',
+        genre: 'Adventure',
+        releaseDate: 'May 1980'
+      };
+      expect(response.body).toHaveLength(2);
+      expect(response.body[0]).toMatchObject(expected);
+  })
+
+  // it('should return user data after successfully saved to db',
+  //     async () => {
+  //         console.log('TEST', mario)
+  //         await request(server).post('/api/games/', mario)
+  //             .expect(201);
+  //     })
+
+// it('should return status 202 after successfully deleting from db',
+//     async () => {
+//         // const testUserData = {
+//         //     username: 'Lisa',
+//         //     password: '123'
+//         // }
+//         console.log('TEST', testUserData)
+//         await request(server).delete('/', testUserData)
+//             .expect(202);
+// })
+
 });
