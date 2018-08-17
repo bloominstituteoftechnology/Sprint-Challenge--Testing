b36dfb48e6e52ac7dc2804bc2fdee8f4c0995187
DirupT <iamcooled@gmail.com>
2018-08-17 10:38:22 -0500

    added tests for POST

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
 
 server.get('/games', (req, res) => {
     res.status(200).json([]);
+});
+
+server.post('/games', (req, res) => {
+    const { title, genre, releaseYear } = req.body;
+    if (!title || !genre) return res.status(422).json({ error: 'Please fill in the required fields' });
+    res.status(201).json({ title, genre, releaseYear });
 })
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,11 +2,35 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('server.js endpoints', () => {
-    describe('GET /', () => {
-        it('should return status code 200, JSON', async () => {
+    describe('GET /games', () => {
+        it('should return status code 200 and an empty array', async () => {
             await request(server)
                 .get('/games')
                 .expect(200, []);
         });
-    })
-})
+    });
+
+    describe('POST /games', () => {
+        it('should return status code 201, JSON, and `title, body` if releaseYear isnt sent', async () => {
+            await request(server)
+                .post('/games')
+                .send({ title: 'A wise fairy', genre: 'Fiction' })
+                .expect('Content-Type', /json/)
+                .expect(201, { title: 'A wise fairy', genre: 'Fiction' });
+        });
+
+        it('should return status code 201, JSON, and `title, body, releaseYear`', async () => {
+            await request(server)
+                .post('/games')
+                .send({ title: 'A wise fairy', genre: 'Fiction', releaseYear: 1999 })
+                .expect('Content-Type', /json/)
+                .expect(201, { title: 'A wise fairy', genre: 'Fiction', releaseYear: 1999 });
+        });
+
+        it('should return status code 422 if title and body are not provided', async () => {
+            await request(server)
+                .post('/games')
+                .expect(422);
+        });
+    });
+});
