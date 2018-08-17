f453e9dd5a3c568392221521595442222e006d15
emaquino44 <prexy44@yahoo.com>
2018-08-17 10:02:03 -0700

    Wrrote tests and created endpoint for GET'/games

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const server = express();
 
 server.use(express.json());
 
+	const games = [{
+		title: 'Pacman',
+		genre: 'Arcade',
+		releaseYear: 1980
+	}];
 
 server.get('/', (req, res) => {
   res.status(200).send('API is running');
 });
 
+server.get('/games', (req, res) => {
+  res.status(200).json(games);
+});
+
 
 module.exports=server;
+
+
+
--- server.spec.js
+++ server.spec.js
@@ -10,4 +10,25 @@ describe('server.js', ()=> {
 				expect(response.status).toEqual(expected);
 				});
 			});
+
+	describe('GET endpoint(/games)', ()=> {
+			it('should return status code 200 Ok', async ()=> {
+				const expected = 200;
+				const response = await request(server).get('/games');
+				expect(response.status).toEqual(expected);
+				});
+			it('should return an array of games', async ()=> {
+				const expected = [{
+							title: 'Pacman',
+							genre: 'Arcade',
+							releaseYear: 1980
+						}];
+				const response = await request(server).get('/games');
+				expect(response.body).toEqual(expected);
+				});
+			});
+
+
+
+
 	});
