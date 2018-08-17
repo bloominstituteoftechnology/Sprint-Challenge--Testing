52964411961b407b4df86500758e0f36fb829d38
LBWright <lwright.js@gmail.com>
2018-08-17 11:56:48 -0500

    successfully added tests for the DELETE route

--- index.js
+++ index.js
@@ -50,4 +50,20 @@ server.post('/games', (req, res) => {
   res.status(201).json(games);
 });
 
+server.delete('/games/:id', (req, res) => {
+  const { id } = req.params;
+
+  const toDelete = games.filter(game => game.id === id);
+  if (toDelete.length === 0) {
+    res.status(404).json({ error: 'Game not found' });
+  }
+
+  games = games.filter(game => {
+    if (game.id !== id) {
+      return true;
+    } else return false;
+  });
+  res.status(200).json(games);
+});
+
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -3,8 +3,9 @@ const server = require('./index');
 describe('server', () => {
   describe('GET /games', () => {
     it('should return a 200 status code', async () => {
-      const res = await request(server).get('/games');
-      expect(res.status).toEqual(200);
+      const res = await request(server)
+        .get('/games')
+        .expect(200);
     });
     it('returned response should be type JSON', async () => {
       const res = await request(server).get('/games');
@@ -37,8 +38,9 @@ describe('server', () => {
   });
   describe('GET /games/:id', () => {
     it('should return a status code of 200', async () => {
-      const res = await request(server).get('/games/2');
-      expect(res.status).toEqual(200);
+      const res = await request(server)
+        .get('/games/2')
+        .expect(200);
     });
     it('should respond with a type of JSON', async () => {
       const res = await request(server).get('/games/2');
@@ -109,6 +111,52 @@ describe('server', () => {
         .post('/games')
         .send(game);
       expect(res.body[res.body.length - 1]).toEqual(expected);
+    });
+  });
+  describe('/DELETE/:id', () => {
+    it('should respond with a status code of 200', async () => {
+      const res = await request(server)
+        .del('/games/1')
+        .expect(200);
+    });
+    it('should respond with an array of all games', async () => {
+      const res = await request(server).del('/games/2');
+      expect(res.body).toEqual([
+        {
+          genre: 'First Person Shooter',
+          id: '3',
+          releaseYear: 2003,
+          title: 'Halo'
+        },
+        { genre: 'MMORPG', id: 4, releaseYear: 2018, title: 'Monster Hunter' },
+        {
+          genre: 'RPG',
+          id: 5,
+          releaseYear: 2005,
+          title: 'Knights of the Old Republic'
+        },
+        { genre: 'Game Genre', id: 6, releaseYear: 2018, title: 'Game Title' }
+      ]);
+    });
+
+    it('should successfully remove an item from the data', async () => {
+      const res = await request(server).del('/games/3');
+      expect(res.body).toEqual([
+        { genre: 'MMORPG', id: 4, releaseYear: 2018, title: 'Monster Hunter' },
+        {
+          genre: 'RPG',
+          id: 5,
+          releaseYear: 2005,
+          title: 'Knights of the Old Republic'
+        },
+        { genre: 'Game Genre', id: 6, releaseYear: 2018, title: 'Game Title' }
+      ]);
+    });
+
+    it('should respond with a status of 404 if not found', async () => {
+      const res = await request(server)
+        .delete('/games/10')
+        .expect(404);
     });
   });
 });
