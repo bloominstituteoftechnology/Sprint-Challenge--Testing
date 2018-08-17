3160071457f00b647978d8d1bfbfef43f806d1e9
Wdifulvio523 <w.difulvio523@gmail.com>
2018-08-17 11:39:21 -0400

    Created three tests for GET /games and created the endpoints to make the tests pass

--- server.js
+++ server.js
@@ -3,15 +3,42 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
-const db = {games: ['Monopoly', 'Risk', 'Clue', 'Life', 'Trivia Pursuit', 'Sorry!']}
+const db = {
+  games: [
+    {
+      title: "Monopoly",
+      genre: "board",
+      releaseYear: 1945
+    },
+    {
+      title: "Risk",
+      genre: "board",
+      releaseYear: 1955
+    },
+    {
+      title: "Clue",
+      genre: "board",
+      releaseYear: 1925
+    },
+    {
+      title: "Life",
+      genre: "board",
+      releaseYear: 1985
+    }
+  ]
+};
 
 server.get("/", (req, res) => {
-    res.status(200).json({ api: "running" });
+  res.status(200).json({ api: "running" });
+});
+
+server.get("/games", (req, res) => {
+    res.status(200).json(db.games);
   });
 
 
 
 
+  
 
-
-  module.exports = { server, db };
+module.exports = { server, db };
--- server.spec.js
+++ server.spec.js
@@ -1,25 +1,44 @@
 const request = require("supertest");
 
-const {server, db} = require("./server.js");
+const { server, db } = require("./server.js");
 
-describe('server.js', () => {
-    describe('root endpoint(/)', () => {
-        it('should return status code 200 OK', async () => {
-            const expected = 200;
-            const res = await request(server).get('/');
-            expect(res.status).toBe(expected)
-        })
+describe("server.js", () => {
+  describe("root endpoint(/)", () => {
+    it("should return status code 200 OK", async () => {
+      const expected = 200;
+      const res = await request(server).get("/");
+      expect(res.status).toBe(expected);
+    });
 
-        it("should return JSON", async () => {
-            const res = await request(server).get("/");
-            expect(res.type).toEqual("application/json");
-          });
-      
-          it("should return object that looks like {api:running} ", async () => {
-            const expected = { api: "running" };
-            const res = await request(server).get("/");
-            expect(res.body).toEqual(expected);
-          });
+    it("should return JSON", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toEqual("application/json");
+    });
+
+    it("should return object that looks like {api:running} ", async () => {
+      const expected = { api: "running" };
+      const res = await request(server).get("/");
+      expect(res.body).toEqual(expected);
+    });
+  });
+
+  describe('GET endpoint (/games)', () => {
+
+    it('should return status code 200 OK', async () => {
+        const expected = 200;
+        const res = await request(server).get("/games");
+        expect(res.status).toEqual(expected);
     })
-})
 
+    it("should return JSON", async () => {
+        const res = await request(server).get("/games");
+        expect(res.type).toEqual("application/json");
+      });
+  
+      it("should return object that looks like expected ", async () => {
+        const expected = db.games;
+        const res = await request(server).get("/games");
+        expect(res.body).toEqual(expected);
+      });
+  })
+});
