563e0cfd1096097fb2f4ae94823d118d2bd8744d
jeffyjkang <jeffyjkang@gmail.com>
2018-08-17 09:28:56 -0700

    mvp

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ const server = express();
 
 server.use(express.json());
 
+let db = [
+  { title: "crossword", genre: "casual", releaseYear: "1920" },
+  { game: "bingo", genere: "party", releaseYear: "1910" },
+  { game: "chess", genre: "strategy", releaseYear: "1" }
+];
+
 server.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
 });
+
+server.get("/games", (req, res) => {
+  res.status(200).json(db);
+});
+
+server.post("/games", (req, res) => {
+  const { game } = req.body;
+  if (!game) {
+    res.status(422).json({ message: "need game" });
+  }
+  res.status(201).json({ game });
+});
+
+module.exports = { server, db };
--- server.spec.js
+++ server.spec.js
@@ -1,17 +1,63 @@
 const request = require("supertest");
 
-const server = require("./server.js");
+const { server, db } = require("./server.js");
 
 describe("server.js", () => {
   describe("root endpoint (/)", () => {
     it("should return status code 200 OK", async () => {
       const expected = 200;
-      //   request(server)
       const response = await request(server).get("/");
-      // .then(response => {
-      //checks
       expect(response.status).toEqual(expected);
-      // });
+    });
+  });
+  describe("endpoint get (/games)", () => {
+    it("should return status code 200 ok", async () => {
+      const expected = 200;
+      const response = await request(server).get("/games");
+      expect(response.status).toEqual(expected);
+    });
+    it("should return array of games", async () => {
+      const expected = db;
+      const response = await request(server).get("/games");
+      expect(response.body).toEqual(expected);
+    });
+    it("should return empty array if no games", async () => {
+      const expected = [];
+      const response = await request(server).get("/games");
+      expect(response.body).toEqual(expected);
+    });
+  });
+  describe("endpoint post (/games)", () => {
+    it("should return error code 422 if no game", async () => {
+      const expected = 422;
+      const response = await request(server)
+        .post("/games")
+        .send({ game: "" });
+      expect(response.status).toEqual(expected);
+    });
+    it("should return status code 201 created game", async () => {
+      const expected = 201;
+      const response = await request(server)
+        .post("/games")
+        .send({
+          game: { title: "checkers", genre: "casual", releaseYear: "1920" }
+        });
+      expect(response.status).toEqual(expected);
+    });
+    it("should return game", async () => {
+      const expected = {
+        game: {
+          title: "checkers",
+          genre: "casual",
+          releaseYear: "1920"
+        }
+      };
+      const response = await request(server)
+        .post("/games")
+        .send({
+          game: { title: "checkers", genre: "casual", releaseYear: "1920" }
+        });
+      expect(response.body).toEqual(expected);
     });
   });
 });
