9216591a884341e7f64d2aca6a60f0f817a54aff
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-17 10:20:55 -0600

    Wrote three tests for GET to /games

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
 
+const db = [
+  {title: 'Pacman', genre: 'Arcade', releaseYear: 1980},
+  {title: 'Monopoly', genre: 'Board', releaseYear: 1935},
+  {title: 'Tetris', genre: 'Arcade', releaseYear: 1984}
+];
+
 server.use(express.json());
 
 // Endpoints go here
--- server.spec.js
+++ server.spec.js
@@ -1,12 +1,6 @@
 const request = require('supertest');
 const server = require('./server.js');
 
-const db = [
-  {title: 'Pacman', genre: 'Arcade', releaseYear: 1980},
-  {title: 'Monopoly', genre: 'Board', releaseYear: 1935},
-  {title: 'Tetris', genre: 'Arcade', releaseYear: 1984}
-];
-
 describe('server.js', () => {
   describe('root endpoint', () => {
     test('should return status code 200', async () => {
@@ -50,6 +44,26 @@ describe('server.js', () => {
       const response = await request(server)
         .post('/games')
         .send({title: 'Pacman', genre: 'Arcade', releaseYear: 1980})
+      expect(response.body).toEqual(expected);
+    })
+  })
+  describe('GET to /games', () => {
+    test('should return status code 200', async () => {
+      const expected = 200;
+      const response = await request(server).get('/games');
+      expect(response.status).toEqual(expected);
+    })
+    test('should return json', async () => {
+      const response = await request(server).get('/games');
+      expect(response.type).toEqual('application/json');
+    })
+    test('should return list of games', async () => {
+      const expected = [
+        {title: 'Pacman', genre: 'Arcade', releaseYear: 1980},
+        {title: 'Monopoly', genre: 'Board', releaseYear: 1935},
+        {title: 'Tetris', genre: 'Arcade', releaseYear: 1984}
+      ]
+      const response = await request(server).get('/games');
       expect(response.body).toEqual(expected);
     })
   })
