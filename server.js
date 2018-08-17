e81c4b730a8a70a98a4dbae449ba65f15378f70d
Joe Stanfield <jlstan06@hotmail.com>
2018-08-17 12:14:00 -0400

    added constant for games with an array

--- server.js
+++ server.js
@@ -4,14 +4,37 @@ const server = express();
 
 server.use(express.json())
 
+let games = [
+    {
+        id:1, 
+        title: 'Madden', 
+        genre: 'Sports'
+    },
+    {
+        id:2,
+        title: 'NBA 2K',
+        genre: 'Sports'
+    },
+    {
+        id:3,
+        title: 'Dead by Daylight',
+        releaseYear: 2017
+    }
+];
+
+let gameId = 4;
+
 server.get('/games', (req, res) => {
-    res.status(200).json([]);
+    res.status(200).json(games);
 })
 
 server.post('/games', (req, res) => {
     const { title, genre, releaseYear } = req.body;
+    const game = { id:gameId, title, genre, releaseYear }
     if (!title || !genre) return res.status(422).json({error:'I need you to fill in the required fields please' });
-    res.status(201).json({title, genre, releaseYear });
+    games.push(game)
+    gameId++;
+    res.status(201).json({game});
 })
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -6,22 +6,22 @@ const server  = require('./server');
 
 describe('server.js endpoints', () => {
     describe('GET /games', () => {
-        test('should return status code 200, JSON', async () => { 
+        test('should return status code 200 and games', async () => { 
     
         await request(server)
             .get('/games')
-            .expect(200, []);
+            .expect(200);
         })
     })
 
 
 describe('POST /games' , () => {
-    test('should return status code 201, JSON, and title body if releaseYear isnt sent', async () => {
+    test('should return status code 201 and JSON', async () => {
         await request(server)
             .post('/games')
-            .send({title:'Madden', genre: 'Sports'})
+            .send({title:'Fortnite', genre: 'Action'})
             .expect('Content-Type', /json/)
-            .expect(201, {title: 'Madden', genre: 'Sports'})
+            .expect(201)
     })
 
     test('should return status code 201, JSON, and title, body, releaseYear', async () => {
