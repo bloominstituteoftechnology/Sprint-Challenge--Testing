7fec0196d4460ffcf7ff3c38614fa43ddd2409b0
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-17 09:34:12 -0700

    Build tests for GET '/games'

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -21,6 +21,32 @@ describe('server.js', () => {
             const response = await request(server).get('/')
             expect(response.body).toEqual(expected)
         })
-        
+    })
+
+    describe('GET /games', async () => {
+        it('returns a status code of 200', async () => {
+            const expected = 201
+            const response = await request(server).get('/games')
+
+            expect(response.status).toEqual(expected)
+        })
+
+        it('returns an array', async () => {
+            const response = await request(server).get('/games')
+
+            expect(response.type).arrayContaining([])
+        })
+
+        it('If there is at least one game, check that it contains a title', async () => {
+            const response = await request(server).get('/games')
+            const games = response.body
+
+            if(games.length > 0){
+                expect(games[0].title).anything()
+            }
+            else(
+                expect(response.type).arrayContaining([])
+            )
+        })
     })
 })
