b667dcd4104bee4626dc06bac708dffb1fe9124f
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-17 12:47:10 -0400

    MVP Done

--- server.js
+++ server.js
@@ -4,14 +4,41 @@ const server = express();
 
 server.use(express.json());
 
-const games = [{
+const games = [
+  {
     title: 'Pacman',
     genre: 'Arcade',
     releaseYear: 1980
-}]
+  },
+  {
+    title: 'Marvel vs Capcom',
+    genre: 'Arcade',
+    releaseYear: 1985
+  },
+  {
+    title: 'Starship',
+    genre: 'Arcade',
+    releaseYear: 1970
+  },
+]
 
-server.get('/api/games', (req, res) => {
-  res.status(200).json([]);
+server.get('/', (req, res) => {
+  res.status(200).send({ api: "running" });
+});
+
+server.get('/games', (req, res) => {
+  res.status(200).send(games);
+});
+
+server.post("/games", async (req, res) => {
+  if (!req.body.title || !req.body.genre) {
+    return res.status(422).json({
+      errorMessage: "Required fields are incomplete."
+    });
+  } else {
+    const game = await req.body;
+    return res.status(200).json(game);
+  }
 });
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,52 +2,47 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
+game = {
+  title: "Pacman",
+  genre: "Arcade", 
+  releaseYear: 1980 
+};
+
 describe('server.js', () => {
-  describe('GET /api/games', async () => {
+  // GET
+  describe('GET /games', () => {
     it('should return status code 200 OK', async () => {
       const expected = 200;
 
-      const response = await request(server).get('/api/games');
+      const response = await request(server).get('/games');
 
       expect(response.status).toEqual(expected);
     });
 
-    // GET
-
     it('should return a list of games', async () => {
-      const expected = server.games;
+      const expected = true;
+      const response = await request(server).get('/games')
+      const actual = Array.isArray(response.body)
 
-      const res = await request(server).get('/api/games')
+      expect(actual).toEqual(expected);
+    });
+  })
+  // POST
+  describe('POST /games', () => {
+    it('should return a 422 code if the information is incomplete', async () => {
+      const expected = 422;
+      const response = await request(server).post('/games').send({title: false, genre: false,  releaseYear: 1980});
+      const actual = response.status;
 
-      expect(res.body).toEqual(expected);
+      expect(actual).toEqual(expected);
     });
 
-    it('should return an empty list if there are no games in the server', async () => {
-      const expected = [];
+    it('should return a 200 code if the information is complete', async () => {
+      const expected = 200;
 
-      const res = await request(server).get('/api/games')
+      const response = await request(server).post('/games').send({title: "Pacman", genre: "Arcade",  releaseYear: 1980});
 
-      expect(res.body).toEqual(expected);
-    });
-
-    // POST
-
-    describe('POST /api/games'), () => {
-      it('should return a 422 code if the information is incomplete', async () => {
-        const expected = 422;
-
-        const response = await request(server).post('/api/games').send({title: "", genre: "Arcade",  releaseYear: 1980});
-
-        expect(response.status).toEqual(expected);
-      });
-
-      it('should return a 200 code if the information is complete', async () => {
-        const expected = 200;
-
-        const response = await request(server).post('/api/games').send({title: "Pacman", genre: "Arcade",  releaseYear: 1980});
-
-        expect(response.status).toEqual(expected);
-      })
-    }
+      expect(response.status).toEqual(expected);
+    })
   })
 })
