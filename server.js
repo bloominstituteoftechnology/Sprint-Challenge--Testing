25529bc106ac9e6f2b63634980f5a127bc2471b1
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-17 09:09:46 -0700

    created games array

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ const server = express();
 
 server.use(express.json());
 
+let games = [];
+
 server.get('/', (req, res) => {
     res.status(200).json({ success: true, data: { api: 'running' } });
 });
 server.get('/games', (req, res) => {
-    res.status(200).json([]);
+    res.status(200).json(games);
 });
 
 server.post('/games', (req, res) => {
     const { title, genre, releaseYear } = req.body;
-    console.log('name', name)
     if (!title || !genre) return res.status(422).json({ error: 'Please, provide both a title and genre.' })
+    games.push({ title, genre, releaseYear })
     res.status(201).json({ title: title, genre: genre, releaseYear: releaseYear })
+
 });
 
 module.exports = server;
