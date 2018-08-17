36bdd46ab130f55fc172c59774f87462ed8b3ed1
Ari Mercado <ari@Apples-Mac-3.local>
2018-08-17 14:49:39 -0700

    added some logic in the server

--- server.spec.js
+++ server.spec.js
@@ -9,10 +9,29 @@ describe(' server ', () => {
     })
     it('should response with status code 200', async () => {
       const response = await request(server).get('/games')
+      //console.log(response.body)
       expect(response.status).toEqual(200);
     });
   });
 
+  describe('POST /games', () => {
+    it('should respond type JSON', async () => {
+      const res = await request(server).post('/games')
+      expect(res.type).toEqual('application/json');
+    })
 
-   
+    it('should response with status code 201', async () => {
+      const res = await request(server).post('/games')
+      expect(res.status).toEqual(201);
+    })
+
+    it('should return {id, title, genre} in response body', async () => {
+      const expected = {id:'4', title: 'game4', genre: 'shooter'}
+      const res = await request(server)
+        .post('/games')
+        .send({id:'4', title: 'game4', genre: 'shooter'})
+      expect(res.body).toEqual(expected);
+    })
+
+  })    
 });
