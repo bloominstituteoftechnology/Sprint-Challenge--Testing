82ad8f0168e98143c16636c2540a3bf2c614b6fb
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-17 10:17:39 -0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const helmet = require('helmet')
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet())
+
+
+module.exports = server;
