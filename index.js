81567b6f02625721faff734d1e7de4a818a4dfc1
Vu Cao <cdhvu@yahoo.com>
2018-08-17 10:43:30 -0700

    Added test for GET /games.

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ server.use(express.json());
 const games = []
 
 server.get('/games', async (req, res) => {
-    res.status(200).send('api running');
+    res.status(200).json(games);
 })
 
 server.post('/games', async(req, res) => {
     if (!(req.body.title && req.body.genre)) {
-        res.status(422).json({message: 'Failed to add game to database.', error: 'Missing game title and genre.'})
+        return res.status(422).json({message: 'Failed to add game to database.', error: 'Missing game title and genre.'})
     }
 
     try {
         const game = req.body;
         games.push(game);
         res.status(201).json(games);
-    } catch (error) {
+    } 
+    catch (error) {
         res.status(500).json({message: 'Failed to add game to database.', err: error.message})
     }
-    
 })
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -2,6 +2,42 @@ const request = require('supertest');
 const server = require('./index.js');
 
 describe('server test(index.js)', () => {
+    describe('GET endpoint /games', () => {
+        it('it should return a status code 200 OK', async () => {
+            const expected = 200;
+            const response = await request(server)
+                .get('/games');
+            expect(response.status).toEqual(expected);
+        })
+
+        it('it should return an empty list of game when there is no game', async () => {
+            expected = []
+            const response = await request(server)
+                .get('/games');
+            expect(response.body).toEqual(expected);
+        })
+
+        it('it should return a list of game when there are games', async () => {
+            await request(server)
+                .post('/games')
+                .send({
+                    title: 'Pacman', 
+                    genre: 'Arcade', 
+                    releaseYear: 1980 
+                  });
+
+            const expected = [{
+                title: 'Pacman', 
+                genre: 'Arcade', 
+                releaseYear: 1980 
+              }]
+              
+            const response = await request(server)
+                .get('/games');
+            expect(response.body).toEqual(expected);
+        })
+    })
+
     describe('POST endpoint /games', () => {
         it('it should return a status code 201 CREATED if the request body is valid.', async () => {
             const expected = 201;
@@ -24,20 +60,5 @@ describe('server test(index.js)', () => {
                 });
             expect(response.status).toEqual(expected);
         });
-    })
-
-    describe('GET endpoint /games', () => {
-        it('it should return a status code 200 OK', async () => {
-            const expected = 200;
-            const response = await request(server)
-                                    .get('/games');
-            expect(response.status).toEqual(expected);
-        })
-
-        it('it should return a list of game', async () => {
-            const response = await request(server)
-                                    .get('/games');
-            expect(typeof response.body).toEqual('number');
-        })
     })
 })
