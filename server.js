38097bac8ffbe5ea0b40c34d3cae343b36427a1d
Sukhada Gholba <sukhadagholba@gmail.com>
2018-08-17 08:15:44 -0700

    created server file to address GET request, added test file to check for correct status code response of 200

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+
+const games =[
+{
+  title: 'Pacman', // required
+  genre: 'Arcade', // required
+  releaseYear: 1980 // not required
+}
+
+
+];
+
+
+
+server.get('/', (req, res) => {
+
+	res.status(200).json(games);
+});
+
+
+
+
+
+
+
+
+
+module.exports = server;
---
+++ server.spec.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server.js', () => {
+
+describe('server testing for get request', () => {
+
+it('should return status code 200 OK', async() => {
+                const expected =200;
+
+                const response = await request(server).get('/');
+                expect(response.status).toEqual(expected);
+
+});
+});
+
+});
