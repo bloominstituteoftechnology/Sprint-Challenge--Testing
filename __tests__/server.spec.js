75fea35391be73c1974c0c41f317dfbe2af748e6
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-17 10:09:02 -0700

    Add initial tests for POST /games

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -23,30 +23,79 @@ describe('server.js', () => {
         })
     })
 
-    describe('GET /games', async () => {
+    describe('GET /games', () => {
         it('returns a status code of 200', async () => {
-            const expected = 201
+            const expected = 200
             const response = await request(server).get('/games')
 
             expect(response.status).toEqual(expected)
         })
 
         it('returns an array', async () => {
+            const expected = []
             const response = await request(server).get('/games')
 
-            expect(response.type).arrayContaining([])
+            expect(response.type).toEqual(expect.arrayContaining(expected))
         })
 
         it('If there is at least one game, check that it contains a title', async () => {
+            const expected = []
             const response = await request(server).get('/games')
             const games = response.body
 
             if(games.length > 0){
-                expect(games[0].title).anything()
+                expect(games[0].title).toEqual(expect.anything())
             }
             else(
-                expect(response.type).arrayContaining([])
+                expect(response.type).toEqual(expect.arrayContaining(expected))
             )
         })
+
+        it('If there is at least one game, check that it contains a genre', async () => {
+            const expected = []
+            const response = await request(server).get('/games')
+            const games = response.body
+
+            if(games.length > 0){
+                expect(games[0].genre).toEqual(expect.anything())
+            }
+            else(
+                expect(response.type).toEqual(expect.arrayContaining(expected))
+            )
+        })
+    })
+
+    describe('POST /games', () => {
+        it('should return JSON', async () => {
+            const expected = 'application/json'
+            const response = await request(server).post('/games')
+            expect(response.type).toEqual(expected)
+        })
+
+        it('should return 422 when no information is sent', async () => {
+            const expected = 422
+            const response = await request(server).post('/games')
+            expect(response.status).toEqual(expected)
+        })
+
+        it('should return an error message when no title is sent', async () => {
+            const expected = { message: "Please include a valid title and genre" }
+
+            const noTitle = { genre: "first person shooter"}
+
+            const response = await request(server).post('/games').send(noTitle)
+            expect(response.body).toEqual(expected)
+        })
+
+        it('should return an error message when no genre is sent', async () => {
+            const expected = { message: "Please include a valid title and genre" }
+
+            const noGenre = { title: "Doom"}
+
+            const response = await request(server).post('/games').send(noGenre)
+            expect(response.body).toEqual(expected)
+        })
+
+        
     })
 })
--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const server = express()
 
 server.use(express.json())
 
+let games = [
+    {
+        id: 1,
+        title: 'Pacman', 
+        genre: 'Arcade', 
+        releaseYear: 1980 
+    },
+    {
+        id: 2,
+        title: 'Galaga', 
+        genre: 'Arcade', 
+        releaseYear: 1982
+    }
+]
+
 server.get('/', (req,res) => {
     res.status(200).json({ message : "App running" })
 })
 
+server.get('/games', (req, res) => {
+    res.status(200).json(games)
+})
 
 
 module.exports = server
