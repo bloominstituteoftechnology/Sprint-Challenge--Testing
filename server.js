2352b8a741e74a7a5183bbb3f5200262b7eb06a1
Nicky Chen <nicky.chen0@gmail.com>
2018-08-17 09:40:48 -0700

    mvp - get & push done

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ server.get("/", (req, res) => {
   res.status(200).send(games);
 });
 
+server.post("/games", (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  if (!title || !genre) {
+    res.status(422).json({ error: "Please include a title and genre for the game." });
+  } else {
+    games.push(req.body);
+    res.status(201).json(req.body);
+  }
+});
+
 module.exports = server;
--- server.specs.test.js
+++ server.specs.test.js
@@ -10,10 +10,37 @@ describe("server.js", () => {
       expect(response.status).toEqual(expected);
     });
 
-    it("should return the list of games", async () => {
+    it("should return the list of games, or an empty array if the games list is empty.", async () => {
       const expected = games;
       const response = await request(server).get("/");
       expect(response.body).toEqual(expected);
+    });
+  });
+
+  describe("POST routes", () => {
+    it("should return a status code of 201 OK", async () => {
+      const expected = 201;
+      const newGame = {
+        title: "Starcraft",
+        genre: "Strategy",
+        releaseYear: "1994"
+      };
+      const response = await request(server)
+        .post("/games")
+        .send(newGame);
+      expect(response.status).toEqual(expected);
+    });
+
+    it("should return a 422 status code if either title or genre aren't put in", async () => {
+      const expected = 422;
+      const newGame = {
+        title: "Halo",
+        releaseYear: 1993
+      };
+      const response = await request(server)
+        .post("/games")
+        .send(newGame);
+      expect(response.status).toEqual(expected);
     });
   });
 });
