37cf44e90d039f73414f58bd3c1a685d1f23581a
Alec <afroenygam@gmail.com>
2018-08-17 12:50:12 -0400

    Code reformatted

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const server = express();
 
 server.use(express.json());
 
-const games = [
-  {
+const games = [{
     title: 'Pacman',
     genre: 'Arcade',
     releaseYear: 1980
@@ -22,7 +21,9 @@ const games = [
 ]
 
 server.get('/', (req, res) => {
-  res.status(200).json({api: 'Running'});
+  res.status(200).json({
+    api: 'Running'
+  });
 });
 
 server.get('/games', (req, res) => {
@@ -31,7 +32,7 @@ server.get('/games', (req, res) => {
 
 server.post('/games', (req, res) => {
   let notUnique = false;
-  for (let i=0; i<games.length; i++) {
+  for (let i = 0; i < games.length; i++) {
     if (games[i]['title'] === req.body.title) {
       notUnique = true;
       break;
@@ -39,11 +40,12 @@ server.post('/games', (req, res) => {
   }
   if (req.body.title && req.body.genre && !notUnique) {
     games.push(req.body);
-    res.status(201).json({success: true});
-  } else if(notUnique) {
+    res.status(201).json({
+      success: true
+    });
+  } else if (notUnique) {
     return res.status(405).send('Game with that title already exists in database');
-  }
-   else {
+  } else {
     res.status(422).send('Error');
   }
 
