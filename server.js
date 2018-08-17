8f68aa0c07ca894eef827832c815678e59c671c3
DirupT <iamcooled@gmail.com>
2018-08-17 10:56:57 -0500

    updated GET / POST to use games array

--- server.js
+++ server.js
@@ -3,14 +3,40 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-server.get('/games', (req, res) => {
-    res.status(200).json([]);
-});
+let games = [
+    {
+        id: 1,
+        title: 'Jigsaw Planet',
+        genre: 'Puzzle'
+    },
+    {
+        id: 2,
+        title: 'Daymare',
+        genre: 'Survival Horror'
+    },
+    {
+        id: 3,
+        title: 'Fortnite',
+        genre: 'Action',
+        releaseYear: 2017
+    }
+];
+
+let gameId = 4;
 
 server.post('/games', (req, res) => {
     const { title, genre, releaseYear } = req.body;
+    const game = { id: gameId, title, genre, releaseYear };
     if (!title || !genre) return res.status(422).json({ error: 'Please fill in the required fields' });
-    res.status(201).json({ title, genre, releaseYear });
-})
+
+    games.push(game);
+    gameId++;
+
+    res.status(201).json(game);
+});
+
+server.get('/games', (req, res) => {
+    res.status(200).json(games);
+});
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,35 +2,27 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('server.js endpoints', () => {
-    describe('GET /games', () => {
-        it('should return status code 200 and an empty array', async () => {
-            await request(server)
-                .get('/games')
-                .expect(200, []);
-        });
-    });
-
     describe('POST /games', () => {
-        it('should return status code 201, JSON, and `title, body` if releaseYear isnt sent', async () => {
+        it('should return status code 201 and JSON', async () => {
             await request(server)
                 .post('/games')
                 .send({ title: 'A wise fairy', genre: 'Fiction' })
                 .expect('Content-Type', /json/)
-                .expect(201, { title: 'A wise fairy', genre: 'Fiction' });
-        });
-
-        it('should return status code 201, JSON, and `title, body, releaseYear`', async () => {
-            await request(server)
-                .post('/games')
-                .send({ title: 'A wise fairy', genre: 'Fiction', releaseYear: 1999 })
-                .expect('Content-Type', /json/)
-                .expect(201, { title: 'A wise fairy', genre: 'Fiction', releaseYear: 1999 });
+                .expect(201);
         });
 
         it('should return status code 422 if title and body are not provided', async () => {
             await request(server)
                 .post('/games')
                 .expect(422);
+        });
+    });
+
+    describe('GET /games', () => {
+        it('should return status code 200 and an array of games', async () => {
+            await request(server)
+                .get('/games')
+                .expect(200);
         });
     });
 });
