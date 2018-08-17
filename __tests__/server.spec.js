24162a9c1a1c8097bd620dd4ad22c66b48f6b461
markdavidwilliams <markwilliams.aus@gmail.com>
2018-08-17 12:14:20 -0500

    Stretch: wrote GET by id test then implemented into endpoint

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -6,21 +6,6 @@ const server = require('../server');
 
 // group all test in a suite to display on the console
 describe('server', () => {
-  // group all GET endpoint tests
-  describe('GET endpoint (/games)', () => {
-    // tests that GET endpoint returns status code 200
-    it('should return HTTP status code 200', async () => {
-      const response = await request(server).get('/games');
-      expect(response.status).toEqual(200);
-    })
-
-    // tests that GET endpoint returns an array, with or without any items
-    it('should return an array', async () => {
-      const response = await request(server).get('/games');
-      expect(Array.isArray(response.body)).toBe(true);
-    })
-  })
-
   // group all POST endpoint tests
   describe('POST endpoint (/games)', () => {
     // tests that POST endpoint returns status code 201 when correct information is sent
@@ -47,6 +32,36 @@ describe('server', () => {
         releaseYear: 2016,
       });
       expect(response.status).toEqual(405);
+    })
+  })
+
+  // group all GET endpoint tests
+  describe('GET endpoint (/games)', () => {
+    // tests that GET endpoint returns status code 200
+    it('should return HTTP status code 200', async () => {
+      const response = await request(server).get('/games');
+      expect(response.status).toEqual(200);
+    })
+
+    // tests that GET endpoint returns an array, with or without any items
+    it('should return an array', async () => {
+      const response = await request(server).get('/games');
+      expect(Array.isArray(response.body)).toBe(true);
+    })
+  })
+
+  // group all GET/:id endpoint tests
+  describe('GET endpoint(/games/:id', () => {
+    // test that GET/:id returns status code 200 when game is found
+    it('should return HTTP status code 200 when game is found by id', async () => {
+      const response = await request(server).get('/games/1');
+      expect(response.status).toEqual(200);
+    })
+
+    // test that GET/:id returns status code 404 when game not found
+    it('should return HTTP status code 404 when game not found', async () => {
+      const response = await request(server).get('/games/2');
+      expect(response.status).toEqual(404);
     })
   })
 })
--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const server = express();
 // apply middleware
 server.use(express.json());
 
+// establish base id
+let id = 1;
+
 // placeholder for array of games in memory
 let games = [];
+
+// increment id
+const increment = () => {
+  return id++;
+}
 
 // establish GET endpoint
 server.get('/games', (req, res) => {
@@ -17,9 +25,26 @@ server.get('/games', (req, res) => {
     .json(games)
 })
 
+// establish GET/:id endpoint
+server.get('/games/:id', (req, res) => {
+  let requested = Number(req.params.id);
+  let found = games.find(item => {
+    return item.id === requested;
+  });
+  if (found) {
+    res
+      .status(200)
+      .end()
+  } else {
+    res
+      .status(404)
+      .end()
+  }
+})
+
 // establish POST endpoint
 server.post('/games', (req, res) => {
-  let game = req.body;
+  let game = { id: increment(), ...req.body }
   let unique = games.find(item => {
     return item.title === game.title;
   })
