5c4db2304b5d2fe50feb41e2f719d54b5c026153
Tai <bowlofryce@gmail.com>
2018-08-17 13:13:35 -0500

    MVP complete, submit at 13:13 Central time.

--- Review.md
+++ Review.md
@@ -4,3 +4,8 @@
 2.  What is the point of `Test Driven Development`? What do you think about this approach?
 3.  What are `mocks`? What are a good use case for them?
 4.  Mention three types of tests.
+
+1. describe() is used for putting the same test under the same section. it() is the actual code used for testing.
+2. The point of TDD is to ensure that you hit all points of your code and ensure you don't leave bugs behind. It's a good way to think about how your app/program to function and cover unwanted bugs beforehand.
+3. mocks are false input values, a good case would be using for components that need values from the parent component.
+4. Unit Tests, Integration Tests, UI Tests
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+// Storing information locally
+let games = [
+    {
+        title: 'Pacman',
+        genre: 'Arcade',
+        releaseYear: 1980
+    },
+];
+
+// GET /GAMES
+server.get('/games', (req, res) => {
+    res.status(200).json(games);
+});
+
+// POST /GAMES
+server.post('/games', (req, res) => {
+    const { title, genre, releaseYear } = req.body;
+    const newGames = { title, genre, releaseYear };
+    
+    if(!title || !genre || !releaseYear) {
+        res.status(422).json('Require: Title, Genre, Release Year');
+    }
+    games.push(newGames);
+    res.status(201).json(games);
+});
+
+module.exports = server;
