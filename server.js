3e3ad32f263043b8089318f41c7968c525e4e04b
-tristan- <tristan.linehan@gmail.com>
2018-08-17 10:21:42 -0700

    MVP - endpoints GET & POST added

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+server.get('/games', (req, res) => {
+    res.status(200).json([1,2]);
+    // res.status(200).json([]);
+    // res.status(200).json('hey');
+    // res.status(200).json({hey: 'you'});
+});
 
+server.post('/games', (req, res) => {
+    const { title, genre, releaseYear } = req.body;
+    if ( !title || !genre ) {
+        return res.status(422).json([]);
+    };
+    res.status(201).json([]);
+});
 
 module.exports = server;
