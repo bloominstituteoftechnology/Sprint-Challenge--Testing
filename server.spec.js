8eeaa373f1e648148df763b2d9b2dec3b0ea1294
-tristan- <tristan.linehan@gmail.com>
2018-08-17 10:17:52 -0700

    POST tests complete

--- server.spec.js
+++ server.spec.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const server = require('./server.js');
 
 
-describe('GET /games', () => {
+describe ('GET /games', () => {
     it('should return status code 200 from /games' /*with list of games*/, async () => {
         const response = await request(server).get('/games');
         expect(response.status).toEqual(200);
@@ -11,5 +11,21 @@ describe('GET /games', () => {
     it('should return an array, even if empty', async () => {
         const response = await request(server).get('/games');
         expect(response.body).toBeInstanceOf(Array);
+    });
+});
+
+describe('POST /games', () => {
+    it('should return 422 if incomplete fields submitted', async () => {
+        const response = await request(server).post('/games').send({ title: 'Pacman' }) 
+        expect(response.status).toEqual(422);
+    });
+    it('should return 201 if created', async () => {
+        const response = await request(server).post('/games').send(
+            { 
+                title: 'Pacman',
+                genre: 'Arcade'
+            }
+        ); 
+        expect(response.status).toEqual(201);
     });
 });
