2ac9ef17a9e4938cb3c703a84d478857722254f7
Simon <Simon_Freelance@hotmail.com>
2018-08-17 13:52:43 -0400

    GET request implemented for games table

--- server.js
+++ server.js
@@ -4,5 +4,14 @@ const server = express();
 server.use(express.json());
 const serverErrorMsg = 'Error processing your request';
 
+server.get('/', (req, res) => {
+    db('games')
+    .then(response => {
+        res.status(200).json(response);
+    })
+    .catch(err => {
+    res.status(500).json(serverErrorMsg);
+    });
+});
 
 module.exports = server;
