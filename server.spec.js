07f2afb31857143e85c0811cbd9dc0169a1d290f
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-08-17 08:56:36 -0700

    adds server.js

--- server.spec.js
+++ server.spec.js
@@ -28,60 +28,60 @@ describe('server.js', () => {
     const response = await request(server).get('/');
     expect(response.type).toEqual('application/json');
   });
+});
 
-  describe('list games', () => {
-    it('should return status code 200 when hit `/games`', async () => {
-      const expected = 200;
-      const response = await request(server).get('/games');
-      expect(response.status).toEqual(expected);
-    });
+describe('list games', () => {
+  it('should return status code 200 when hit `/games`', async () => {
+    const expected = 200;
+    const response = await request(server).get('/games');
+    expect(response.status).toEqual(expected);
+  });
 
-    it('should return an array of games', async () => {
-      const response = await request(server).get('/games');
-      const valid = Array.isArray(response.body.games);
-      expect(valid).toBe(true);
-    });
-  }
+  it('should return an array of games', async () => {
+    const response = await request(server).get('/games');
+    const valid = Array.isArray(response.body.games);
+    expect(valid).toBe(true);
+  });
+});
 
-  describe('post games', () => {
-    it('should add a game', async () => {
-      const game = {title: 'Pacman', genre:'Arcade', releaseYear: 1980}
-      const response = await request(server)
-        .post('/games')
-        .send({ game: game });
-      const valid = Array.isArray(response.body.games);
-      expect(valid).toBe(true);
-    });
+describe('post games', () => {
+  it('should add a game', async () => {
+    const game = { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 };
+    const response = await request(server)
+      .post('/games')
+      .send({ game: game });
+    const valid = Array.isArray(response.body.games);
+    expect(valid).toBe(true);
+  });
 
-    it('should return a 422 if missing the title of a game', async () => {
-      const expected = 422;
-      const game = {title: '', genre:'Arcade', releaseYear: 1980}
-      const response = await request(server)
-        .post('/games')
-        .send({ game: game });
-        expect(response.status).toEqual(expected);
-    });
+  it('should return a 422 if missing the title of a game', async () => {
+    const expected = 422;
+    const game = { title: '', genre: 'Arcade', releaseYear: 1980 };
+    const response = await request(server)
+      .post('/games')
+      .send({ game: game });
+    expect(response.status).toEqual(expected);
+  });
 
-    it('should return a 422 if missing the genrew of a game', async () => {
-      const expected = 422;
-      const game = {title: 'Pacman', genre:'', releaseYear: 1980}
-      const response = await request(server)
-        .post('/games')
-        .send({ game: game });
-        expect(response.status).toEqual(expected);
-    });
+  it('should return a 422 if missing the genrew of a game', async () => {
+    const expected = 422;
+    const game = { title: 'Pacman', genre: '', releaseYear: 1980 };
+    const response = await request(server)
+      .post('/games')
+      .send({ game: game });
+    expect(response.status).toEqual(expected);
+  });
 
-    it('should delete a game', async () => {
-      const game = 'The Lord f the Flies';
-      const response = await request(server)
-        .delete('/games')
-        .send({ game: game });
-      const gameArr = response.body.games;
-      let valid = true;
-      for (let i = 0; i < gameArr.length; i++) {
-        if (gameArr[i] == game) valid = false;
-      }
-      expect(valid).toBe(true);
-    });
+  it('should delete a game', async () => {
+    const game = 'The Lord f the Flies';
+    const response = await request(server)
+      .delete('/games')
+      .send({ game: game });
+    const gameArr = response.body.games;
+    let valid = true;
+    for (let i = 0; i < gameArr.length; i++) {
+      if (gameArr[i] == game) valid = false;
+    }
+    expect(valid).toBe(true);
   });
 });
