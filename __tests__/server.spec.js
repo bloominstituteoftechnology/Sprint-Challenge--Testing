3c9b8f46b88bd435a79cabc117676c495cf123c1
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-17 11:45:24 -0400

    2 tests for POST, 2 tests for GET

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,20 +1,44 @@
 const request = require('supertest');
-const server = require('./server');
+const server = require('../server');
 
 describe('testing server.js', () => {
-    it('testing GET /games', () => {
+    
+    describe('testing POST /games', () => {
+        
+        it('if info is incomplete, return error code 422', async () => {
+            const expected = 422;
+            const response = await request(server).post('/games').send({title: "", genre: "Arcade",  releaseYear: 1984});
+            expect(response.status).toEqual(expected);
+          });
+
+        it('if info is complete, return status code 200', async () => {
+            const expected = 201;
+            const response = await request(server).post('/games').send({title: "Tetris", genre: "Arcade",  releaseYear: 1984});
+            expect(response.status).toEqual(expected);
+          });
 
     })
 
-    it('testing POST /games', () => {
+    describe('testing GET /games', () => {
+
+        it('should return the list of games', async () => {
+            const expected = {"games": [{title: "Pacman", genre: "Arcade",  releaseYear: 1980}, {title: "Minecraft", genre: "Sandbox", releaseYear: 2009}, {title: "World of Warcraft", genre: "Role-playing", releaseYear: 2004}]};
+            const response = await request(server).get('/games');
+            expect(response.body).toEqual(expected);
+          });
+
+        it('should return HTTP status code 200', async () => {
+            const response = await request(server).get('/games');
+            expect(response.status).toEqual(200);
+          });
+
+    })
+
+    describe('testing GET  /games/:id', () => {
         
     })
 
-    it('testing GET  /games/:id', () => {
-        
-    })
-
-    it('testing DELETE  /games/:id', () => {
+    describe('testing DELETE  /games/:id', () => {
         
     })
 })
--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/games', (req, res) => {
+const games = [{title: "Pacman", genre: "Arcade",  releaseYear: 1980}, {title: "Minecraft", genre: "Sandbox", releaseYear: 2009}, {title: "World of Warcraft", genre: "Role-playing", releaseYear: 2004}]
 
+server.get('/games', (req, res) => {
+        res.status(200).json({games});
 })
 
 server.post('/games', (req, res) => {
-    
+        const { title, genre, releaseYear } = req.body;
+        if (!title || !genre || !releaseYear) {
+            res.status(422).json({ msg: 'Make sure you add a title, a genre, and a release year.'})
+        }
+        res.status(201).json({ title, genre, releaseYear });
 })
 
 server.get('/games/:id', (req, res) => {
