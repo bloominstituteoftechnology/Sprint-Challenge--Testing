b0b7a461faa0d3e7ba09af440975682c5a3e37d4
Allen Hai <allenhai03@gmail.com>
2018-08-17 11:33:18 -0500

    write test to check if response is sending json and to check if the game objects have the required fields

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const server = express();
 server.use(express.json());
 
 server.get('/games', (req, res) => {
-  res.status(200).send('')
+  res.status(200).json([
+    {
+      title: 'wassup', 
+      genre: 'kdjfadk',
+      releaseYear: 4400
+    }
+  ])
 })
 
 module.exports = server
--- server.spec.js
+++ server.spec.js
@@ -2,9 +2,21 @@ const request = require('supertest')
 
 const server = require('./server')
 
-describe('GET [/games]', () => {
+describe('GET [/games]', async () => {
+  
   it('should return a status code of 200', async () => {
-    const req = await request(server).get('/games')
-    expect(req.status).toBe(200)
+    const res = await request(server).get('/games')
+    expect(res.status).toBe(200)
+  })
+  it('should return json', async () => {
+    const res = await request(server).get('/games')
+    expect(res.type).toBe('application/json')
+  })
+  it('game objects should have title, genre, and releaseYear fields', async () => {
+    const res = await request(server).get('/games')
+    gameMatches = res.body.filter(game => 
+      game['title'] && game['genre'] && game['releaseYear'] 
+    )
+    expect(res.body.length).toBe(gameMatches.length)
   })
 })
