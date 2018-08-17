6d5bdd11e4f890a8f4d2ce2403192aaa80f8d947
Joe Stanfield <jlstan06@hotmail.com>
2018-08-17 11:54:23 -0400

    added test for post

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
 server.get('/games', (req, res) => {
     res.status(200).json([]);
+})
+
+server.post('/games', (req, res) => {
+    const { title, genre, releaseYear } = req.body;
+    if (!title || !genre) return res.status(422).json({error:'I need you to fill in the required fields please' });
+    res.status(201).json({title, genre, releaseYear });
 })
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,8 +2,10 @@ const request = require('supertest');
 
 const server  = require('./server');
 
+
+
 describe('server.js endpoints', () => {
-    describe('GET /', () => {
+    describe('GET /games', () => {
         test('should return status code 200, JSON', async () => { 
     
         await request(server)
@@ -11,4 +13,29 @@ describe('server.js endpoints', () => {
             .expect(200, []);
         })
     })
+
+
+describe('POST /games' , () => {
+    test('should return status code 201, JSON, and title body if releaseYear isnt sent', async () => {
+        await request(server)
+            .post('/games')
+            .send({title:'Madden', genre: 'Sports'})
+            .expect('Content-Type', /json/)
+            .expect(201, {title: 'Madden', genre: 'Sports'})
+    })
+
+    test('should return status code 201, JSON, and title, body, releaseYear', async () => {
+        await request(server)
+            .post('/games')
+            .send({title: 'Madden', genre:'Sports', releaseYear:2018});
+    });
+
+    test('should return status code 422 if title and body are not provided', async () => {
+        await request(server)
+            .post('/games')
+            .expect(422);
+    })
+})
+
+
 })
