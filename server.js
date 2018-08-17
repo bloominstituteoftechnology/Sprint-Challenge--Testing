a6e18aecfb301b160d03e81ec68adb01658b1e95
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-17 10:54:24 -0500

    POST /games

--- server.js
+++ server.js
@@ -8,4 +8,15 @@ server.get("/", (req, res) => {
   res.status(200).json({ api: "running" });
 });
 
+server.post("/games", async (req, res) => {
+  if (!req.body.title || !req.body.genre) {
+    return res.status(422).json({
+      errorMessage: "Required fields are incomplete."
+    });
+  } else {
+    const game = await req.body;
+    return res.status(201).json(game);
+  }
+});
+
 module.exports = server;
