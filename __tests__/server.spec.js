6ecdc6902f03963210a2a4fe31caacd0312affe1
cmok4290 <cmok4290@gmail.com>
2018-08-17 12:22:06 -0400

    added get body and get type tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -4,9 +4,57 @@ const server = require('../index');
 describe('SERVER', () => {
     describe('Index Route', () => {
         describe('get', () => {
-            it('should return OK status code from get route', async () => {
+            it('should return OK status code', async () => {
                 const res = await request(server).get('/');
                 expect(res.status).toEqual(200);
+            });
+
+            it('should return response body, array', async () => {
+                const expected = {
+                    success: true,
+                    data: {
+                        results: [
+                            {
+                                title: 'Arkanoid',
+                                genre: 'breakout',
+                                releaseYear: 1986
+                            },
+                            {
+                                title: 'Asteroids',
+                                genre: 'multi-directional shooter',
+                                releaseYear: 1979
+                            },
+                            {
+                                title: 'Defender',
+                                genre: 'scrolling shooter',
+                                releaseYear: 1981
+                            },
+                            {
+                                'title': 'Rush\'n\'Attack',
+                                'year': 1985,
+                                'genre': 'run and gun',
+                              },
+                              {
+                                'title': 'Sinistar',
+                                'year': 1982,
+                                'genre': 'scrolling shooter',
+                              },
+                              {
+                                'title': 'Skate or Die!',
+                                'year': 1988,
+                                'genre': 'skateboarding',
+                              }
+                        ]
+                    }
+                };
+
+                const res = await request(server).get('/');
+                expect(res.body).toEqual(expected);
+            });
+
+            it('should return response type, JSON', async () => {
+                const res = await request(server).get('/');
+                expect(res.type).toEqual('application/json');
             });
         });
     });
