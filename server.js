805d4f63996dca8d3f36c4d73acd0682f724b52e
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-17 09:04:41 -0700

    added get endoint for /games and some tests for post

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.status(200).json({ success: true, data: { api: 'running' } });
 });
+server.get('/games', (req, res) => {
+    res.status(200).json([]);
+});
 
 server.post('/games', (req, res) => {
     const { title, genre, releaseYear } = req.body;
     console.log('name', name)
-
-    res.status(200).json({ title: title, genre: genre, releaseYear: releaseYear })
+    if (!title || !genre) return res.status(422).json({ error: 'Please, provide both a title and genre.' })
+    res.status(201).json({ title: title, genre: genre, releaseYear: releaseYear })
 });
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -18,19 +18,36 @@ describe('server.js', () => {
             expect(response.type).toEqual('application/json');
         });
     });
+    describe('server.js', () => {
+        describe('root endpoint (/games)', () => {
+            it('should return status code 200 OK and the list of games', async () => {
+                const expected = 200;
+
+                const response = await request(server).get('/games');
+
+                expect(response.status).toEqual(expected);
+                expect(response.body).toEqual([]);
+            });
+        });
+    })
 
     describe('POST /games', () => {
 
         it('should return status code 201 OK', async () => {
-            const expected = 200;
-            const response = await request(server).post('/games').send({ title: 'PW', genre: 'MMO', releaseYear: '2005' });
+            const expected = 201;
+            const response = await request(server).post('/games').send({ title: 'PW', genre: 'MMO' });
             expect(response.status).toEqual(expected);
         });
 
         it('should return JSON when new game is created', async () => {
             const expected = { title: 'PW', genre: 'MMO', releaseYear: '2005' };
-            const response = await request(server).post('/games');
+            const response = await request(server).post('/games').send({ title: 'PW', genre: 'MMO', releaseYear: '2005' });
             expect(response.body).toEqual(expected);
+        });
+        it('should return status code 422 if the information is incomplete', async () => {
+            const expected = 422;
+            const response = await request(server).post('/games');
+            expect(response.status).toEqual(expected);
         });
     });
 });
