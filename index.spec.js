b963f48a011ffed2dae736b9af6fc0d9a42feaa4
LBWright <lwright.js@gmail.com>
2018-08-17 12:05:01 -0500

    cleaned up the code a little bit to make it easier to read. Added whitespace.

--- index.spec.js
+++ index.spec.js
@@ -7,10 +7,12 @@ describe('server', () => {
         .get('/games')
         .expect(200);
     });
+
     it('returned response should be type JSON', async () => {
       const res = await request(server).get('/games');
       expect(res.type).toEqual('application/json');
     });
+
     it('should return an array of games in response body', async () => {
       const expected = [
         {
@@ -36,16 +38,19 @@ describe('server', () => {
       expect(res.body).toEqual(expected);
     });
   });
+
   describe('GET /games/:id', () => {
     it('should return a status code of 200', async () => {
       const res = await request(server)
         .get('/games/2')
         .expect(200);
     });
+
     it('should respond with a type of JSON', async () => {
       const res = await request(server).get('/games/2');
       expect(res.type).toEqual('application/json');
     });
+
     it('should return with a single object', async () => {
       const expected = {
         id: '2',
@@ -56,12 +61,14 @@ describe('server', () => {
       const res = await request(server).get('/games/2');
       expect(res.body).toEqual(expected);
     });
+
     it('should return with a 404 status if the game is not found', async () => {
       const res = await request(server)
         .get('/games/10')
         .expect(404);
     });
   });
+
   describe('POST /games', () => {
     it('should return successfully with a status code of 201', async () => {
       const game = {
@@ -74,6 +81,7 @@ describe('server', () => {
         .send(game)
         .expect(201);
     });
+
     it('should return with an id', async () => {
       const game = {
         title: 'Knights of the Old Republic', // required
@@ -85,6 +93,7 @@ describe('server', () => {
         .send(game);
       expect(res.body[3].id).toEqual(4);
     });
+
     it('should return unsuccessfully with a status code of 422', async () => {
       const game = {
         title: 'Monster Hunter' // required
@@ -94,6 +103,7 @@ describe('server', () => {
         .send(game);
       expect(res.status).toEqual(422);
     });
+
     it('should return an array of games with the newly posted object', async () => {
       const game = {
         title: 'Game Title', // required
@@ -106,12 +116,12 @@ describe('server', () => {
         genre: 'Game Genre', // required
         releaseYear: 2018 // not required
       };
-
       const res = await request(server)
         .post('/games')
         .send(game);
       expect(res.body[res.body.length - 1]).toEqual(expected);
     });
+
     it('should return a status of 405 if a duplicate game is added to the system', async () => {
       const game = {
         title: 'Knights of the Old Republic', // required
@@ -124,12 +134,14 @@ describe('server', () => {
         .expect(405);
     });
   });
+
   describe('/DELETE/:id', () => {
     it('should respond with a status code of 200', async () => {
       const res = await request(server)
         .del('/games/1')
         .expect(200);
     });
+
     it('should respond with an array of all games', async () => {
       const res = await request(server).del('/games/2');
       expect(res.body).toEqual([
