b00d34db50cda79c2b3058bccd32956a53703aac
-tristan- <tristan.linehan@gmail.com>
2018-08-17 09:45:45 -0700

    GET tests complete

--- server.spec.js
+++ server.spec.js
@@ -3,3 +3,13 @@ const request = require('supertest');
 const server = require('./server.js');
 
 
+describe('GET /games', () => {
+    it('should return status code 200 from /games' /*with list of games*/, async () => {
+        const response = await request(server).get('/games');
+        expect(response.status).toEqual(200);
+    });
+    it('should return an array, even if empty', async () => {
+        const response = await request(server).get('/games');
+        expect(response.body).toBeInstanceOf(Array);
+    });
+});
