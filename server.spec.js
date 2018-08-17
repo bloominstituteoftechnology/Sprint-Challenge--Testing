560f3fceb62524cd8da29d3a415571d39c15d92a
chainchompa <chainchompa@gmail.com>
2018-08-17 12:22:02 -0400

    wrote tests for root endpoint

--- server.spec.js
+++ server.spec.js
@@ -3,6 +3,19 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server.js', () => {
+  describe('root endpoint(/)', () => {
+    it('should return status code 200 OK', async () => {
+      const expected = 200;
+      const response = await request(server)
+      .get('/');
+      expect(response.status).toBe(expected);
+    });
+     it('should return JSON', async () => {
+      const response = await request(server)
+      .get('/');
+      expect(response.type).toEqual('application/json');
+    });
+
   describe('get /games', () => {
     it('should return status code 200 ok', async () => {
       const expected = 200;
@@ -25,25 +38,7 @@ describe('server.js', () => {
       expect(response.body).toEqual(expected);
     })
   })
+  describe('post /games', () => {
+    it('should ')
+  })
 })
-
-
-
-
-
-
-  describe('POST /greet/:name', () => {
-    it('should return { hello: name } when name provided inside body', async () => {
-      // arrange
-      const expected = { hello: 'frodo baggins' };
-
-      // act
-      const response = await request(server)
-        .post('/greet/frodo')
-        .send({ lastName: 'baggins' });
-
-      // assert
-      expect(response.body).toEqual(expected);
-    });
-  });
-});
