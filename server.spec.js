8d73f5b202632a0a642ee2522110238295625be3
johnoro <officialjohnorourke@gmail.com>
2018-09-21 13:13:14 -0400

    Added tests for PUT.

--- server.spec.js
+++ server.spec.js
@@ -73,47 +73,47 @@ describe('server.js', () => {
     });
 
     it('should return a Created http code (201) when only given a title and genre', async () => {
-      const goodRes = await req(server).post('/games')
+      const goodReq = await req(server).post('/games')
         .send({
           'title': 'Galaga',
           'genre': 'Arcade'
         })
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json');
-      expect(goodRes.status).toBe(201);
+      expect(goodReq.status).toBe(201);
     });
 
     it('should return a Not Allowed http code (405) if given a taken title', async () => {
-      const badRes = await req(server).post('/games')
+      const badReq = await req(server).post('/games')
         .send({
           'title': 'Asteroids',
           'genre': 'Arcade'
         })
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json');
-      expect(badRes.status).toBe(405);
+      expect(badReq.status).toBe(405);
     });
 
-    it('should return an Unprocessable Entity http code (422) if not given genre.', async () => {
-      const badRes = await req(server).post('/games')
+    it('should return an Unprocessable Entity http code (422) if not given genre', async () => {
+      const badReq = await req(server).post('/games')
         .send({
           'title': 'Ms. Pacman',
           'releaseYear': 1981
         })
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json');
-      expect(badRes.status).toBe(422);
+      expect(badReq.status).toBe(422);
     });
 
-    it('should return an Unprocessable Entity http code (422) if not given title.', async () => {
-      const badRes = await req(server).post('/games')
+    it('should return an Unprocessable Entity http code (422) if not given title', async () => {
+      const badReq = await req(server).post('/games')
         .send({
           'genre': 'Arcade',
           'releaseYear': 1980
         })
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json');
-      expect(badRes.status).toBe(422);
+      expect(badReq.status).toBe(422);
     });
   });
 
@@ -139,6 +139,40 @@ describe('server.js', () => {
 
     it('should return a Not Found http code (404) if game with given id isn\'t found', async () => {
       const badReq = await req(server).delete('/games/-100twenty');
+      expect(badReq.status).toBe(404);
+    });
+  });
+
+  describe('PUT /games/:id', () => {
+    let res;
+
+    beforeAll(async () => {
+      res = await req(server).put('/games/2')
+        .send({
+          'game': 'Pacman',
+          'genre': 'Arcade',
+          'releaseYear': 1980
+        })
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json');
+    });
+
+    it('should return an OK http code (200)', () => {
+      expect(res.status).toBe(200);
+    });
+
+    it('should return a count of the records updated (1)', () => {
+      expect(res.body).toBe(1);
+    });
+
+    it('should return a Not Found http code (404) if given a bad id', async () => {
+      const badReq = await req(server).put('/games/-100two')
+        .send({
+          'game': 'Not Pacman',
+          'genre': 'Arcade'
+        })
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json');
       expect(badReq.status).toBe(404);
     });
   });
