0ac4f04ce2195bb2f2a937456cc37844a869d9df
William <kwontaewoongah@gmail.com>
2018-09-21 10:30:56 -0700

    finished post route

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ const games=[];
 server.get('/games',(req,res)=>{
     res.status(200).json({games:games});
 })
+server.post('/games',(req,res)=>{
+    const newGame=req.body;
+    if (!newGame.title||!newGame.genre||!newGame.releaseYear){
+        res.status(422).json({error:'missing field(s)'});
+    }
+    else {
+        res.status(201).json({id:id});
+        id++;
+    }
+})
 module.exports=server;
--- server.spec.js
+++ server.spec.js
@@ -2,7 +2,7 @@ const request=require('supertest');
 const server=require('./server');
 
 describe('server.js',()=>{
-    describe('games route',()=>{
+    describe('GET games route',()=>{
         it('should return a 200 status code',async()=>{
             const expectedStatusCode=200;
             response=await request(server).get('/games');
@@ -17,5 +17,28 @@ describe('server.js',()=>{
             const response=await (request(server)).get('/games');
             expect(response.body).toEqual(expectedBody);
         })
+    })
+    describe('POST games route',()=>{
+        it('returns 422 status code if information is incomplete',async()=>{
+            const response=await (request(server)).post('/games').send({title:'Pacman'});
+            expect(response.status).toEqual(422);
+        })
+        it ('returns 201 status code if information is complete',async()=>{
+            const response=await(request(server)).post('/games').send({
+                title: 'Pacman', 
+                genre: 'Arcade', 
+                releaseYear: 1980 
+              })
+              expect(response.status).toEqual(201);
+        })
+        it('returns id of the game',async()=>{
+            const response=await(request(server)).post('/games').send({
+                title: 'Super Mario', 
+                genre: 'Console', 
+                releaseYear: 1985 
+              })
+              expect(response.body.id).toEqual(2);
+        })
+        
     })
 })
