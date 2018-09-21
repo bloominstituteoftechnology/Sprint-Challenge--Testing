072c7a0ac177483e1c4300a4c42654d159d8f905
dasgma <dasgrigoma@gmail.com>
2018-09-21 08:29:19 -0700

    Server.js and server.spec.js created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' })
+});
+
+
+
+
+module.exports = server;
