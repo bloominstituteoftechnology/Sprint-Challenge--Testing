81e2fcad8f6d053627f048984d423c3694d363d5
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-21 08:33:07 -0700

    Added dependencies and set up files.

---
+++ server.spec.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const server = require('./server.js');
+
+describe('server', () => {
+	it('run all the tests', () => {
+			expect(true).toBeTruthy();
+	});
+});
