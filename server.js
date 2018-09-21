736ab5e42a73a1d6359f29e37eb13867cf2ab89a
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-21 10:21:49 -0600

    Added GET endpoint-- All tests are passing

--- server.js
+++ server.js
@@ -2,5 +2,16 @@ const express = require('express');
 
 const server = express(); 
 
+const games = [
+    { id: "1", title: "Pacman", genre: "Arcade", releaseYear: 1980 },
+    { id: "2", title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
+    { id: "3", title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
+    { id: "4", title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
+  ];
+
+server.get("/games", (req, res) => {
+    res.status(200).json(games); 
+})
+
 
 module.exports = server; 
--- server.spec.js
+++ server.spec.js
@@ -2,11 +2,11 @@ const server = require("./server");
 const request = require("supertest");
 
 const games = [
-  { id: "1", title: "Pacman", genre: "Arcade", releaseYear: 1980 },
-  { id: "2", title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
-  { id: "3", title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
-  { id: "4", title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
-];
+    { id: "1", title: "Pacman", genre: "Arcade", releaseYear: 1980 },
+    { id: "2", title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
+    { id: "3", title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
+    { id: "4", title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
+  ];   
 
 describe("Server", () => {
   
@@ -19,10 +19,9 @@ describe("Server", () => {
       const response = await request(server).get("/games");
       expect(response.body).toEqual(games);
     });
-    it("should return an array", async () => {
+    it("should return an array", async () => {        
       const response = await request(server).get("/games");
       expect(Array.isArray(games)).toBeTruthy();
     });
   });
-  
 });
