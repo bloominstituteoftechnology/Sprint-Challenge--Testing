f267be8ad2e919a08b146ed1a627b7b39da839fc
MichaelBoegner <cmikeb@gmail.com>
2018-09-21 12:37:16 -0400

    MVP Completed

--- server.js
+++ server.js
@@ -14,4 +14,52 @@ server.get('/', (req, res) => {
 })
 
 
+//=========Middleware==========//
+function propertiesChecker(req, res, next){
+    const game = req.body;
+
+    if(game.title && game.genre && game.releaseYear) {
+        next(); 
+    } else {
+        res.status(422).json({msg: "Posting a game requires properties of 'title', 'genre', and 'releaseYear'."})
+    }
+}
+
+function couldNotGet(req, res, next) {
+    var today = new Date();
+    var todaysDate = today.getDate();
+    
+    if(todaysDate % 2 === 1) {
+        res.status(403).json({msg: "Games can only be retrieved on an even date of the month."});
+    } else {
+        next(); 
+    }
+}
+
+
+
+
+//=========Endpoints==========//
+
+server.get('/games', couldNotGet, (req, res) => {
+    db('games')
+        .then(games => {
+            res.status(200).json(games)
+        })
+        .catch(err => { 
+            res.status(500).json({msg: "No games for you budday!"})
+        })
+})
+
+server.post('/games', propertiesChecker, (req, res) => {
+    game = req.body
+    db.insert(game).into('games')
+        .then(games => {
+            res.status(200).json(games)
+        })
+        .catch(err => { 
+            res.status(500).json({msg: "Hey pal! Don't cha know how to add a game!?"})
+        })
+})
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -9,11 +9,62 @@ describe('BASIC TEST IS RUNNING TEST', () => {
 })
 
 describe('GET', () => {
-    it.skip('should return a status code of 200 after getting users', async () => {
-        const response = await request(server).get('/users');
+    it.skip('should return a status code of 200 after getting games', async () => {
+        const response = await request(server).get('/games');
             
         expect(response.status).toEqual(200); 
     })
+
+    it.skip('should return a blank array, if there are no games stored', async () => {
+        const response = await request(server).get('/games');
+            
+        expect(response.body).toEqual([]); 
+    })
+
+    it('should return a status code of 403 if date of the month is odd', async () => {
+        const response = await request(server).get('/games');
+            
+        expect(response.status).toEqual(403); 
+    })
+})
+
+describe('POST', () => {
+    it.skip('should return a status code of 200 after posting a game', async () => {
+        const response = await request(server)
+            .post('/games')
+            .send({
+                title: "Duck Season",
+                genre: "VR",
+                releaseYear: 2018, 
+            })
+            
+        expect(response.status).toEqual(200); 
+    })
+
+    it('should return a status code of 422 if posting game without required three properties', async () => {
+        const response = await request(server)
+            .post('/games')
+            .send({
+                name: "Tilt",
+                genre: "VR",
+                releaseYear: 2016, 
+            })
+            
+        expect(response.status).toEqual(422); 
+    })
+
+    it('should return a status code of 500 after posting a duplicate game', async () => {
+        const response = await request(server)
+            .post('/games')
+            .send({
+                title: "GTAV",
+                genre: "action/MMO",
+                releaseYear: 2013, 
+            })
+            
+        expect(response.status).toEqual(500); 
+    })
+
 })
 
 
