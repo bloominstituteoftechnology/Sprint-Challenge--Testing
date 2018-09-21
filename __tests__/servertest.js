1c2b58382a701bbcae1d3c7f80a3428364688936
LaikaFusion <newrad4@gmail.com>
2018-09-21 11:37:56 -0400

    games route and tests done

--- __tests__/servertest.js
+++ __tests__/servertest.js
@@ -16,4 +16,33 @@ describe('server.js', () => {
       }));
     });
   });
+
+
+  describe('post games route', () => {
+    it('should 422 if no body', async () => {
+      const expectedStatusCode = 422;
+      const response = await request(server).post('/games');
+      expect(response.status).toEqual(expectedStatusCode);
+    });
+    it('should respond with one object in an array', async () => {
+      const response = await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade', releaseYear:1980 });
+      expect(response.body).toHaveLength(1);
+    });
+    it('should 200 on successful post', async () => {
+      const response = await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade', releaseYear:1980 });
+      expect(response.status).toEqual(200);
+    });
+    it('release year not required', async () => {
+      const response = await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade' });
+      expect(response.status).toEqual(200);
+    });
+    it('should respond with updated game list', async () => {
+      const response = await request(server).post('/games').send({ title: 'Pacman', genre: 'Arcade', releaseYear:1980 });
+      expect(response.body).toEqual(expect.arrayContaining([{
+        title: expect.stringMatching('Pacman'),
+        genre: expect.stringMatching('Arcade'),
+        releaseYear: expect.anything()
+      }]));
+    });
+  });
 })
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
-let store= [];
+let store = [];
 
 app.use(express.json());
 
+app.post("/games", (req, res) => {
+  if (!req.body.title || !req.body.genre) {
+    res.status(422).json({ errorMessage: "Invalid body" });
+    return;
+  }
 
+  if (!req.body.releaseYear) {
+    req.body.releaseYear = 0;
+  }
+  store.push({
+    title: req.body.title,
+    genre: req.body.genre,
+    releaseYear: req.body.releaseYear
+  });
 
+  res.status(200).json(store);
+  return;
+});
 
 app.use("/", (req, res) =>
   res
     .status(404)
     .json({ errorMessage: "You probably want to use a different endpoint" })
 );
-
 
 module.exports = app;
