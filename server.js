ff0cbdd1436ee94b18f511d5d845d137fa70e317
William <kwontaewoongah@gmail.com>
2018-09-21 10:06:57 -0700

    finished get

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express=require('express');
 const server=express();
 server.use(express.json());
 
+let id=1;
+const games=[];
+
+server.get('/games',(req,res)=>{
+    res.status(200).json({games:games});
+})
 module.exports=server;
--- server.spec.js
+++ server.spec.js
@@ -1,3 +1,21 @@
 const request=require('supertest');
 const server=require('./server');
 
+describe('server.js',()=>{
+    describe('games route',()=>{
+        it('should return a 200 status code',async()=>{
+            const expectedStatusCode=200;
+            response=await request(server).get('/games');
+            expect(response.status).toEqual(expectedStatusCode);
+        })
+        it('should return a JSON object from the games route',async()=>{
+            const response=await request(server).get('/games');
+            expect(response.type).toEqual('application/json');
+        })
+        it('should return an array',async()=>{
+            const expectedBody={games:[]};
+            const response=await (request(server)).get('/games');
+            expect(response.body).toEqual(expectedBody);
+        })
+    })
+})
