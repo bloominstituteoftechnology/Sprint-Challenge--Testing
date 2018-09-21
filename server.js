f225cfa7a282b02b4342715ae2a75f86ff0e01fd
Charlie Winslow <cwinslow22@yahoo.com>
2018-09-21 11:12:27 -0500

    completed successful tests for POST /games

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const server = express();
 
 server.use(express.json());
 
-
+server.post('/games', (req, res) => {
+    let title = req.body.title;
+    let genre = req.body.genre;
+    let releaseYear = req.body.releaseYear;
+    if (!title || !genre) {
+        return res.status(422).json({ message: 'Please include both title and genre' })
+    };
+    return res.status(201).json({
+        id: 1,
+        title: `${title}`,
+        genre: `${genre}`,
+        releaseYear: `${releaseYear}`
+    });
+})
 
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -6,4 +6,37 @@ describe('server.js', () => {
     it('should run the tests', () => {
         expect(true).toBeTruthy();
     });
+
+    describe('POST /games', () => {
+        it('returns the correct status code when receiving game data', async () => {
+            const response = await request(server)
+                .post('/games')
+                .send({
+                    title: 'Pacman',
+                    genre: 'Arcade',
+                    releaseYear: '1980'
+                })
+            expect(response.status).toBe(201);
+        });
+
+        it('returns status 422 if required fields are not included', async () => {
+            const response = await request(server)
+                .post('/games')
+                .send({
+                    title: 'Pacman',
+                    releaseYear: '1980'
+                })
+            expect(response.status).toBe(422);
+        });
+
+        it('returns JSON', async () => {
+            const response = await request(server)
+            .post('/games')
+            .send({
+                title: 'Pacman',
+                releaseYear: '1980'
+            })
+        expect(response.type).toBe('application/json');
+        });
+    });
 })
