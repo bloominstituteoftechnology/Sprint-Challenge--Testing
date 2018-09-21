60703480ffcd72bc6d8098f2cfadc7665c7ec10f
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-09-21 13:59:21 -0600

    Added incrementing ID

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const server = express(); 
 server.use(express.json())
 
+let id = 4; 
 const games = [
-    { id: "1", title: "Pacman", genre: "Arcade", releaseYear: 1980 },
-    { id: "2", title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
-    { id: "3", title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
-    { id: "4", title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
+    { id: 1, title: "Pacman", genre: "Arcade", releaseYear: 1980 },
+    { id: 2, title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
+    { id: 3, title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
+    { id: 4, title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
   ];
+
 
 server.get("/games", (req, res) => {
     if(games.length > 0){
@@ -19,17 +21,19 @@ server.get("/games", (req, res) => {
 })
 
 server.post("/games", (req, res) => {
-    const { id, title, genre } = req.body;
+    const { title, genre } = req.body;
   
-    if (!id || !title || !genre) {
-        return res.status(422).end();
+    if (!title || !genre) {
+        return res.status(422).json({error: "Please enter valid request body requirements: id, title, genre"});
     } 
 
     const matching = games.filter(game => game.title === title); 
     if(matching.length > 0){
         return res.status(405).json({error: "Game already exists"})
     }
-
+    id += 1; 
+    req.body.id = id;  
+    games.push(req.body)
     res.status(201).json(req.body);
   });
 
--- server.spec.js
+++ server.spec.js
@@ -1,12 +1,12 @@
 const server = require("./server");
 const request = require("supertest");
 
-// id, title, and genre are required categories, releaseYear is not required
+// title and genre are required categories, releaseYear is not required
 const games = [
-  { id: "1", title: "Pacman", genre: "Arcade", releaseYear: 1980 },
-  { id: "2", title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
-  { id: "3", title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
-  { id: "4", title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
+  { id: 1, title: "Pacman", genre: "Arcade", releaseYear: 1980 },
+  { id: 2, title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 },
+  { id: 3, title: "Assassins Creed", genre: "Action", releaseYear: 2017 },
+  { id: 4, title: "Mario Cart", genre: "Kids Wii Gaming", releaseYear: 2000 }
 ];
 
 describe("Server", () => {
@@ -26,30 +26,43 @@ describe("Server", () => {
   });
 
   describe("POST request to /games", () => {
-
-
     it("should return a status code of 201", async () => {
-        const response = await request(server)
-          .post("/games")
-          .send({ id: "5", title: "PUBG", genre: "Battle Royale", releaseYear: 2018 });
-        expect(response.status).toEqual(201);
-      });
-      it('should return a status code of 422 if information isnt complete', async () => {
-          const response = await request(server).post("/games").send({id: "5", title: "Bla Bla"}); 
-          expect(response.status).toEqual(422); 
-      });
-      it('should return a status code of 405 if client tries to duplicate a game that already exists', async () => {
-          const response = await request(server).post('/games').send({ id: "2", title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 }); 
-          expect(response.status).toEqual(405); 
-      })
-      it('should return a response body type of JSON', async () => {
-          const response = await request(server).post("/games").send({id: "6", title: "Far Cry 5", genre: "EA", releaseYear: 2017}); 
-          expect(response.type).toEqual('application/json'); 
-      });
-      it('should return a response body containing the posted data', async () => {
-          const body = {id: "7", title: "FallOut 4", genre: "EA", releaseYear: 2017}
-          const response = await request(server).post("/games").send(body); 
-          expect(response.body).toEqual(body); 
-      });
-  }); 
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "PUBG", genre: "Battle Royale", releaseYear: 2018 });
+      expect(response.status).toEqual(201);
+    });
+    it("should return a status code of 422 if information isnt complete", async () => {
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "Bla Bla" });
+      expect(response.status).toEqual(422);
+    });
+    it("should return a status code of 405 if client tries to duplicate a game that already exists", async () => {
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "Fortnite", genre: "Battle Royale", releaseYear: 2017 });
+      expect(response.status).toEqual(405);
+    });
+    it("should return a response body type of JSON", async () => {
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "Far Cry 5", genre: "EA", releaseYear: 2017 });
+      expect(response.type).toEqual("application/json");
+    });
+    it("should return a response body containing the posted data", async () => {
+      //ID has to be set to 7 because post requests start at 5 and two tests before this one increment the index,
+      //checking for accurate body response, only test where ID is required in the response obj
+      const body = {
+        id: 7,
+        title: "FallOut 4",
+        genre: "EA",
+        releaseYear: 2017
+      };
+      const response = await request(server)
+        .post("/games")
+        .send(body);
+      expect(response.body).toEqual(body);
+    });
+  });
 });
