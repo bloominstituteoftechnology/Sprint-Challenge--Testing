c6a8ef737e6a4830d0fc1201ce1a9ea4a8527a12
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-21 11:19:15 -0400

    review.md and project setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json('API Running...');
+});
+
+
+module.exports = server;
---
+++ server.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server.js', () => {
+    it('runs the tests', () => {
+        expect(true).toBeTruthy();
+    });
+
+
+
+});
