c030449421074b6ba0c9ef0311ab1d147f2de366
Carey <careycbaldwin@gmail.com>
2018-09-21 12:16:15 -0400

    wrote tests for GET /games
    all failing because no endpoints yet

--- server.js
+++ server.js
@@ -7,15 +7,10 @@ const games = require('./data.js')
 server.use(express.json());
 
 
-
-
-
 server.get('/', (req,res)=> {
 res.status(200).json({api: 'running'});
 });
 
-server.get('/games', (req,res) => {
-    res.status(200).json({games});
-});
+
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
-
+//tests for GET /
 describe ('GET request for /', () => {
     it('returns a 200 status code', async () => {
         //get access to the server
@@ -18,5 +18,56 @@ describe ('GET request for /', () => {
         expect(response.body).toEqual(expectedBody);
     });
 });
+
+//Tests for getting list of games on /games endpoint
+describe('GET request for games', () => {
+
+    it('returns a list of games', async() => {
+        const response = await request(server);
+        response
+        .get('/games')
+        .expect(response.body).toMatchObject([
+                {
+                    id: '1',
+                    name: 'monopoly',
+                    difficulty: 'easy'
+                },     
+                {
+                    id: '2',
+                    name: 'chess',
+                    difficulty: 'hard'
+                },
+                {
+                    id: '3',
+                    name: 'uno',
+                    difficulty: 'easy'
+                },
+                {
+                    id: '4',
+                    name: 'scrabble',
+                    difficulty: 'medium'
+                },
+                {
+                    id: '5',
+                    name: 'checkers',
+                    difficulty: 'easy'
+                }   
+        ])
+    })
+
+    it('returns a 200 status code', () => {
+        const response = await request(server);
+        response
+        .get('/games')
+        .expect(response.status).toEqual(200);
+    })
+
+    it('returns an empty array if no data', () => {
+        const response = await request(server)
+        response
+        .get('/games')
+        .expect(response.body).toMatchObject([]);
+    });
+})
 
 
