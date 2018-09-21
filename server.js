606988682c6655383a38971ae01cbc35044692c8
skennedy93 <sean47900@gmail.com>
2018-09-21 11:34:34 -0500

    GET endpoint and test working

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const server = express();
 
 server.use(express.json());
 
+const games = [
+    { id: "1", title: "Pacman", genre: "Arcade"},
+    { id: "2", title: "Galaga", genre: "Arcade" },
+    { id: "3", title: "Pong", genre: "Arcade" },
+  ];
+
 server.get('/', (req, res) => {
    res.status(200).json('Testing');
 });
+
+server.get("/games", (req, res) => {
+    res.status(200).json(games); 
+})
+
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -1,11 +1,24 @@
 const request = require('supertest');
  const server = require('./server.js');
 
+ const games = [
+    { id: "1", title: "Pacman", genre: "Arcade"},
+    { id: "2", title: "Galaga", genre: "Arcade" },
+    { id: "3", title: "Pong", genre: "Arcade" },
+  ];
 
  describe('server.js', () => {
     it('runs test', () => {
         expect(true).toBeTruthy();
     });
 
+    describe("GET request to /games", () => {
+
+        it("should return the list of games and genre", async () => {
+          const response = await request(server).get("/games");
+          expect(response.body).toEqual(games);
+        });
+
+      });
 
  });
