80b1d0914be1c82908bf710e742557792430fc96
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-21 08:47:39 -0700

    Set up server.spec.js file

--- server.spec.js
+++ server.spec.js
@@ -5,4 +5,28 @@ describe('server', () => {
 	it('run all the tests', () => {
 			expect(true).toBeTruthy();
 	});
+
+	describe('POST /games', () => {
+		it('should take in an object that takes a title, genre and releaseYear', async () => {
+
+		});
+
+		it('should validate that the required fields are included inside the body, return status code 422 if not', async () => {
+
+		});
+
+		it('should verify that the endpoint returns the correct HTTP status code when receiving correct or incorrect data', async () => {
+			
+		});
+	});
+
+	describe('GET /games', () => {
+		it('should return the list of games and HTTP status code 200', async () => {
+
+		});
+
+		it('should always return an array, return an empty array if there are no games stored', async () => {
+			
+		});
+	});
 });
