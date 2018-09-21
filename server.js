d2bffae38343c1ed7562576dd4b3a35bb3df7ae0
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-21 09:42:17 -0700

    Testing GET endpoints.

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+
+let games = [
+		{ title: 'Pacman', genre: 'Arcade', releaseYear: '1980' },
+];
+
+
+server.get('/', (req, res) => {
+	res.status(200).json({ api: 'running' });
+});
+
+server.get('/games', (req, res) => {
+	res.status(200).json(games);
+});
+
+module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -6,6 +6,17 @@ describe('server', () => {
 			expect(true).toBeTruthy();
 	});
 
+	it('should return a JSON object from the index route', async () => {
+		const expectedBody = { api: 'running' };
+		const response = await request(server).get('/');
+		expect(response.body).toEqual(expectedBody);
+	});
+
+	it('should return a JSON object from the index route', async () => {
+		const response = await request(server).get('/');
+		expect(response.type)
+	});
+
 	describe('POST /games', () => {
 		it('should take in an object that takes a title, genre and releaseYear', async () => {
 
@@ -16,17 +27,27 @@ describe('server', () => {
 		});
 
 		it('should verify that the endpoint returns the correct HTTP status code when receiving correct or incorrect data', async () => {
-			
+
 		});
 	});
 
 	describe('GET /games', () => {
-		it('should return the list of games and HTTP status code 200', async () => {
-
+		it('should return HTTP status code 200', async () => {
+			const response = await request(server).get('/games');
+			expect(response.status).toEqual(200);
 		});
 
-		it('should always return an array, return an empty array if there are no games stored', async () => {
-			
+		it('should return an array', async () => {
+			const response = await request(server).get('/games');
+			expect(Array.isArray(response.body)).toBeTruthy();
+		});
+
+		it('should return a list of games', async () => {
+			const response = await request(server).get('/games');
+			const expected = [
+				{ title: 'Pacman', genre: 'Arcade', releaseYear: '1980' },
+			];
+			expect(response.body).toEqual(expected);
 		});
 	});
 });
