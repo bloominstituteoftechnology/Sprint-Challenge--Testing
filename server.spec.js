60d405a9782e9bc44e69dd2b042c83d4c33ef578
skennedy93 <sean47900@gmail.com>
2018-09-21 10:33:06 -0500

    project setup with basic test to make sure it works

---
+++ server.spec.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+ const server = require('./server.js');
+
+
+ describe('server.js', () => {
+    it('runs test', () => {
+        expect(true).toBeTruthy();
+    });
+
+
+ });
