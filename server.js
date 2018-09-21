9c8c7767af16912111fd843a4791e46d802323c9
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-21 11:50:27 -0400

    tests for post /games

--- server.js
+++ server.js
@@ -8,5 +8,13 @@ server.get('/', (req, res) => {
     res.status(200).json('API Running...');
 });
 
+server.post('/games', (req, res) => {
+    const game = req.body;
+    if (!game.title || !game.genre) {
+        res.status(422).json({ error: "Please provide a title and genre for the game." })
+    } else
+        res.status(201).json(game);
+    });
+
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -2,11 +2,58 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
+const defaultGame = {
+    title: 'Pacman',
+    genre: 'Arcade',
+    releaseYear: 1980
+  };
+
 describe('server.js', () => {
-    it('runs the tests', () => {
-        expect(true).toBeTruthy();
+    it('returns a 200 (OK) status code', async () => {
+        const response = await request(server).get('/');
+
+        expect(response.status).toEqual(200);
     });
 
+    describe('POST /games', () => {
+        it('should return a 422 status code if no title is provided', async () => {
+            let game = { ...defaultGame, title: '' }
+
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+
+            expect(response.status).toEqual(422);
+        });
+        it('should return a 422 status code if no genre is provided', async () => {
+            let game = { ...defaultGame, genre: '' }
+
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+
+            expect(response.status).toEqual(422);
+        });
+        it('should return a 201 status code if all required fields are provided', async () => {
+            let game = { ...defaultGame }
+
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+
+            expect(response.status).toEqual(201);
+        });
+        it('should return the provided object', async () => {
+            let game = { ...defaultGame }
+
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+
+            expect(response.body)
+            .toEqual({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+        });
+    });
 
 
 });
