f60d2037e90e9e77abb10dad0d0aecd760487768
szincone <sawyerzincone@gmail.com>
2018-09-21 09:26:53 -0600

    added games db

---
+++ db/games.js
@@ -0,0 +1,32 @@
+const games = [
+  {
+    id: "0",
+    title: "Pacman",
+    genre: "Arcade",
+    releaseYear: 1980,
+  },
+  {
+    id: "1",
+    title: "Skyrim",
+    genre: "Console",
+    releaseYear: 2010,
+  },
+  {
+    id: "2",
+    title: "Pokemon",
+    genre: "Handheld",
+    releaseYear: 1995,
+  },
+];
+
+const newGame = {
+  id: "100",
+  title: "Risk",
+  genre: "Board Game",
+  releaseYear: 1965,
+};
+
+module.exports = {
+  games,
+  newGame,
+};
