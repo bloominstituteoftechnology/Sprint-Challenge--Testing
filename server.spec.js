982ec8f55aa44576844cdbb41b8e5dfae6e5ba77
adamcglee <adamcglee@gmail.com>
2018-09-21 10:53:35 -0700

    added one more test case for POST and cleaned up code structure.

--- server.spec.js
+++ server.spec.js
@@ -5,37 +5,46 @@ describe("server.js", () => {
   it("run the tests", () => {
     expect(true).toBeTruthy();
   });
+
+  describe("GET /games", () => {
+    it("should return a 200 status code", async () => {
+      const response = await request(server).get("/games");
+      expect(response.status).toEqual(200);
+    });
+  
+    it("should return an array", async () => {
+      const response = await request(server).get("/games");
+      expect(Array.isArray(response.body)).toEqual(true);
+    });
+  });
+  
+  describe("POST /games", () => {
+    it("should return a 200 status code", async () => {
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "Pacman", genre: "Arcade", releaseYear: 1980 });
+      expect(response.status).toEqual(200);
+    });
+    it("should return a 422 status code", async () => {
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "", genre: "Arcade", releaseYear: 1980 });
+      expect(response.status).toEqual(422);
+    });
+    it("should return a 422 status code", async () => {
+      const response = await request(server)
+        .post("/games")
+        .send({ title: "Pacman", genre: "", releaseYear: 1980 });
+      expect(response.status).toEqual(422);
+    });
+    it("should return a 200 status code", async () => {
+        const response = await request(server)
+          .post("/games")
+          .send({ title: "Pacman", genre: "Arcade", releaseYear: null });
+        expect(response.status).toEqual(200);
+      });
+  });
+
 });
 
-describe("GET /games", () => {
-  it("should return a 200 status code", async () => {
-    const response = await request(server).get("/games");
-    expect(response.status).toEqual(200);
-  });
 
-  it("should return an array", async () => {
-    const response = await request(server).get("/games");
-    expect(Array.isArray(response.body)).toEqual(true);
-  });
-});
-
-describe("POST /games", () => {
-  it("should return a 200 status code", async () => {
-    const response = await request(server)
-      .post("/games")
-      .send({ title: "Pacman", genre: "Arcade", releaseYear: 1980 });
-    expect(response.status).toEqual(200);
-  });
-  it("should return a 422 status code", async () => {
-    const response = await request(server)
-      .post("/games")
-      .send({ title: "", genre: "Arcade", releaseYear: 1980 });
-    expect(response.status).toEqual(422);
-  });
-  it("should return a 422 status code", async () => {
-    const response = await request(server)
-      .post("/games")
-      .send({ title: "Pacman", genre: "", releaseYear: 1980 });
-    expect(response.status).toEqual(422);
-  });
-});
