5d8024cd9f7d1111066c7792e542dcbafee7f6c0
dasgma <dasgrigoma@gmail.com>
2018-09-21 09:04:45 -0700

    POST/ games  success and error

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running' })
 });
 
+const games = [];
+let id = 0;
+
+server.post('/games', (req, res) => {
+    const {title, genre, releaseYear} = req.body;
+
+    if(!title || !genre) {
+        return res.status(422).json({message: 'Required fields: Title & Genre!!!!!!!!'});
+    };
+
+    const game = {id: id++, title, genre, releaseYear};
+    games.push(game);
+    res.status(201).json(game);
+});
 
 
 
--- server.spec.js
+++ server.spec.js
@@ -1,3 +1,72 @@
 const server = require('./server');
 const request = require('supertest');
 
+describe('server.js', () => {
+    it('Server should be running with status code 200 OK', async() => {
+        const expected = 200;
+        const response = await request(server).get('/');
+
+        expect(response.status).toEqual(expected);
+    });
+
+    describe('POST /games error', () => {
+        it('should return status 422 for not provided title and genre fields', async () => {
+            const game1 = { title: 'MU Origin' };
+            const expected = 422;
+            const response1 = await request(server).post('/games').send(game1);
+                expect(response1.status).toEqual(expected);
+            
+            const game2 = { genre: 'RPG' };
+            const response2 = await request(server).post('/games').send(game2);
+                expect(response2.status).toEqual(expected);
+        });
+
+        it('should return JSON', async () => {
+            const game = { title: 'MU Origin' };
+            const expected = 'application/json';
+            const response = await request(server).post('/games').send(game);
+            
+            expect(response.type).toEqual(expected)
+        });
+
+        it('should return an error', async () => {
+            const game = { title: 'MU Origin' };
+            const expected = { message: 'Required fields: Title & Genre!!!!!!!!' };
+            const response = await request(server).post('/games').send(game);
+      
+            expect(response.body).toEqual(expected);
+        });
+    });
+
+    describe('POST /games posted successfully', () => {
+        let response;
+
+        beforeEach(async () => {
+            let game = {
+                title: 'Pacman',
+                genre: 'Arcade',
+                releaseYear: 1980 
+            };
+
+            response = await request(server).post('/games').send(game);
+        });
+
+        it('should return status 201', () => {
+            const expected = 201;
+
+            expect(response.status).toEqual(expected);
+        });
+
+        it('should return JSON', () => {
+            const expected = 'application/json';
+
+            expect(response.type).toEqual(expected);
+        });
+
+        it('should have an id field', () => {
+            expect(response.body.id).toBeDefined();
+            expect(typeof response.body.id).toBe('number');
+        });
+      });
+
+});
