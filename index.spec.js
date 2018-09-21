ea80cb29c9105d002b8e7b225ac4c9bd004a0049
mkerbleski <mkerbles@gmail.com>
2018-09-21 10:38:21 -0600

    complete POST test suite

--- index.spec.js
+++ index.spec.js
@@ -32,25 +32,35 @@ describe('server GET requests', () => {
     })
    
 
-    it('should return an empty array if no game exist from "/games" endpoint', async () => {
-        const response = await request(server).get('./games');
+    // it('should return an empty array if no game exist from "/games" endpoint', async () => {
+    //     const response = await request(server).get('./games');
 
-        expect(response.body).toEqual([])
-    })
+    //     expect(response.body).toEqual([])
+    // })
 
 })
 
 describe('server POST requests', () => {
-    test('should return a 200 status code if includes a title and genere', () => {
-      
+    test('should return a 200 status code if includes a title and genere', async () => {
+        const completeRequest = { title: 'frogger', genere: 'arcade'}
+        const response = await request(server).post('/games').send(completeRequest)
+  
+        expect(response.status).toEqual(200)
     })
 
-    test('should return a 422 status code title or genere are excluded from request', () => {
-      
+    test('should return an requested object as response', async () => {
+        const completeRequest = { title: 'frogger', genere: 'arcade'}
+        const response = await request(server).post('/games').send(completeRequest)
+  
+        expect(response.body).toEqual(completeRequest)
+    })
+
+    test('should return a 422 status code title or genere are excluded from request', async () => {
+        const missingTitle = { genere: 'arcade'}
+        const response = await request(server).post('/games').send(missingTitle)
+  
+        expect(response.status).toEqual(422)
     })
     
-    test('should return a 422 status code title or genere are excluded from request', () => {
-      
-    })
     
 })
