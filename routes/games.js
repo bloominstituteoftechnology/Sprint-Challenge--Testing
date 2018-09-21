5199a19c3dde854d0c88edb6b8961750c60326fb
kamalnrf <kamalnrf@hotmail.com>
2018-09-21 21:07:19 +0530

    Added Get games route

--- routes/games.js
+++ routes/games.js
@@ -1,8 +1,29 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const gameStore = [
+  {
+    id: 1,
+    title: "Pacman",
+    genre: "Arcade",
+    releaseYear: 1978
+  },
+  {
+    id: 2,
+    title: "Space Invader",
+    genre: "Shooter Game",
+    releaseYear: 1980
+  },
+  {
+    id: 3,
+    title: "Gravitar",
+    genre: "Arcade",
+    releaseYear: 1982
+  }
+];
 
-})
+router.get("/", (req, res, next) =>
+  res.status(200).json({ status: true, games: gameStore })
+);
 
-module.exports = router
+module.exports = router;
--- tests/games.test.js
+++ tests/games.test.js
@@ -9,3 +9,18 @@ describe('Index Route', () => {
         expect(res.body.status).toBe(true)
     })
 })
+
+describe('Games Route', () => {
+    describe('GET /api/games', () => {
+        it ('should check for respose', async () => {
+            const res = await request(server).get('/api/games')
+            expect(res.status).toBe(200)
+        })
+
+        it ('should check for games in response', async () => {
+            const res = await request(server).get('/api/games')
+            expect(res.body.status).toBe(true)
+            expect(typeof res.body.games).toBe('object')
+        })
+    })
+})
