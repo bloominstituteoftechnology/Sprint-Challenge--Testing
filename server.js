1fc1491b2e0a3323df8fc40d4cf7f189ff09c9b4
szincone <sawyerzincone@gmail.com>
2018-09-21 09:24:16 -0600

    added index.js and server.js plumbing, server up and running with basic GET

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+// test endpoint
+server.get("/", (req, res) => {
+  const passedBody = { api: "running" };
+  res.status(200).json(passedBody);
+});
+
+module.exports = server;
