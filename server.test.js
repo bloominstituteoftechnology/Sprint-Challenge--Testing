b1c9145e3e64ef9b930357b790dfb2cf9a1236eb
Amanda Phillips <amanda@phillipsdev.org>
2018-10-12 08:15:11 -0500

    removed some console.logs

--- server.test.js
+++ server.test.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose');
-const chai = require('chai');
+const mongoose = require("mongoose");
+const chai = require("chai");
 const { expect } = chai;
-const sinon = require('sinon');
-const server = require('./server');
+const server = require("./server");
 
-const chaiHTTP = require('chai-http');
+const chaiHTTP = require("chai-http");
 
-const Game = require('./models');
+const Game = require("./models");
 chai.use(chaiHTTP);
 
-describe('Games', () => {
+describe("Games", () => {
   before(done => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/test');
+    mongoose.connect("mongodb://localhost/test");
     const db = mongoose.connection;
-    db.on('error', () => console.error.bind(console, 'connection error'));
-    db.once('open', () => {
-      console.log('we are connected');
+    db.on("error", () => console.error.bind(console, "connection error"));
+    db.once("open", () => {
+      console.log("we are connected");
       done();
     });
   });
@@ -24,7 +23,7 @@ describe('Games', () => {
   after(done => {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
-      console.log('we are disconnected');
+      console.log("we are disconnected");
     });
   });
   // declare some global variables for use of testing
@@ -36,9 +35,9 @@ describe('Games', () => {
     // each time this hook runs, you should save a document to your db
     // by saving the document you'll be able to use it in each of your `it` blocks
     const myGame = new Game({
-      title: 'Donkey Kong',
-      genre: 'Platformer',
-      releaseDate: 'July 1981',
+      title: "Donkey Kong",
+      genre: "Platformer",
+      releaseDate: "July 1981"
     });
     myGame
       .save()
@@ -61,20 +60,19 @@ describe('Games', () => {
   });
 
   // test the POST here
-  describe('[POST] /api/game/create', () => {
-    it('should add a new game', done => {
+  describe("[POST] /api/game/create", () => {
+    it("should add a new game", done => {
       const myGame = {
-        title: 'Super Mario Bros 2',
-        genre: 'Platformer',
-        releaseDate: 'Sept 1985',
+        title: "Super Mario Bros 2",
+        genre: "Platformer",
+        releaseDate: "Sept 1985"
       };
       chai
         .request(server)
-        .post('/api/game/create')
+        .post("/api/game/create")
         .send(myGame)
         .end((err, res) => {
           if (err) {
-            console.log(err);
             done();
           }
           expect(res.status).to.equal(200);
@@ -85,19 +83,17 @@ describe('Games', () => {
   });
 
   // test the GET here
-  describe('[GET] /api/game/get', () => {
-    it('should get all games', done => {
+  describe("[GET] /api/game/get", () => {
+    it("should get all games", done => {
       chai
         .request(server)
-        .get('/api/game/get')
+        .get("/api/game/get")
         .end((err, res) => {
           if (err) {
             console.error(err);
             done();
           }
           expect(res.body[0].title).to.eql(testGame.title);
-          // console.log(gameId.toString());
-          // console.log(res.body[0]._id);
           expect(res.body[0]._id).to.equal(gameId.toString());
         });
       done();
@@ -105,19 +101,18 @@ describe('Games', () => {
   });
 
   // test the PUT here
-  describe('[PUT] /api/game/update', () => {
-    it('should update a game with the given id', done => {
+  describe("[PUT] /api/game/update", () => {
+    it("should update a game with the given id", done => {
       const gameUpdate = {
         id: gameId,
-        title: 'Super Mario Bros',
+        title: "Super Mario Bros"
       };
       chai
         .request(server)
-        .put('/api/game/update')
+        .put("/api/game/update")
         .send(gameUpdate)
         .end((err, res) => {
           if (err) {
-            console.log(err);
             done();
           }
           expect(res.body.title).to.equal(gameUpdate.title);
@@ -128,22 +123,20 @@ describe('Games', () => {
   // --- Stretch Problem ---
   // Test the DELETE here
 
-  describe('[DELETE] /api/game/destroy/:id', () => {
-    it('should remove the specified game from the database', done => {
+  describe("[DELETE] /api/game/destroy/:id", () => {
+    it("should remove the specified game from the database", done => {
       chai
         .request(server)
         .delete(`/api/game/destroy/${gameId}`)
         .end((err, res) => {
           if (err) {
-            console.log(err);
             done();
           }
           expect(res.text).to.equal(
-            '{"success":"Donkey Kong was removed from the DB"}',
+            '{"success":"Donkey Kong was removed from the DB"}'
           );
           Game.findById(gameId, (err, deletedGame) => {
             if (err) {
-              console.log(err);
               done();
             }
             expect(deletedGame).to.equal(null);
