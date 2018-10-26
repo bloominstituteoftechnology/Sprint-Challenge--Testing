a1ec18208dc3942f539853c435437c9b91d6886a
Alexandra <alexswartz9753@gmail.com>
2018-10-26 09:55:34 -0400

    built index route and tested it

---
+++ index.js
@@ -0,0 +1,3 @@
+const server = require('./server.js');
+
+server.listen(8000, ()=> console.log(`API running on port 8000`));
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res)=> {
+    res.status(200).json({message: "Hi :)"});
+});
+
+module.exports = server;
