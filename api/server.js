45296e06dd0a09ba9ef339baa831ab245e4fa7d1
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-10-26 09:49:51 -0600

    Implement Mock Data Resources
     - games is an array of objects.
     - id is meant to be used to increment when adding new object to the
       games array.

--- api/server.js
+++ api/server.js
@@ -5,10 +5,24 @@ const helmet = require('helmet');
 // --- Instantiate Server ---
 const server = express();
 
-// --- Implement Middleware
+// --- Implement Middleware ---
 server.use(express.json(), helmet());
 
+// --- Data Resource ---
+let games = [
+    {
+        id : 0,
+        title: 'Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+      }
+]
+
+let id = 0;
+
 /// ----- CRUD Endpoints -----
+
+// --- Test Endpoint ---
 server.get('/testmebaby/149', (request, response) => {
     response.status(200).json('One More Time');
 })
