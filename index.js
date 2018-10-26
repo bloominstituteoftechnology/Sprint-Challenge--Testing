e15ae39c22b641a2e54cee13eba319d893b4e11f
Anthony Greb <agreb255@gmail.com>
2018-10-26 12:27:42 -0400

    get tests are passing

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const server = require("./api/server.js");
 
-server.listen(8000, () => console.log("/nserver up on port 8000/n"));
+server.listen(8000, () => console.log("/n server up on port 8000 /n"));
--- index.spec.js
+++ index.spec.js
@@ -2,8 +2,56 @@ const request = require("supertest");
 
 const server = require("./api/server.js");
 
+const games = [
+  {
+    id: "1",
+    title: "Final Fantasy VII",
+    genre: "RPG",
+    releaseDate: 1 / 31 / 1997
+  },
+  {
+    id: "2",
+    title: "Final Fantasy VIII",
+    genre: "RPG",
+    releaseDate: 2 / 11 / 1999
+  },
+  {
+    id: "3",
+    title: "Final Fantasy IX",
+    genre: "RPG",
+    releaseDate: 7 / 7 / 2000
+  },
+  {
+    id: "4",
+    title: "Final Fantasy X",
+    genre: "RPG",
+    releaseDate: 7 / 19 / 2001
+  },
+  {
+    id: "5",
+    title: "The Witcher 3",
+    genre: "RPG",
+    releaseDate: 5 / 19 / 2015
+  }
+];
+
 describe("server", () => {
   it("can run tests", () => {
     expect(true).toBeTruthy();
+  });
+});
+
+describe("GET /games", () => {
+  it("should return and array", async () => {
+    const response = await request(server).get("/games");
+    expect(Array.isArray(response.body)).toBeTruthy();
+  });
+  it("should return a 200 status", async () => {
+    const response = await request(server).get("/games");
+    expect(response.status).toEqual(200);
+  });
+  it("should gimme my games", async () => {
+    const response = await request(server).get("/games");
+    expect(response.body).toEqual(games);
   });
 });
