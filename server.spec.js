d267503437c374b39b386f28dc7436bdfccedb2f
Eric B <timberwolf.eric@gmail.com>
2018-10-26 14:28:49 -0500

    Added some GET endpoints tests

--- server.spec.js
+++ server.spec.js
@@ -32,3 +32,23 @@ describe("POST endpoint for /games API", () => {
         expect(response.type).toBe("application/json");
       });
 })
+
+// GET endpoints Tests
+
+describe ("GET endpoint for /games API", () => {
+  it('should return status code 200', async () => {
+    const response = await request(server).get('/games');
+    expect(response.status).toBe(200);
+  });
+
+  it('should return JSON', async () => {
+    const response = await request(server).get('/games');
+    expect(response.type).toBe('application/json');
+  });
+
+  it('should return a game title', async () => {
+    const expected = 'Pacman';
+    const response = await request(server).get('/games');
+    expect(response.body.title).toEqual(expected);
+  });
+})
