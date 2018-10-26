e3bff826c62f35da70724621d6bde2d3017d7e2d
Clint Kunz <clintkunz@gmail.com>
2018-10-26 09:30:15 -0600

    test to see if status is 200 on get request

--- index.spec.js
+++ index.spec.js
@@ -4,8 +4,13 @@ const request = require('supertest');
 describe('API testing', () => {
     describe('GET', () => {
         it('should send a list of games in an array', async () => {
-            const response = await request(server).get('/');
+            const response = await request(server).get('/games');
             expect(Array.isArray(response.body)).toBe(true);
+        })
+
+        it('should send a status of 200', async () => {
+            const response = await request(server).get('/games');
+            expect(response.status).toBe(200);
         })
     })
 })
