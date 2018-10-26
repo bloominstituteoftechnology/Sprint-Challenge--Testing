3d020a4502aa9aba1a2e59064f888a822c48bdab
Anthony Greb <agreb255@gmail.com>
2018-10-26 11:06:34 -0400

    initial commit + server up, dependencies installed, files created

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+//sanity check
+server.get("/", (req, res) => {
+  res.send("server is up");
+});
+
+module.exports = server;
