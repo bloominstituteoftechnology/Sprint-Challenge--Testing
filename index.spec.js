422a88e5df210951d8719bac9590bb82d16ce3e8
Nicocchi <nicocchilover@gmail.com>
2018-10-26 11:24:20 -0400

    GET /games test wrote

--- index.spec.js
+++ index.spec.js
@@ -19,5 +19,43 @@ describe('Server', () => {
         });
     });
 
-    
+    describe('GET /games', () => {
+        it('returns status code 200(OK)', async () => {
+            const res = await request(server).get('/games');
+            expect(res.status).toBe(200);
+        });
+        
+        it('returns JSON', async () => {
+            const res = await request(server).get('/games');
+            expect(res.type).toBe('application/json');
+        });
+
+        it('returns an array of objects of games', async () => {
+            const expected = [
+                {
+                    title: 'Pacman', // required
+                    genre: 'Arcade', // required
+                    releaseYear: 1980 // not required
+                },
+                {
+                    title: 'Rocket Knight Adventures', // required
+                    genre: 'Platformer', // required
+                    releaseYear: 1993 // not required
+                },
+                {
+                    title: 'Metal Slug', // required
+                    genre: 'Platformer', // required
+                    releaseYear: 1996 // not required
+                },
+                {
+                    title: 'Sonic R', // required
+                    genre: 'Racing', // required
+                    releaseYear: 1997 // not required
+                }
+                
+            ]
+            const res = await request(server).get('/games');
+            expect(res.body).toBe(expected);
+        });
+    });
 });
