5dcc6fe238430a30a2ea01d44e2c9638544e92bd
ckirkwood94 <ckirkwood94@gmail.com>
2018-10-26 12:49:12 -0400

    Resolved error when test ran multiple times from --watch

--- api/server.js
+++ api/server.js
@@ -3,8 +3,6 @@ const helmet = require('helmet');
 
 const gamesRoutes = require('./games/gamesRoutes');
 
-const port = 9001;
-
 const server = express();
 
 // MIDDLEWARE
@@ -14,4 +12,4 @@ server.use(express.json());
 // ROUTES
 server.use('/games', gamesRoutes);
 
-server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
+module.exports = server;
