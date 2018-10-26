658c21e4d78600f90553853f589a475c478bb139
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-26 12:20:03 -0400

    added response code and array length tests to GET test suite, received the appropriate failures

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -2,10 +2,23 @@ const server = require('../server.js');
 const request = require('supertest');
 
 describe('test suites for basic API operations', () => {
+
   describe('GET tests', () => {
+
     test('should return games list as an array', async () => {
       const response = await request(server).get('/');
       expect(Array.isArray(response.body)).toBe(true);
     });
+
+    test('status code returned should be 200', async () => {
+      const response = await request(server).get('/games');
+      expect(response.status).toBe(200);
+    });
+
+    test('game list length should be 5', async () => {
+      const response = await request(server).get('/games');
+      expect(response.body.length).toBe(5);
+    });
+
   });
 });
