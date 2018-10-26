739bcbec539db92f2bdd0d8e32e7026531be1f63
Clint Kunz <clintkunz@gmail.com>
2018-10-26 09:38:52 -0600

    test on POST to return 422 error if incorrect body sent to API

--- index.spec.js
+++ index.spec.js
@@ -6,16 +6,24 @@ describe('API testing', () => {
         it('should send a list of games in an array', async () => {
             const response = await request(server).get('/games');
             expect(Array.isArray(response.body)).toBe(true);
-        })
+        });
 
         it('should send a status of 200', async () => {
             const response = await request(server).get('/games');
             expect(response.status).toBe(200);
-        })
+        });
 
         it('should return a list of games with the length of 3', async () => {
             const response = await request(server).get('/games');
             expect(response.body.length).toBe(3);
+        });
+    });
+
+    describe('POST', () => {
+        it('should return an error of 422 if the right data is not sent to API', async () => {
+            const game = {"game": "Battleship"}
+            const response = await request(server).post('/games').send(game);
+            expect(response.status).toBe(422);
         })
-    })
-})
+    });
+});
