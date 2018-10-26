5a066d33cb9f17a7bcfbde30645589c5984f519a
Vera Simon <ghjkl>
2018-10-26 08:40:37 -0700

    Sprint Challenge: Testing - server.spec.js
    Updated the GET non-existant path test suite to be more inline with the project

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -5,24 +5,19 @@ const { statusObj } = require('../errorHandler/handlers');
 
 describe('~~ server.js ~~', () => {
 	describe('~~ GET something without a route ~~', () => {
-		it('should return status 404 (not found) when browsing to "/"', async () => {
-			const response = await request(server).get('/');
+		it('should return status 404 (not found) when browsing to "/games/gibberish"', async () => {
+			const response = await request(server).get('/games/gibberish');
 			expect(response.status).toBe(404);
 		});
 
-		// without async
-		// it('should return status 404 (not found) when browsing to "/"', () => {
-		//     return request(server).get('/').then((res) => expect(res.status).toBe(404));
-		// });
-
 		it('should return JSON', async () => {
-			const response = await request(server).get('/');
+			const response = await request(server).get('/games/gibberish');
 			expect(response.type).toBe('application/json');
 		});
 
-		it('should return a custom JSON 404 status object when GET "/"', async () => {
-			const expected = statusObj('h404', `The requested path '/' doesn't exist.`);
-			const response = await request(server).get('/');
+		it('should return a custom JSON 404 status object when GET "/games/gibberish"', async () => {
+			const expected = statusObj('h404', `The requested path '/games/gibberish' doesn't exist.`);
+			const response = await request(server).get('/games/gibberish');
 			expect(response.body).toEqual(expected);
 		});
 	});
