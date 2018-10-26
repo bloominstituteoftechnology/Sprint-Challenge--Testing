a3bd1f4ebb8bc209da1f7923b71d67452d520e5d
Max <maxkajiwara@gmail.com>
2018-10-26 09:39:14 -0700

    Add tests

--- server.spec.js
+++ server.spec.js
@@ -5,7 +5,7 @@ const server = require('./server');
 
 describe('server', () => {
 	describe('POST /games', () => {
-		it('should add game to the database', async () => {
+		it('should return JSON message', async () => {
 			const game = {
 				title: 'Pacman',
 				genre: 'Arcade',
@@ -18,6 +18,49 @@ describe('server', () => {
 				.send(game);
 
 			expect(response.body).toEqual(expected);
+		});
+
+		it('should return 201 status code when game is successfully added', async () => {
+			const game = {
+				title: 'Pacman',
+				genre: 'Arcade',
+				releaseYear: 1980
+			};
+			const expected = 201;
+
+			const response = await request(server)
+				.post(`/games`)
+				.send(game);
+
+			expect(response.status).toEqual(expected);
+		});
+
+		it('should return 422 status code when title not provided', async () => {
+			const game = {
+				genre: 'Arcade',
+				releaseYear: 1980
+			};
+			const expected = 422;
+
+			const response = await request(server)
+				.post(`/games`)
+				.send(game);
+
+			expect(response.status).toEqual(expected);
+		});
+
+		it('should return 422 status code when genre not provided', async () => {
+			const game = {
+				title: 'Pacman',
+				releaseYear: 1980
+			};
+			const expected = 422;
+
+			const response = await request(server)
+				.post(`/games`)
+				.send(game);
+
+			expect(response.status).toEqual(expected);
 		});
 	});
 
