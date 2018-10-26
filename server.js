495b482b4980fcd6e55be4cae198dd1f64ca226a
Timothy Hoang <timh1203@gmail.com>
2018-10-26 13:13:38 -0400

    Add tests to GET route

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const server = express();
-
 server.use(express.json());
+
+let db = [
+  {
+    name: 'Game 1',
+    breed: 'Action',
+    releaseYear: '1991'
+  },
+  {
+    name: 'Game 2',
+    breed: 'Sci-Fi',
+    releaseYear: '1992'
+  },
+  {
+    name: 'Game 3',
+    breed: 'Adventure',
+    releaseYear: '1993'
+  }
+];
+
+let emptyDb = [];
 
 server.route('/')
   .get((req, res) => res.status(200).json({ message: 'En vivo!' }))
+
+server.route('/games')
+  .get((req, res) => {
+    // if (emptyDb.length < 1) return res.status(404).json(emptyDb);
+    return res.status(200).json(db)
+  })
 
 server.route('/games')
   .post((req, res) => {
--- server.test.js
+++ server.test.js
@@ -33,4 +33,42 @@ describe('server', () => {
       expect(response.status).toBe(500);
     })
   })
+
+  describe('GET /games ROUTE', () => {
+    it('return 200 with list of games', async () => {
+      const response = await request(server).get('/games');
+      const expected = [
+        {
+          name: 'Game 1',
+          breed: 'Action',
+          releaseYear: '1991'
+        },
+        {
+          name: 'Game 2',
+          breed: 'Sci-Fi',
+          releaseYear: '1992'
+        },
+        {
+          name: 'Game 3',
+          breed: 'Adventure',
+          releaseYear: '1993'
+        }
+      ]
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(expected);
+    })
+    it('always returns an array, even if no games stored', async () => {
+      const response = await request(server).get('/games');
+      expect(Array.isArray(response.body)).toBe(true);
+    })
+    it('returns an empty array, even if no games stored', async () => {
+      const response = await request(server).get('/games');
+
+      // Is there a better way to write this other than setting db = []?
+      response.body = [];
+
+      const expected = [];
+      expect(response.body).toEqual(expected);
+    })
+  })
 })
