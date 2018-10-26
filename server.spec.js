2e3f86f21d8f8dbec0c63b48c10579a1d98dbb64
Eric B <timberwolf.eric@gmail.com>
2018-10-26 13:41:05 -0500

    Wrote some POST tests

---
+++ server.spec.js
@@ -0,0 +1,34 @@
+const request = require("supertest");
+
+const server = require ("./server.js");
+
+// POST Tests
+describe("POST endpoint for /games API", () => {
+    it("should POST game(s) to games array", async () => {
+        const title = "Pacman";
+        const genre = "Arcade";
+
+        const expected = { title: "Pacman", genre: "Arcade" };
+        const response = await request(server).post("/games").send({ title, genre });
+        expect(response.body).toEqual(expected);
+    })
+
+    it("should return status code 200", async () => {
+      const title = "Street Fighter 2";
+      const genre = "Arcade";
+
+      const response = await request(server)
+        .post("/games").send({ title, genre });
+        expect(response.status).toEqual(200);
+      });
+
+      it("should return status 422 if bad POST request", async () => {
+        const response = await request(server).post("/games");
+        expect(response.status).toEqual(422);
+      });
+
+      it("should return JSON", async () => {
+        const response = await request(server).post("/games");
+        expect(response.type).toBe("application/json");
+      });
+})
