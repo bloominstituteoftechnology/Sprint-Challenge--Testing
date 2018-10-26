dc51bba3cec381af8a37ce5205c67841f4eb3267
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-10-26 08:23:39 -0700

    MVP: GET endpoint done, will add more later

--- api/server.js
+++ api/server.js
@@ -10,4 +10,22 @@ server.get('/', (req, res) => {
   res.status(200).json({ message: "Server running" });
 });
 
+server.get('/games', (req, res) => {
+  db('games')
+    .then(games => {
+
+      if (!games || games.length < 1) {
+        res.status(404).json(games)
+      } else {
+        res.status(200).json(games);
+      }
+
+    })
+    .catch(err => res.status(500).json({ error: 'Could not retrieve games list' }, err));
+});
+
+server.post('/games', (req, res) => {
+  
+})
+
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -5,4 +5,34 @@ describe('SERVER', () => {
   it('should run tests', () => {
     expect(true).toBeTruthy();
   });
+
+  describe('GET /games', () => {
+    it('should return statusCode = 404 when games list is empty', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.status).toBe(404);
+    });
+
+    it('should return the games array even if it is empty', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.body).toEqual([]);
+    });
+
+    it('should return JSON', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.type).toBe('application/json');
+    });
+
+    // it('should return statusCode = 200 when games list is not empty', async () => {
+    //   const response = await request(server).get('/games');
+
+    //   expect(response.status).toBe(200);
+    // });
+  });
+
+  describe('POST /games', () => {
+    
+  });
 });
