0e26296c53540a8c8f72a97132c8c6529d35a247
Carlos Vargas <cornielleandres@gmail.com>
2018-10-26 11:40:43 -0400

    Write tests for POST /games/

--- api/server.spec.js
+++ api/server.spec.js
@@ -44,4 +44,76 @@ describe('server.js', () => {
 			expect(response.body).toEqual(expected);
 		});
 	});
+
+	describe('POST /games/', () => {
+		describe('calling with all necessary info', () => {
+			afterEach(() => db.migrate.rollback()
+				.then(() => db.migrate.latest())
+				.then(() => db.seed.run())
+			);
+
+			it('should return status 201(Created)', async () => {
+				const response = await request(server).post('/games/');
+				expect(response.status).toBe(201);
+			});
+
+			it('should return JSON', async () => {
+				const response = await request(server).post('/games/');
+				expect(response.type).toBe('application/json');
+			});
+
+			it('should return the newly inserted game', async () => {
+				const newGame = { 'title': 'newGameTitle', 'genre': 'newGameGenre', 'releaseYear': 1990, };
+				const response = await request(server).post('/games/').send(newGame);
+				newGame[id] = 4;
+				const expected = [ newGame ];
+				expect(Array.isArray(response.body)).toBe(true);
+				expect(response.body.length).toEqual(1);
+				expect(response.body).toEqual(expected);
+			});
+
+			it('should return the newly inserted game with release year set to null', async () => {
+				const newGame = { 'title': 'newGameTitle', 'genre': 'newGameGenre' };
+				const response = await request(server).post('/games/').send(newGame);
+				newGame[id] = 4;
+				const expected = [ newGame ];
+				expect(Array.isArray(response.body)).toBe(true);
+				expect(response.body.length).toEqual(1);
+				expect(response.body).toEqual(expected);
+			});
+		});
+
+		describe('calling without all necessary info', () => {
+			afterEach(() => db.migrate.rollback()
+				.then(() => db.migrate.latest())
+				.then(() => db.seed.run())
+			);
+
+			it('should return status 422(Unprocessable Entity (WebDAV))', async () => {
+				const response = await request(server).post('/games/');
+				expect(response.status).toBe(422);
+			});
+
+			it('should return JSON', async () => {
+				const response = await request(server).post('/games/');
+				expect(response.type).toBe('application/json');
+			});
+
+			it('should return an error message when genre is missing', async () => {
+				const newGame = { 'title': 'newGameTitle' };
+				const response = await request(server).post('/games/').send(newGame);
+				const expected = { error: 'Game must have title and genre.' };
+				expect(typeof(response.body)).toBe('object');
+				expect(response.body).toEqual(expected);
+			});
+
+			it('should return an error message when title is missing', async () => {
+				const newGame = { 'genre': 'newGameGenre' };
+				const response = await request(server).post('/games/').send(newGame);
+				const expected = { error: 'Game must have title and genre.' };
+				expect(typeof(response.body)).toBe('object');
+				expect(response.body).toEqual(expected);
+			});
+		});
+	});
 });
