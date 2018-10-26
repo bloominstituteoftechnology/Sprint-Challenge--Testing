0c5f6cb05b01046a3b66afe60b01277d654f0a68
Timothy Hoang <timh1203@gmail.com>
2018-10-26 12:33:43 -0400

    Add tests to POST route

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const server = express();
 
 server.use(express.json());
 
+server.route('/')
+  .get((req, res) => res.status(200).json({ message: 'En vivo!' }))
+
+server.route('/games')
+  .post((req, res) => {
+    const { title, genre, releaseYear } = req.body;
+    if (!title || !genre) return res.status(422).json({ error: 'title and genre required.' })
+    if (typeof title !== 'string' || typeof genre !== 'string') return res.status(500).json({ error: 'title and genre needs to be strings.' })
+    return res.status(201).json({ success: 'successfully created' });
+  })
+
 module.exports = server;
--- server.test.js
+++ server.test.js
@@ -5,7 +5,32 @@ describe('server', () => {
   it('can run tests', () => {
     expect(1).toBeTruthy();
   })
-  it('can run more tests', () => {
-    expect(0).toBeFalsy();
+
+  describe('POST /games ROUTE', () => {
+    it('has title and genre fields included', async () => {
+      const response = await request(server)
+        .post('/games')
+        .send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+      expect(response.body.title).toBeDefined;
+      expect(response.body.genre).toBeDefined;
+    })
+    it('returns 422 if title and genre is incomplete', async () => {
+      const response = await request(server)
+        .post('/games')
+        .send({ title: 'Pacman' });
+      expect(response.status).toBe(422);
+    })
+    it('returns 201 with correct game data types', async () => {
+      const response = await request(server)
+        .post('/games')
+        .send({ title: 'Pacman', genre: 'Arcade', releaseYear: 1980 });
+      expect(response.status).toBe(201);
+    })
+    it('returns 500 with incorrect game data types', async () => {
+      const response = await request(server)
+        .post('/games')
+        .send({ title: 123, genre: 234, releaseYear: 1980 });
+      expect(response.status).toBe(500);
+    })
   })
 })
