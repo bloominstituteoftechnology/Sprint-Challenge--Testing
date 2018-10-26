1c96d6b23d77bec375f5f7a33aca492208287cb4
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-10-26 12:08:50 -0400

    GET tests: - array test - json test - 200 status

--- server.spec.js
+++ server.spec.js
@@ -44,3 +44,22 @@ describe("POST /games", () => {
 });
 
 //GET Tests
+describe("GET /games", () => {
+  it("should return an array", async () => {
+    const getGames = await request(server).get("/games");
+
+    expect(Array.isArray(getGames.body)).toBe(true);
+  });
+
+  it("should return status code 200", async () => {
+    const response = await request(server).get("/games");
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should return JSON", async () => {
+    const response = await request(server).get("/games");
+
+    expect(response.type).toBe("application/json");
+  });
+});
