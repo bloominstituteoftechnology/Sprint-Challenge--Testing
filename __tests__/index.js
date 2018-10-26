7699782473d3e0a3fcf46e9504b32aa127edac46
Luisan <luisangelsc@gmail.com>
2018-10-26 11:51:13 -0500

    POST /games. 200 and 422 for incomplete info. missing wrong info

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,15 +3,74 @@ const request = require('supertest');
 const server = require('../api/server.js')
 
 describe('server.js', () => {
-  describe('root endpoint (/)', () => {
+  describe.skip('root endpoint (/)', () => {
 
-    it('should return something', async () => {
+    it('should return confirmation the server is up', async () => {
       const expected = { api: 'running' }
       const response = await request(server).get('/')
       expect(response.body).toEqual(expected)
     })
 
+  })
 
+  describe('POST /games endpoint', () => {
+
+    const newGame = {
+      title: 'Pacman',
+      genre: 'Arcade',
+      releaseYear: 1980
+    }
+
+    const trivialIncompleteGame = {
+      title: 'Pacman',
+      genre: 'Arcade',
+    }
+
+    const incompleteGame1 = {
+      title: 'Pacman',
+      releaseYear: 1980
+    }
+
+    const incompleteGame2 = {
+      genre: 'Arcade',
+      releaseYear: 1980
+    }
+
+    const incorrectGame = {
+      title: 12345,
+      genre: true,
+      releaseYear: 'nineteen eighty-four'
+    }
+
+    it('should return status code 200 when successful', async () => {
+      const expected = 200
+      const response = await request(server).post('/games').send(newGame)
+      expect(response.status).toBe(expected)
+    })
+
+    it('should return status code 200 when sent without releaseYear', async () => {
+      const expected = 200
+      const response = await request(server).post('/games').send(trivialIncompleteGame)
+      expect(response.status).toBe(expected)
+    })
+
+    it('should return status code 422 if info is incomplete', async () => {
+      const expected = 422
+      const response = await request(server).post('/games').send(incompleteGame1)
+      expect(response.status).toBe(expected)
+    })
+
+    it('should return status code 422 if info is incomplete', async () => {
+      const expected = 422
+      const response = await request(server).post('/games').send(incompleteGame2)
+      expect(response.status).toBe(expected)
+    })
+
+    it.only('should return status code 422 if info is incomplete', async () => {
+      const expected = 300
+      const response = await request(server).post('/games').send(incorrectGame)
+      expect(response.status).toBe(expected)
+    })
 
   })
 })
--- api/server.js
+++ api/server.js
@@ -4,8 +4,34 @@ const server = express();
 
 server.use(express.json())
 
+// const db = require('../data/helpers/models.js')
+
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.post('/games', (req, res) => {
+  const { title, genre } = req.body
+  const releaseYear = req.body.releaseYear || null
+  const game = { title, genre }
+  console.log(game);
+
+  if (title && genre) {
+    res.status(200).json(game)
+  } else {
+    res.status(422).json(game)
+  }
+
+
+  // db.insert(game)
+  //   .then( response => {
+  //     res.status(200).json(game)
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json(err.message)
+  //   })
+
+
+})
 
 module.exports = server;
