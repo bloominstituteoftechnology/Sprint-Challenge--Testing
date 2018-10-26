dcbd168e3d36985626c4f11371e306bee0fafe2e
Lndubose <lndubose13@gmail.com>
2018-10-26 10:57:48 -0500

    Get test work
    First Test: check status code
    Second Test: checks to recieve array even with an empty array
    Third Test: Makes sure it is be sent in json

--- index.spec.js
+++ index.spec.js
@@ -3,6 +3,26 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server', () => {
+  describe('Get /games', () => {
+    it('the get is getting a status code of 200(OK)', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.status).toBe(200);
+    });
+
+    it('the get is recieving an array even if it is empty', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.body).toEqual(expect.arrayContaining([]));
+    });
+
+    it('should return json', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.type).toBe('application/json');
+    });
+  });
+
   it('server running', () => {
     expect(true).toBeTruthy();
     expect(false).toBeFalsy();
--- server.js
+++ server.js
@@ -4,4 +4,45 @@ const server = express();
 
 server.use(express.json());
 
+let gamesList = [
+  // {
+  //   name: 'Spider-Man',
+  //   yearRelease: 2018,
+  //   genre: 'action-adventure',
+  // },
+  // {
+  //   name: 'Fallout 4',
+  //   yearRelease: 2017,
+  //   genre: 'action, role-playing',
+  // },
+  // {
+  //   name: "PLAYERUNKOWN'S Battlegrounds",
+  //   yearRelease: 2017,
+  //   genre: 'Action',
+  // },
+  // {
+  //   name: 'Minecraft',
+  //   yearRelease: 2009,
+  //   genre: 'sandBox, survival Game',
+  // },
+  // {
+  //   name: 'Grand Theft Auto V',
+  //   yearRelease: 2013,
+  //   genre: 'action-adventure',
+  // },
+  // {
+  //   name: 'Portal',
+  //   yearRelease: 2007,
+  //   genre: 'platform, puzzle',
+  // },
+];
+
+server.get('/games', (req, res) => {
+  if (!gamesList.length) {
+    res.status(200).json({ games: [] });
+  } else {
+    res.status(200).json({ games: gamesList });
+  }
+});
+
 module.exports = server;
