108696f1a3730997d2e47391d7e9ed34357e35e5
Andrew <ghobrial.andrew@gmail.com>
2018-10-26 13:37:11 -0400

    delete tests and endpoint complete

--- server.spec.js
+++ server.spec.js
@@ -4,21 +4,21 @@ const request = require("supertest");
 describe("test API endpoints", () => {
   
   // GET tests
-  it('successful get sends back status code 200', async () => {
+  it('**GET**: successful get sends back status code 200', async () => {
     const response = await request(server).get('/api/games');
       expect(response.status).toBe(200);
   });
-  it('get request should receive an array', async () => {
+  it('**GET**: get request should receive an array', async () => {
     const response = await request(server).get('/api/games');
       expect(response.body).toEqual(expect.arrayContaining([]));
   });
-  it('get should return in the form of json', async () => {
+  it('**GET**: get should return in the form of json', async () => {
     const response = await request(server).get('/api/games');
       expect(response.type).toBe('application/json');
   });
 
   // POST tests
-  it("should add game", async () => {
+  it("**POST**: should add game with correct body formatting", async () => {
     const title = "Grand Theft Auto V";
     const genre = "Action-Adventure";
     const releaseYear = 2013;
@@ -28,36 +28,46 @@ describe("test API endpoints", () => {
       genre: "Action-Adventure", 
       releaseYear: 2013 
     };
-     const response = await request(server)
+    const response = await request(server)
       .post("/api/games")
       .send({ title, genre, releaseYear });
      expect(response.body).toEqual(expected);
   });
-  it("should return status code 200 if successful", async () => {
+  it("**POST**: should return status code 200 if successful", async () => {
     const title = "Grand Theft Auto V";
     const genre = "Action-Adventure";
     const releaseYear = 2013;
-     const response = await request(server)
+    const response = await request(server)
       .post("/api/games")
       .send({ title, genre, releaseYear });
-     expect(response.status).toEqual(200);
+    expect(response.status).toEqual(200);
   });
-  it("should return status code 422 if malformed data posted", async () => {
+  it("**POST**: should return status code 422 if malformed data posted", async () => {
     const response = await request(server).post("/api/games");
      expect(response.status).toEqual(422);
   });
-  it("should return JSON format", async () => {
+  it("**POST**: should return JSON format", async () => {
     const response = await request(server).post("/api/games");
      expect(response.type).toBe("application/json");
   });
   /////
 
   // DELETE tests 
-  it("should delete respective game matching correct id", async () => {
+  it("**DELETE**: should delete respective game matching correct id", async () => {
     const id = 0;
     const expected = { gameDeleted: `${id}` };
      const response = await request(server).delete(`/api/games/${id}`);
      expect(response.body).toEqual(expected);
   });
-
+  it("**DELETE**: delete endpoint should return status code 200 if successful", async () => {
+    const id = 0;
+    const response = await request(server).delete(`/api/games/${id}`);
+     expect(response.status).toBe(200);
+  });
+  it("**DELETE**: delete endpoint should return JSON object", async () => {
+    const id = 0;
+    const response = await request(server).delete(`/api/games/${id}`);
+     expect(response.type).toBe("application/json");
+  });
+  //////
 });
