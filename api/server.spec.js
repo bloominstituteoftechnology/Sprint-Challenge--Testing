1a318bd218238df7ef427830badaf4ea63a10752
Carlos Vargas <cornielleandres@gmail.com>
2018-10-26 11:15:34 -0400

    Write tests for GET /games/all

--- api/server.spec.js
+++ api/server.spec.js
@@ -16,7 +16,32 @@ describe('server.js', () => {
 
 		it('should return the message "Server is running."', async () => {
 			const response = await request(server).get('/games/');
+			expect(typeof(response.body)).toBe('object');
 			expect(response.body).toEqual({ message: 'Server is running.' });
+		});
+	});
+
+	describe('GET /games/all', () => {
+		it('should return status 200(OK)', async () => {
+			const response = await request(server).get('/games/all');
+			expect(response.status).toBe(200);
+		});
+
+		it('should return JSON', async () => {
+			const response = await request(server).get('/games/all');
+			expect(response.type).toBe('application/json');
+		});
+
+		it('should return list of all the games', async () => {
+			const response = await request(server).get('/games/all');
+			const expected = [
+				{ 'id': 1, 'title': 'Pacman', 'genre': 'Arcade', 'releaseYear': 1980, },
+				{ 'id': 2, 'title': 'Tetris', 'genre': 'Arcade', },
+				{ 'id': 3, 'title': 'Dragon Quest', 'genre': 'RPG', },
+			];
+			expect(Array.isArray(response.body)).toBe(true);
+			expect(response.body.length).toEqual(3);
+			expect(response.body).toEqual(expected);
 		});
 	});
 });
