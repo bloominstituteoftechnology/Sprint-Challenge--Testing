a18cbe9bc150613dc74502ad354a901d2c018692
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-26 16:28:33 -0500

    Fixed minor bugs

--- server.spec.js
+++ server.spec.js
@@ -3,17 +3,17 @@ const server = require('./server');
 
 describe('server.js', () => {
     describe('GET games', () => {
-        it('should return a 200 status code', async() => {
+        it('should return a 200 status code', async () => {
             const response = await request(server).get('/games');
             expect(response.status).toBe(200);
         });
 
-        it('should return an array', async() => {
+        it('should return an array', async () => {
             const response = await request(server).get('/games');
             expect(Array.isArray(response.body)).toBeTruthy();
           });
 
-        it('should return a list of games', async() => {
+        it('should return a list of games', async () => {
             const response = await request(server).get('/games');
             const expected = [
                 { 
@@ -21,16 +21,22 @@ describe('server.js', () => {
                     title: 'Pacman', 
                     genre: 'Arcade', 
                     releaseYear: 1980
+                },
+                {
+                    id: 2,
+                    title: 'Super Mario', 
+                    genre: 'Platformer', 
+                    releaseYear: 1985 
                 }
             ]
-            expect(response.body).toEqual('expected');
+            expect(response.body).toEqual(expected);
         });
     });
     describe('POST games', () => {
-        it('should return a status code of 422 if missing information', async() => {
+        it('should return a status code of 422 if missing information', async () => {
             const response = await request(server)
             .post('/games')
-            .send({ title: 'Pacman' });
+            .send({ title: '1234' });
             expect(response.status).toBe(422);
         });
 
@@ -41,19 +47,32 @@ describe('server.js', () => {
             expect(response.status).toBe(405);
           });
 
-        it('should return games if succesfully added', async() => {
+        it('should return games if succesfully added', async () => {
             const response = await request(server)
             .post('/games')
             .send({
-                title: 'Pacman', 
-                genre: 'Arcade', 
-                releaseYear: 1980 
+                title: 'World of Warcraft', 
+                genre: 'MMORPG',
+                releaseYear: 2004
             });
             const expected = [
                 {
+                    id: 1,
                     title: 'Pacman', 
                     genre: 'Arcade', 
                     releaseYear: 1980 
+                },
+                {
+                    id: 2,
+                    title: 'Super Mario', 
+                    genre: 'Platformer', 
+                    releaseYear: 1985 
+                },
+                {
+                    id: 3,
+                    title: 'World of Warcraft', 
+                    genre: 'MMORPG',
+                    releaseYear: 2004
                 }
             ];
             expect(response.body).toEqual(expected);
@@ -63,9 +82,8 @@ describe('server.js', () => {
             const response = await request(server)
             .post('/games')
             .send({
-                title: 'Super Mario', 
-                genre: 'Platformer', 
-                releaseYear: 1985 
+                title: 'TEST TITLE',
+                genre: 'NO GENRE'
             });
             expect(response.status).toBe(201);
         });
