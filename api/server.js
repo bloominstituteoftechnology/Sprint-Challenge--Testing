c5834fbf6ee3300494fe94663748baa6ebd771bc
michaelagard <mdagard@gmail.com>
2018-10-26 13:21:16 -0400

    add additional test for posts and local array memory for related tests

--- api/server.js
+++ api/server.js
@@ -13,10 +13,16 @@ server.get('/', (req, res) => { // sanity check root endpoint
   res.send(`${serverName} running on port ${serverPort}<br>More information: <a href="${projectPullRequest}">{serverName}</a>`);
 });
 server.get('/api/games', (req, res) => { // sanity check root endpoint
-  res.status(201).json(gameArray);
+  res.status(200).json(gameArray);
 });
-server.post('/api/games/:game', (req, res) => {
-  gameArray.push(res.body);
-  res.status(201).json(1);
+server.post('/api/games/', (req, res) => {
+  let { title, genre, releaseYear } = req.body;
+  if (title && genre) {
+    gameArray.push(req.body)
+    res.status(201).json(1);
+  } else {
+    res.status(422).json(JSON.stringify(req.params.title))
+    console.log('THIS')
+  }
 });
-module.exports = server; 
+module.exports = server;
