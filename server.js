2c0615bfb4423541ce24f4148a5a93b9a6b03608
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-10-26 12:35:23 -0400

    added data to server.js file and set up a simple GET request to return this data

--- server.js
+++ server.js
@@ -2,10 +2,49 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+// games dummy data
+
+const games = [
+  {
+    "title": "Dota 2",
+    "genre": "MOBA",
+    "releaseYear": 2011
+  },
+
+  {
+    "title": "Unreal Tournament",
+    "genre": "First-Person Shooter",
+    "releaseYear": 1999
+  },
+
+  {
+    "title": "Deus Ex",
+    "genre": "Action role-playing",
+    "releaseYear": 2000
+  },
+
+  {
+    "title": "World of Warcraft",
+    "genre": "MMORPG",
+    "releaseYear": 2000
+  },
+
+  {
+    "title": "Blade and Soul",
+    "genre": "MMORPG",
+    "releaseYear": 2012
+  }
+
+]
 // simple GET request to test server
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Server running successfully!' });
 });
 
+// get games data
+
+server.get('/games', (req, res) => {
+  res.status(200).json(games);
+})
 module.exports = server;
