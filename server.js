f0a1c0ef316cde5198b1c1ced491928ca99df5f3
Lucas Beemer <lucasbeemer@gmail.com>
2018-10-26 09:28:58 -0700

    5 passed tests for POST

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ server.get('/games', (req, res) => {
     res.status(200).json(gamesArr);
 });
 
+server.post('/games', (req, res) => {
+    const game = req.body;
+    if (!game.title || !game.genre) {
+        res.status(422).json({ error: "Please provide a title and genre for the game." });
+    } else if (gamesArr.filter(obj => obj.title === game.title).length > 0) {
+        res.status(405).json({ error: "Please provide a unique title for the game." });
+    } else {
+        gamesArr.push(game);
+        res.status(201).json(game);
+    } 
+});
 
 
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -14,6 +14,43 @@ const defaultGame = {
         const response = await request(server).get('/');
         expect(response.status).toEqual(200);
     });
+    describe('POST /games', () => {
+        it('should return status code 422 if no title is provided', async () => {
+            let game = { ...defaultGame, title: '' }
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+            expect(response.status).toEqual(422);
+        });
+        it('should return status code 422 if no genre is provided', async () => {
+            let game = { ...defaultGame, genre: '' }
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+            expect(response.status).toEqual(422);
+        });
+        it('should return status code 201 if all required fields are provided', async () => {
+            let game = { ...defaultGame }
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+            expect(response.status).toEqual(201);
+        });
+        it('should return the provided object', async () => {
+            let game = { ...defaultGame, title: 'Atari', releaseYear: 1972 }
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+            expect(response.body).toEqual({ title: 'Atari', genre: 'Arcade', releaseYear: 1972 });
+        });
+        it('should return status code 405 if the title already exists', async () => {
+            let game = { ...defaultGame }
+            const response = await request(server)
+            .post('/games')
+            .send(game)
+            expect(response.status).toEqual(405);
+        });
+    });
 
 
 });
