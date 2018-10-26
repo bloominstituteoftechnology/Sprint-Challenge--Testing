5ae156e058930d32bee6bc00cae95e2ede9f0189
ckirkwood94 <ckirkwood94@gmail.com>
2018-10-26 12:04:23 -0400

    Write tests for get and post endpoints

---
+++ api/tests/server.spec.js
@@ -0,0 +1,85 @@
+const request = require('supertest');
+const server = require('../server.js');
+
+describe('server', () => {
+  ///////////////////////
+  // GET /games endpoint
+  ///////////////////////
+  describe('GET /games', () => {
+    it('should return status code 200(OK)', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.status).toBe(200);
+    });
+
+    it('should return json', async () => {
+      const response = await request(server).get('/games');
+
+      expect(response.type).toBe('application/json');
+    });
+
+    it('should return array in response.body', async () => {
+      const response = await request(server).get('/games');
+
+      expect(typeof response.body).toBe('array');
+    });
+  }); // describe GET /games
+
+  ///////////////////////
+  // POST /games endpoint
+  ///////////////////////
+  describe('POST /games', () => {
+    it('should return status code 201(Created)', async () => {
+      const game = {
+        title: 'Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980, // not required
+      };
+      const response = await request(server)
+        .post('/games')
+        .send(game);
+
+      expect(response.status).toBe(201);
+    });
+
+    describe('Check for required fields', () => {
+      it('should return 422 if title is missing from body', async () => {
+        const game = {
+          title: '', // required
+          genre: 'Arcade', // required
+          releaseYear: 1980, // not required
+        };
+        const response = await request(server)
+          .post('/games')
+          .send(game);
+
+        expect(response.status).toBe(422);
+      });
+
+      it('should return 422 if genre is missing from body', async () => {
+        const game = {
+          title: 'Pacman', // required
+          genre: '', // required
+          releaseYear: 1980, // not required
+        };
+        const response = await request(server)
+          .post('/games')
+          .send(game);
+
+        expect(response.status).toBe(422);
+      });
+
+      it('should return 201 if release year is missing from body', async () => {
+        const game = {
+          title: 'Pacman', // required
+          genre: 'Arcade', // required
+        };
+        const response = await request(server)
+          .post('/games')
+          .send(game);
+
+        expect(response.status).toBe(422);
+      });
+    }); // describe Check for required fields
+  }); // describe POST /games
+}); // describe server
