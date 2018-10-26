578034df05ccfa88b214073e9989968ded5acbd6
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-26 08:20:20 -0700

    add get should return array

--- api/server.js
+++ api/server.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const server = express();
 
+const games = [
+	{
+		title: "Pac-man",
+		genre: "arcade",
+		releaseYear: 1980
+	}
+];
+
 server.use(express.json());
 server.get("/", (req, res) => {
 	res.status(200).json({ message: "server is up and running" });
+});
+server.get("/games", (req, res) => {
+	res.status(200).send(games);
+});
+
+server.post("/games", (req, res) => {
+	const { game } = req.body;
 });
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -6,4 +6,8 @@ describe("GET /", () => {
 		const response = await request(server).get("/");
 		expect(response.status).toBe(200);
 	});
+	it("should return an array of games", async () => {
+		const response = await request(server).get("/games");
+		expect(Array.isArray(response.body)).toBe(true);
+	});
 });
