197c93bb42108ccb389b4cdd8fb5996a07592f43
Arkadiusz Kusiak <akusiak23@gmail.com>
2018-10-26 11:22:55 -0500

    POST

--- server.spec.js
+++ server.spec.js
@@ -26,4 +26,41 @@ describe('server.js', () => {
             expect(response.body).toEqual('expected');
         });
     });
+    describe('POST games', () => {
+        it('should return a status code of 422 if missing information', () => {
+            const response = request(server)
+            .post('/games')
+            .send({ title: 'Pacman' });
+            expect(response.status).toBe(422);
+        });
+
+        it('should return games if succesfully added', () => {
+            const response = request(server)
+            .post('/games')
+            .send({
+                title: 'Pacman', 
+                genre: 'Arcade', 
+                releaseYear: 1980 
+            });
+            const expected = [
+                {
+                    title: 'Pacman', 
+                    genre: 'Arcade', 
+                    releaseYear: 1980 
+                }
+            ];
+            expect(response.body).toEqual(expected);
+        });
+        
+        it('should return a status code of 201 if successful', () => {
+            const response = request(server)
+            .post('/games')
+            .send({
+                title: 'Super Mario', 
+                genre: 'Platformer', 
+                releaseYear: 1985 
+            });
+            expect(response.status).toBe(201);
+        });
+    });
 });
