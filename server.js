bc344f0e65e3a95f222f8048ca0d222f7312e0c7
Eric B <timberwolf.eric@gmail.com>
2018-10-26 14:42:52 -0500

    Added some endpoints & starting to test

--- server.js
+++ server.js
@@ -5,4 +5,18 @@ server.use(express.json());
 
 const games = [];
 
+server.post("/games", (req, res) => {
+  const { title, genre } = req.body;
+
+  if (!title || !genre) {
+    return res.status(422).json({ error: "Incorrect info was provided: { title, genre}" });
+  }
+  games.push({ title: title, genre: genre });
+  res.status(200).json({ title: title, genre: genre });
+});
+
+server.get('/games', (req, res) => {
+    res.status(200).json(games);
+});
+
 module.exports = server;
