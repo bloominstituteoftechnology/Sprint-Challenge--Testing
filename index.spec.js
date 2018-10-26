7e0b70037321d5a6e8b7d773a247b5e644167bf6
Keiran <keirankozlowski@gmail.com>
2018-10-26 12:08:54 -0400

    Updated most POST tests.

--- index.spec.js
+++ index.spec.js
@@ -26,4 +26,50 @@ describe('server', () => {
             expect(response.body).toEqual([]);
         });
     });
+
+    describe('POST /games/add route', () =>{
+        it('should take in a json', async () => {
+            const title = 'Video Game Man';
+            const genre = 'action-adventure';
+            const releaseYear = '1986';
+            const expected = { 
+                title: title,
+                genre: genre,
+                releaseYear: releaseYear
+             };
+
+            const response = await request(server).post(`/games/add`).send( { lastName } );
+
+            expect(response.body).toEqual(expected);
+        });
+        it('should return 422 status code if missing title or genre', async () => {
+            const title = 'Video Game Man';
+            const genre = '';
+            const releaseYear = '1986';
+            const expected = { 
+                title: title,
+                genre: genre,
+                releaseYear: releaseYear
+             };
+
+            const response = await request(server).post(`/games/add`);
+
+            expect(response.status).toBe(422);
+        });
+        it('should return 200 status code if all info is entered', async () => {
+            const title = 'Video Game Man';
+            const genre = 'action-adventure';
+            const releaseYear = '1986';
+            const expected = { 
+                title: title,
+                genre: genre,
+                releaseYear: releaseYear
+             };
+
+            const response = await request(server).post(`/games/add`);
+
+            expect(response.status).toBe(200);
+            done();
+        });
+    });
 });
