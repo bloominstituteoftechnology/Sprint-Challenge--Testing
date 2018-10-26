0017102c931be325af476566cadd82d1f9884f1d
Max <maxkajiwara@gmail.com>
2018-10-26 09:12:36 -0700

    Set up server

---
+++ migrations/20181026090439_games.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('games', table => {
+		table.increments();
+		table.string('title').notNullable();
+		table.string('genre').notNullable();
+		table.number('releaseYear').notNullable();
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('games');
+};
