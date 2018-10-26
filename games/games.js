61c940401479b862377e6cd258fc4c6eaed0a091
Csillag61 <csillagok@gmail.com>
2018-10-26 12:37:31 -0400

    GET by ID, Update and Delete tests added

--- games/games.js
+++ games/games.js
@@ -1,13 +1,3 @@
-const router = require("express").Router();
-const g = require('./games');
 
-router.route('/')
-    .get(g.GET)
-    .post(g.POST)
 
-router.route('/:id')
-    .get(g.GET)
-    .put(g.PUT)
-    .delete(g.DELETE);
 
-module.exports = router;
--- server.spec.js
+++ server.spec.js
@@ -1,5 +1,5 @@
-const req = require("supertest");
-const server = require("./server");
+const req = require('supertest');
+const server = require('./server');
 
 describe('server.js', () => {
     describe('GET /games', () => {
@@ -8,6 +8,7 @@ describe('server.js', () => {
         beforeAll(async () => {
             res = await req(server).get('/games');
         });
+
         it('OK (200)', () => {
             expect(res.status).toBe(200);
         });
@@ -16,28 +17,53 @@ describe('server.js', () => {
             expect(Array.isArray(res.body)).toBe(true);
         });
 
-        it('checks on objects correct properties',()=> {
-            if (res.body.length > 0){
+        it('checks if  object has correct properties', () => {
+            if (res.body.length > 0) {
                 expect(
-                    res.body[0].hasOwnProperty('title')&&
+                    res.body[0].hasOwnProperty('title') &&
                     res.body[0].hasOwnProperty('genre')
                 ).toBe(true);
-                }
+            }
         });
     });
+
+    describe('GET /games/:id', () => {
+        let res;
+
+        beforeAll(async () => {
+            res = await req(server).get('/games/1');
+        });
+
+        it('should return  OK  (200)', () => {
+            expect(res.status).toBe(200);
+        });
+
+        it('should return an object', () => {
+            expect(typeof res.body).toBe('object');
+        });
+
+        it('checks on objects properties correctly', () => {
+            expect(
+                res.body.hasOwnProperty('title') &&
+                res.body.hasOwnProperty('genre')
+            ).toBe(true);
+        });
+    });
+
     describe('POST /games', () => {
         let res;
-        beforeAll(async()=> {
+
+        beforeAll(async () => {
             res = await req(server).post('/games')
-            .send({
+                .send({
                     'title': 'Star Wars',
                     'genre': 'Role-playing',
                     'releaseYear': 2003
                 })
                 .set('Content-Type', 'application/json')
                 .set('Accept', 'application/json');
-        });  
-        
+        });
+
         it('should return a Created http code (201)', () => {
             expect(res.status).toBe(201);
         });
@@ -56,6 +82,7 @@ describe('server.js', () => {
                 .set('Accept', 'application/json');
             expect(goodReq.status).toBe(201);
         });
+
         it('should return a Not Allowed (405) if given a taken title', async () => {
             const badReq = await req(server).post('/games')
                 .send({
@@ -89,5 +116,64 @@ describe('server.js', () => {
             expect(badReq.status).toBe(422);
         });
     });
-})
-        
+
+    describe('DELETE /games/:id', () => {
+        let res;
+
+        beforeAll(async () => {
+            res = await req(server).delete('/games/1');
+        });
+
+        it('should return an Accepted http code (202)', () => {
+            expect(res.status).toBe(202);
+        });
+
+        it('should return a count of records deleted (1)', () => {
+            expect(res.body).toBe(1);
+        });
+
+        it('should delete the game with the provided ID', async () => {
+            const gameReq = await req(server).get('/games/1');
+            expect(gameReq.status).toBe(404); // not found
+        });
+
+        it('should return a Not Found http code (404) if game with given id isn\'t found', async () => {
+            const badReq = await req(server).delete('/games/-100twenty');
+            expect(badReq.status).toBe(404);
+        });
+    });
+
+    describe('PUT /games/:id', () => {
+        let res;
+
+        beforeAll(async () => {
+            res = await req(server).put('/games/2')
+                .send({
+                    'game': 'Portal',
+                    'genre': 'Puzzle-platformer',
+                    'releaseYear': 2007
+                })
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json');
+        });
+
+        it('should return an OK  (200)', () => {
+            expect(res.status).toBe(200);
+        });
+
+        it('should return a count of the records updated (1)', () => {
+            expect(res.body).toBe(1);
+        });
+
+        it('should return a Not Found http code (404) if given a bad id', async () => {
+            const badReq = await req(server).put('/games/-100two')
+                .send({
+                    'game': 'Not Portal',
+                    'genre': 'Puzzle-platformer'
+                })
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json');
+            expect(badReq.status).toBe(404);
+        });
+    });
+});
