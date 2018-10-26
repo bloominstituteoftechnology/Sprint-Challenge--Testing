d2fa47290d548edb7af65e5bd9ba92494ee5c7c2
BrianARuff <thelonewolf198927@gmail.com>
2018-10-26 12:55:04 -0400

    added in test for stretch goals

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -9,6 +9,16 @@ describe("API", () => {
         .send({ title: "WoW", genre: "MMO-RPG" });
       expect(resp.status).toBe(422);
     });
+    it("should return status of 405 if title is not unique", async () => {
+      const resp = await request(server)
+        .post("/games")
+        .send({
+          title: "World of Warcraft",
+          genre: "MMORPG",
+          releaseYear: 2004
+        });
+      expect(resp.status).toBe(405);
+    });
     it("should return status code of 201 when post is successful", async () => {
       const resp = await request(server)
         .post("/games")
@@ -31,8 +41,35 @@ describe("API", () => {
       });
     });
     it("should always return an array", async () => {
-      const resp = await request(server).get('/games');
+      const resp = await request(server).get("/games");
       expect(Array.isArray(resp.body)).toBe(true);
+    });
+  });
+  describe("GET /games/:id", () => {
+    it("if game id isn't found it should return game not found message", async () => {
+      const resp = await request(server).get("/games/100");
+      expect(resp.body.message).toBe("Game not found");
+    });
+    it("should return game if found by id", async () => {
+      const resp = await request(server).get("/games/1");
+      expect(resp.body).toMatchObject({
+        title: "Pacman",
+        genre: "Arcade",
+        releaseYear: 1980
+      });
+    });
+  });
+  describe("DELETE /games/:id", () => {
+    it("should return status 404 if game not found", async () => {
+      const resp = await request(server).del(
+        `/games/10000}`
+      );
+      expect(resp.status).toBe(404);
+    });
+    it("should return status 200 if game is found", async () => {
+      const games = await request(server).get("/games");
+      const resp = await request(server).del(`/games/${games.body[games.body.length - 1].id}`);
+      expect(resp.status).toBe(200);
     })
   });
 });
--- app/server.js
+++ app/server.js
@@ -19,6 +19,15 @@ server.post("/games", (req, res) => {
     return res.status(422).json({ message: missing });
   } else {
     if (game) {
+      db.select("*")
+        .from("games")
+        .then(games => {
+          return games.forEach(g => {
+            if (g.title === title) {
+              return res.status(405).json({ message: "Title not unique" });
+            }
+          });
+        });
       db.insert(game)
         .into("games")
         .then(count => {
@@ -33,6 +42,37 @@ server.get("/games", (req, res) => {
   db("games")
     .then(games => {
       res.status(200).json(games);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/games/:id", (req, res) => {
+  const { id } = req.params;
+  db("games")
+    .where({ id })
+    .first()
+    .then(game => {
+      if (!game) {
+        res.status(404).json({ message: "Game not found" });
+      }
+      if (game) {
+        res.status(200).json(game);
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete("/games/:id", (req, res) => {
+  const { id } = req.params;
+  db("games")
+    .where({ id })
+    .del()
+    .then(count => {
+      if(!count) {
+        res.status(404).json({message: "Game not found"});
+      } else {
+        res.status(200).json(count)
+      }
     })
     .catch(err => res.status(500).json(err));
 });
