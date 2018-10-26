529a63906d1c87a0379859cbc0736e61e98bde64
travis-russell <russell.travis@yahoo.com>
2018-10-26 10:20:57 -0500

    server test

---
+++ index.spec.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const server = require("./api/server");
+
+describe('basic server test', () => {
+
+    it('can run tests', () => {
+        expect(true).toBeTruthy();
+    })
+})
