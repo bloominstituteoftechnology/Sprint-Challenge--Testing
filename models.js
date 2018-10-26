7de0ab5b2ff93b74be418edbfc7735b9ae17599e
Charletta <charlettabullard12@outlook.com>
2018-10-26 12:20:33 -0400

    Refactored app.js to remove all of mongoose instances as well as refactored server and models.js to remove mongoose database

--- models.js
+++ models.js
@@ -1,31 +1,29 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
 // This is our video game schema. Notice the two methods below
 // Notice the fields that are required when saving data to this model
 // Notice that release date is just a string. I don't want you to have to worry about dates :)
-const NESGameSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  genre: {
-    type: String,
-    required: true
-  },
-  releaseDate: String
-});
+// const NESGameSchema = new Schema({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   genre: {
+//     type: String,
+//     required: true
+//   },
+//   releaseDate: String
+// });
 
-NESGameSchema.methods.getGameTitle = function() {
-  return this.title;
-};
+// NESGameSchema.methods.getGameTitle = function() {
+//   return this.title;
+// };
 
-NESGameSchema.statics.getGames = function(cb) {
-  Game.find({}, (err, games) => {
-    if (err) return cb(err);
-    cb(games);
-  });
-};
+// NESGameSchema.statics.getGames = function(cb) {
+//   Game.find({}, (err, games) => {
+//     if (err) return cb(err);
+//     cb(games);
+//   });
+// };
 
-const Game = mongoose.model('Game', NESGameSchema);
+// const Game = mongoose.model("Game", NESGameSchema);
 
-module.exports = Game;
+// module.exports = Game;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
 
-const Game = require('./models');
+const Game = require("./models");
 
 const server = express();
-server.use(bodyParser.json());
-server.use(morgan('combined'));
 
-server.post('/api/game/create', (req, res) => {
+server.use(express.json());
+
+//Testing server instance
+server.get("/", (req, res) => {
+  res.send("server is up");
+});
+
+server.post("/api/game/create", (req, res) => {
   const { title, releaseDate, genre } = req.body;
   const myGame = new Game({ title, releaseDate, genre });
   myGame
@@ -18,38 +21,38 @@ server.post('/api/game/create', (req, res) => {
     })
     .catch(err => {
       res.status(422);
-      res.json({ error: 'Error saving data to the DB', message: err });
+      res.json({ error: "Error saving data to the DB", message: err });
     });
 });
 
-server.get('/api/game/get', (req, res) => {
+server.get("/api/game/get", (req, res) => {
   Game.find({}, (err, games) => {
     if (err) {
       res.status(500);
-      res.json({ error: 'Something really bad happened' });
+      res.json({ error: "Something really bad happened" });
       return;
     }
     res.json(games);
   });
 });
 
-server.put('/api/game/update', (req, res) => {
+server.put("/api/game/update", (req, res) => {
   // All I care about is the game title and id.. don't worry about genre or date.
   const { title, id } = req.body;
   if (!title || !id) {
-    return res.status(422).json({ error: 'Must Provide a title && Id' });
+    return res.status(422).json({ error: "Must Provide a title && Id" });
   }
   Game.findById(id, (err, game) => {
     if (err || game === null) {
       res.status(422);
-      res.json({ error: 'Cannot find game by that id' });
+      res.json({ error: "Cannot find game by that id" });
       return;
     }
     game.title = title;
     game.save((saveErr, savedGame) => {
       if (err || game === null) {
         res.status(500);
-        res.json({ error: 'Something really bad happened' });
+        res.json({ error: "Something really bad happened" });
         return;
       }
       res.json(game);
@@ -57,7 +60,7 @@ server.put('/api/game/update', (req, res) => {
   });
 });
 
-server.delete('/api/game/destroy/:id', (req, res) => {
+server.delete("/api/game/destroy/:id", (req, res) => {
   // to delete a game you can send up an id on the request body or the params
   let id = undefined;
   if (req.params.id) {
@@ -71,14 +74,14 @@ server.delete('/api/game/destroy/:id', (req, res) => {
   if (id === undefined) {
     // if it's undefined throw error back to client
     res.status(422);
-    res.json({ error: 'You need to give me an ID' });
+    res.json({ error: "You need to give me an ID" });
     return;
   }
   Game.findByIdAndRemove(id, (err, removedGame) => {
     // search for game by that id and remove it
     if (err) {
       res.status(422);
-      res.json({ error: 'Cannot find game by that id' });
+      res.json({ error: "Cannot find game by that id" });
       return;
     }
     res.json({ success: `${removedGame.title} was removed from the DB` });
