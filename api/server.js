ad003e9423db5e0eb2de25913ea37bed36b6e1b5
Eva <evalee904@gmail.com>
2018-10-26 11:37:31 -0500

    nodemon added, fixed up routing.

--- api/server.js
+++ api/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 const server = express();
+// const router = express.Router();
 
-server.use(express.json(), helmet());
+server.use(express.json());
+server.use(helmet());
 
 let games = {
   title: 'Pacman',
   genre: 'Arcade',
   releaseYear: 1980
-}
+};
+
+// router.route('/games')
+//   .get((req, res) => {
+//     res.status(200).json(games);
+//   })
+
+server.get('/games', (req, res) => {
+  res.status(200).json(games);
+})
+
 
 
 module.exports = server;
