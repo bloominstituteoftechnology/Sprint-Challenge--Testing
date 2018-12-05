94ebe8dc1df6e92ba7e6fcfa3a45dec827ffb5da
Lee Len Formento <leebatef@gmail.com>
2018-12-04 21:42:09 -0500

    Initial test

---
+++ index.spec.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server.js', () => {
+    it('runs tests', () => {
+        expect(true).toBeTruthy();
+    }) // runs tests
+    
+}) // server.js
