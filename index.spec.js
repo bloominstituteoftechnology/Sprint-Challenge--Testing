cfd259c92fdedc2b6993b905fc9c49a0aa0af4a9
Lee Len Formento <leebatef@gmail.com>
2018-12-04 22:34:32 -0500

    Wrote POST test. Route handler. 422 and 201 test to go.

--- index.spec.js
+++ index.spec.js
@@ -5,5 +5,17 @@ describe('server.js', () => {
     it('runs tests', () => {
         expect(true).toBeTruthy();
     }) // runs tests
-    
+    describe('post /games', () => {
+    it('should post a game', async () => {
+        const title = "Pacman";
+        const genre = "Arcade";
+        const releaseYear = 1980;
+        const expected = { message: 'Pacman Arcade from 1980 has been added!' };
+
+        const response = await request(server)
+        .post('/games')
+        .send({title, genre, releaseYear});
+        expect(response.body).toEqual(expected);
+      }); // should post a game
+    }) // post /games
 }) // server.js
--- server.js
+++ server.js
@@ -8,4 +8,17 @@ server.get('/', (req, res) => {
     res.send('Hi Besssie!');
 })
 
+const games = []; // created games array
+
+server.post('/games', (req, res) => {
+    const { title, genre, releaseYear }  = req.body;
+    const newGame = { title, genre, releaseYear };
+    if(!title || !genre) {
+     res.status(422).json({ error: 'Please provide some of the missing information.'})
+    } else {
+      games.push(newGame);
+      res.status(200).json({ message: `${title} ${genre} from ${releaseYear} has been added!` });
+    };
+  })
+
 module.exports = server;
