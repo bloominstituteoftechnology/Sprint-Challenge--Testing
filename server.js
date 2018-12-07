403923c694224d846e78d57edb138b7f4b6b7186
Steven O'Neill <oneillsteven957@gmail.com>
2018-12-07 12:17:10 -0600

    added endpoints

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const server = express();
 
 server.use(express.json());
 
+server.post('/games', (req, res) => {
+    const { title, genre } = req.body;
+    if (!title || !genre) {
+        res.status(422).json({ message: 'please add title and genre' })
+    } else {
+        res.status(200).json(req.body);
+    }
+});
+
+server.get('/games', (req, res) => {
+    Object.keys(req.body).length ? res.status(200).json([req.body]) : res.status(200).json([]);
+});
+
 // sanity check endpoint
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'alive' });
