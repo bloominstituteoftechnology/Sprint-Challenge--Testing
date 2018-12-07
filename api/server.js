88dffda4ced6d0f21521db116db1fbf917b5726f
Lola <lolahef@gmail.com>
2018-12-07 11:25:52 -0500

    Answers Review.md; Sets up file structure & dependencies

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'alive' });
+})
+
+module.exports = server;
