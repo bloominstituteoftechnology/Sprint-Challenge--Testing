460c3e4eb9469f73d2d753ef3c4648dce6eea317
sean-one <basement365@gmail.com>
2018-12-07 09:20:47 -0800

    created database table

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,9 +1,21 @@
 const server = require('./server');
 const request = require('supertest');
 
-describe('server up checks', () => {
-    it('entry test route', async () => {
-        const response = await request(server).get('/');
-        expect(response.status).toBe(422)
+
+describe('server routes', () => {
+    describe('should check the server is up and running', () => {
+        it('entry test route', async () => {
+            const response = await request(server).get('/');
+            expect(response.status).toBe(200)
+        });
+        
+        it('should return body with {message: "running...', async () => {
+            const response = await request(server).get('/');
+            expect(response.body).toEqual({ message: 'running...'});
+        });
     });
+
+    // describe('GET route', () => {
+    //     it('should ')
+    // });
 });
