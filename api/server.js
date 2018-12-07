1174c6e5c8a57724ff77d356495850531db569e0
ryanboris <ryan.boris@gmail.com>
2018-12-07 11:39:44 -0500

    Creates GET /api/games endpoint

--- api/server.js
+++ api/server.js
@@ -9,6 +9,12 @@ server.get('/', (req, res) => {
 	console.log('working')
 	res.status(200).json({ api: 'root endpoint is alive and well' })
 })
+
+server.get('/api/games', (req, res) => {
+	const games = []
+	res.status(200).json(games)
+})
+
 server.post('/api/games', (req, res) => {
 	const { title, genre } = req.body
 	!title || !genre
