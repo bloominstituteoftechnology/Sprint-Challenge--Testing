921555d7faa72ac7a0c29bdd10c668aaac8c2f91
ryanboris <ryan.boris@gmail.com>
2018-12-07 11:34:00 -0500

    Writes POST end point tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -13,3 +13,38 @@ describe('GET', () => {
 		})
 	})
 })
+
+describe('POST', () => {
+	describe('/api/games', () => {
+		it('should respond with 201(ADDED) when a successful request is made', async () => {
+			const game = {
+				title       : 'PacMan',
+				genre       : 'arcade',
+				releaseYear : 1982,
+			}
+			const response = await request(server).post('/api/games').send(game)
+			expect(response.status).toBe(201)
+		})
+		it('should respond with a success message when request is successful', async () => {
+			const game = {
+				title       : 'PacMan',
+				genre       : 'arcade',
+				releaseYear : 1982,
+			}
+			const response = await request(server).post('/api/games').send(game)
+			expect(response.body).toEqual({ message: 'Success' })
+		})
+		it('should respond with status code 422 when missing title or genre', async () => {
+			const game = {
+				title : 'PacMan',
+			}
+			const game2 = {
+				genre : 'arcade',
+			}
+			const response = await request(server).post('/api/games').send(game)
+			expect(response.status).toBe(422)
+			const response2 = await request(server).post('/api/games').send(game2)
+			expect(response2.status).toBe(422)
+		})
+	})
+})
