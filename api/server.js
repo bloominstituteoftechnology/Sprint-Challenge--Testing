12d68150532c9d10ad1c11d6d6db1ee9c6071888
kyranmccann <kyran.mccann@gmail.com>
2018-12-07 09:26:46 -0800

    adding id

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const server = express();
 server.use(express.json());
 
 let games = [];
+let gameId = 1 ;
 
 //sanity check
 server.get('/', (req, res) => {
@@ -17,7 +18,7 @@ server.get('/games', (req, res) => {
 
 server.post('/games', (req, res) => {
   const { title, genre, releaseYear } = req.body;
-  const newGame = { title, genre, releaseYear };
+  const newGame = { title, genre, releaseYear, id:gameId };
   if (!title || !genre) {
     return res.status(422).json({ error: 'title and genre are required' });
   }
@@ -28,6 +29,7 @@ server.post('/games', (req, res) => {
     return res.status(405).json({ error: 'That title already exists!' })
   }
   games.push(newGame);
+  gameId++;
   res.status(201).json(games);
 })
 
