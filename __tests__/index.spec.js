a28819b27744d6f20c0397166b5d67414552c3e8
Kat Fries <katherynejf@gmail.com>
2018-12-07 11:38:55 -0500

    passed red and green test for returning JSON object from the index route

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -10,10 +10,17 @@ describe('server.js', () => {
       expect(response.status).toBe(200);
     })
 
-    it('should return appliction/json', async () => {
+    it('should return application/json', async () => {
       let response = await request(server).get('/')
       // RED TEST: expect(response.type).toBe('application/xml');
       expect(response.type).toBe('application/json');
+    })
+
+    it('should return a JSON object from the index route', async () => {
+      const expectedBody = { api: 'Ready!' };
+        // RED TEST: const expectedBody = { api: 'up' };
+      const response = await request(server).get('/');
+      expect(response.body).toEqual(expectedBody);
     })
   })
 
