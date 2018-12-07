2401dbdb535036a7a47785ad707f427e66d0a2b2
Axolotl <samuelmachat@gmail.com>
2018-12-07 12:10:23 -0500

    write tests for post route

--- index.spec.js
+++ index.spec.js
@@ -5,20 +5,37 @@ const server = require('./api/server.js')
 describe('server.js', () => {
   describe('POST /games', () => {
     it('should return 201 if good request is sent', async () => {
-      const item = 'walrus'
+      const newGame = {
+        title: 'Pacman',
+        genre: 'Arcade',
+        releaseYear: 1980
+      }
       let response = await request(server)
         .post('/games')
-        .send({ item })
+        .send({ ...newGame })
       expect(response.status).toBe(201)
     })
 
-    it('should return 400 if bad request is sent', async () => {
-      const person = '45'
+    it('should return 201 if good request is sent without releaseYear', async () => {
+      const newGame = {
+        title: 'Pacman',
+        genre: 'Arcade'
+      }
       let response = await request(server)
         .post('/games')
-        .send({ person })
+        .send({ ...newGame })
+      expect(response.status).toBe(201)
+    })
 
-      expect(response.status).toBe(400)
+    it('should return 422 if bad request is sent', async () => {
+      const newGame = {
+        title: 'Pacman'
+      }
+      let response = await request(server)
+        .post('/games')
+        .send({ ...newGame })
+
+      expect(response.status).toBe(422)
     })
   })
 
