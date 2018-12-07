c6052a5bd10a54dcaede979e361619e9f0a34914
Unknown <zachary.arney@gmail.com>
2018-12-07 12:38:07 -0600

    adds POST /games endpoint

--- api/server.js
+++ api/server.js
@@ -3,5 +3,21 @@ const server = express();
 
 server.use(express.json());
 
+const games = [];
+
+server.post('/games', (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  const newGame = { title, genre, releaseYear };
+  if (!title || !genre || !releaseYear) {
+    res
+      .status(422)
+      .json({ message: 'please include title, genre and year of release' });
+  } else {
+    games.push(newGame);
+    res.status(200).json({
+      wow: `${title} is a great ${genre} game from ${releaseYear}!`,
+    });
+  }
+});
 
 module.exports = server;
