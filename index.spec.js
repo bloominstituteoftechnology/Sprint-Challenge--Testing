3639940d64dcb6d28e1d2f20ece044542dcd491e
zacharyarney <zachary.arney@gmail.com>
2018-12-07 11:48:39 -0600

    tests for POST /games endpoint

---
+++ index.spec.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const server = require('./api/server');
+
+describe('server.js', () => {
+  describe('POST /games endpoint', () => {
+    it('should return status code 200', async () => {
+      let response = await request(server)
+        .post('/games')
+        .send({
+          title: 'Pacman',
+          genre: 'Arcade',
+          releaseYear: 1980,
+        });
+      expect(response.status).toBe(200);
+    });
+    it('should return status code 422', async () => {
+      let response = await request(server)
+        .post('/games')
+        .send({
+          title: 'Galaga',
+          genre: 'Arcade',
+        });
+      expect(response.status).toBe(422);
+    });
+    it('should return statement about the game in an object', async () => {
+      let response = await request(server)
+        .post('/games')
+        .send({
+          title: 'Galaga',
+          genre: 'Arcade',
+          releaseYear: 
+        });
+      expect(response.status).toBe(422);
+    });
+  });
+});
