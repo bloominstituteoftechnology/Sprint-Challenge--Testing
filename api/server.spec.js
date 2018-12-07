4bfb8b3f5a5265413dfff114c81261d88f6b46a7
sean-one <basement365@gmail.com>
2018-12-07 09:59:28 -0800

    created three failing post test

--- api/server.spec.js
+++ api/server.spec.js
@@ -16,16 +16,41 @@ describe('server routes', () => {
     
     describe('get games route', () => {
         it('should check that the get games route is active', async () => {
-            const response = await request(server).get('/games');
+            const response = await request(server).get('/api/games');
             expect(response.status).toBe(441)
-        })
+        });
+
         it('should check for a list of games', async () => {
-            const response = await request(server).get('/games');
+            const response = await request(server).get('/api/games');
             expect(response.body).toEqual('list');
         });
+
         it('should have a response type of', async () => {
-            const response = await request(server).get('/games');
+            const response = await request(server).get('/api/games');
             expect(response.type).toBe('there');
+        });
+    });
+
+    describe('post game route', () => {
+        it('should check post route is active', async () => {
+            const response = await request(server)
+                .post('/api/addgame')
+                .send({ title: "Pacman", genre: "Arcade", releaseYear: 1980 });
+            expect(response.status).toBe(441);
+        });
+
+        it('should check servers response body', async () => {
+            const response = await request(server)
+                .post('/api/addgame')
+                .send({ title: "Pacman", genre: "Arcade", releaseYear: 1980 });
+            expect(response.body).toBe({ message: 'Pacman has been added!' })
+        });
+        
+        it('should check response type', () => {
+            const response = await request(server)
+                .post('/api/addgame')
+                .send({ title: "Pacman", genre: "Arcade", releaseYear: 1980 });
+            expect(response.body).toBe({ message: 'Pacman has been added!' })
         });
     });
 
