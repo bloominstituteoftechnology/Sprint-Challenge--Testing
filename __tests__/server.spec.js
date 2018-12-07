cf0f8f11a90c3d17fda40868385de5f3c200069e
ryanboris <ryan.boris@gmail.com>
2018-12-07 11:38:09 -0500

    Adds GET endpoint tests

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,6 +1,5 @@
 const request = require('supertest')
 const server = require('../api/server.js')
-
 describe('GET', () => {
 	describe('root "/" ', () => {
 		it('should respond with 200(OK)', async () => {
@@ -10,6 +9,21 @@ describe('GET', () => {
 		it('should provide a JSON response', async () => {
 			const response = await request(server).get('/')
 			expect(response.body).toEqual({ api: 'root endpoint is alive and well' })
+		})
+	})
+	describe('/api/games', () => {
+		it('should respond with 200(OK)', async () => {
+			const response = await request(server).get('/api/games')
+			expect(response.status).toBe(200)
+		})
+		it('should return an array', async () => {
+			const response = await request(server).get('/api/games/')
+			expect(typeof(response.body)).toBe('object')
+		})
+		it('should return an empty array when no games are in the database', async () => {
+			const response = await request(server).get('/api/games')
+			expect((typeof response.body)).toBe('object')
+			expect(response.body).toHaveLength(0)
 		})
 	})
 })
