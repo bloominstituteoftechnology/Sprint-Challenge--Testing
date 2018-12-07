fc7faf8c23c8edc016ecff9f555057ca5421c978
Matthew Wright <matthewbwright@gmail.com>
2018-12-07 09:53:59 -0700

    creates test for GET status of 200

--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,10 @@ const request = require('supertest');
 const server = require('./api/server.js');
 
 describe('GET endpoint /games', () => {
-  it('', async () => {});
+  it('returns a 200 status', async () => {
+    const response = await request(server).get('/games');
+    expect(response.status).toBe(200);
+  });
   it('', async () => {});
   it('', async () => {});
 });
