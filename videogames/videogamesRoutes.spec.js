eedc4311baf5d48e1729b0ab696d56692f6b9a7a
Elvis Ibarra <ielvisd@gmail.com>
2018-12-07 15:45:47 -0800

    Routes and Tests work

--- videogames/videogamesRoutes.spec.js
+++ videogames/videogamesRoutes.spec.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 
 const server = require('../api/server.js');
 
-describe('vgRoutes.js', () => {
+describe('videogamesRoutes.js', () => {
   describe('/api route', () => {
     it('should return status code 200', async () => {
       let response = await request(server).get('/api');
