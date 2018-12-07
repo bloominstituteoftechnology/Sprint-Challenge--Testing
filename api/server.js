d457d02e6442e0ff19c61aaf9ad4768c2f1448e0
SavannahGreuel <smallidgesavannah@gmail.com>
2018-12-07 10:09:28 -0700

    finished all tests, starting on routes

--- api/server.js
+++ api/server.js
@@ -10,5 +10,19 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'live' });
 });
 
+//POST
+server.post('/games', (req,res) => {
+  const { title, genre } = req.body;
+  if (title && genre) {
+    res.status(201).json(req.body)
+  } else {
+    res.status(422).json({ message: "Must provide a title and genre"})
+  }
+})
+
+//GET
+server.get('/games', (req,res) => {
+  Object.keys(req.body).length ? res.status(200).json([req.body]) : res.status(200).json([]);
+})
 
 module.exports = server;
