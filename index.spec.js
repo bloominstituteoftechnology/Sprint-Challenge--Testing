7a46621685362d32e46bf166836a4e96f3f8f230
zacharyarney <zachary.arney@gmail.com>
2018-12-07 12:06:52 -0600

    GET /games endpoint tests

--- index.spec.js
+++ index.spec.js
@@ -30,7 +30,24 @@ describe('server.js', () => {
           genre: 'Arcade',
           releaseYear: 1981,
         });
-      expect(response.status).toEqual({ wow: 'Galaga is a great Arcade game from 1981!' });
+      expect(response.status).toEqual({
+        wow: 'Galaga is a great Arcade game from 1981!',
+      });
+    });
+  });
+  
+  describe('GET /games endpoint', () => {
+    it('should return status code 200', async () => {
+      let response = await request(server).get('/games');
+      expect(response.status).toBe(200);
+    });
+    it('should return JSON', async () => {
+      let response = await request(server).get('/games');
+      expect(response.type).toBe('application/json');
+    });
+    it('should return list of games', async () => {
+      let response = await request(server).get('/games');
+      expect(response.body).toEqual({ the: 'list' });
     });
   });
 });
