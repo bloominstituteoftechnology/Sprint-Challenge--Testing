d590ef939a1315317ba35af328da57d2a4267b22
KHollobaugh <khollobaugh@gmail.com>
2018-12-07 12:29:05 -0500

    get endpoint finished

--- api/server.js
+++ api/server.js
@@ -8,6 +8,17 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: 'running'});
+});
+
+server.get('/api/games', (req, res) => {
+    db('games')
+    .select('title', 'genre', 'releaseYear')
+    .then(games => {
+        res.status(200).json(games);
+    })
+    .catch(err => {
+        res.status(400).json({message: 'error retrieving games'});
+    })
 })
 
 
