27874acf02eae1640288fb58d52a538285fb0596
MEBonham <programmer@mckaybonham.com>
2018-12-07 11:11:07 -0700

    Timeout error

--- index.spec.js
+++ index.spec.js
@@ -19,39 +19,39 @@ describe("games/get", () => {
     it("should return an array", async () => {
         const response = await request(server).get("/api/games");
         expect(response).toBeTruthy();
-        expect(response).toBe(200);
-        // expect(response.text.type).toBe("application/json");
-        expect(Array.isArray(response.text)).toBeTruthy();
+        // expect(response).toBe(200);
+        // expect(response.body.type).toBe("application/json");
+        expect(Array.isArray(response.body)).toBeTruthy();
     });
 
-    // it("should return status code 200", async () => {
-    //     const response = await request(server).get("/api/games");
-    //     expect(response.status).toBe(200);
-    // });
+    it("should return status code 200", async () => {
+        const response = await request(server).get("/api/games");
+        expect(response.status).toBe(200);
+    });
 
 });
 
 describe("games/post", () => {
 
-    // it("should return status 201 given a proper game object", async () => {
-    //     const newGame = {
-    //         title: "Super Mario Bros. 1",
-    //         genre: "Console",
-    //         releaseYear: 1985
-    //     };
-    //     const response = await request(server).post("/api/games")
-    //         .send(newGame);
-    //     expect(response.status).toBe(201);
-    // });
+    it("should return status 201 given a proper game object", async () => {
+        const newGame = {
+            title: "Super Mario Bros. 1",
+            genre: "Console",
+            releaseYear: 1985
+        };
+        const response = await request(server).post("/api/games")
+            .send(newGame);
+        expect(response).toBe(201);
+    });
 
-    // it("should return status 422 given incomplete game object", async () => {
-    //     const badGame = {
-    //         title: "Commander Keen 1"
-    //     };
-    //     const response = await request(server).post("/api/games")
-    //         .send(badGame);
-    //     expect(response.status).toBe(422);
-    // });
+    it("should return status 422 given incomplete game object", async () => {
+        const badGame = {
+            title: "Commander Keen 1"
+        };
+        const response = await request(server).post("/api/games")
+            .send(badGame);
+        expect(response.status).toBe(422);
+    });
 
     // it("should add to the length of games", async () => {
     //     const games = await request(server).get("api/games");
