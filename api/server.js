26e55843dde92a6fcb546bdab1c6b3c0e5ee0b51
Brian G <brian.gregorius@gmail.com>
2018-12-07 11:31:55 -0500

    created post endpoint and error test if post requirements are not met

--- api/server.js
+++ api/server.js
@@ -4,9 +4,39 @@ const server = express();
 
 server.use(express.json());
 
+
+const games = [
+    {
+        title: 'Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+      },
+      {
+        title: 'Contra', // required
+        genre: 'Arcade', // required
+        releaseYear: 1981 // not required
+      },
+      {
+        title: 'Commando', // required
+        genre: 'Arcade', // required
+        releaseYear: 1982 // not required
+      }
+];
+
+
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'server is up' });
 });
+
+server.post('/games', (req, res) => {
+    const { title, genre, releaseYear} = req.body;
+
+    if(!title || !genre){
+        res.status(422).json({message:`Both title and genre are required`});
+    } else{
+    res.status(200).json(games)};
+})
+
 
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -10,6 +10,22 @@ describe('server', () => {
         expect(response.status).toBe(200);
     });
 
+    describe('POST /games', async () => {
+        it('respond with a 200 status(ok)', async () => {
+            const response = await request(server)
+                .post('/games')
+                .send({ title: 'Mario', genre: 'Arcade', releaseYear: 1982 });
+            expect(response.status).toBe(200);
+        });
+
+        it('respond with a 400 status(fail)', async () => {
+            const response = await request(server)
+                .post('/games')
+                .send({ title: ''});
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({ message: `Both title and genre are required`})
+        });
+    })
 
 });
 
