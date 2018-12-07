bba71a623595f175ced82e980ba151fb612b67e4
Kat Fries <katherynejf@gmail.com>
2018-12-07 13:32:30 -0500

    add knex and sqlite3, migration, and seed

---
+++ data/migrations/20181207132230_01-all-games.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('allGames', table => {
+    // table.increments();
+
+    table.string('title', 255).notNullable();
+    table.string('genre', 255).notNullable();
+    table.integer('releaseYear', 255)
+  });
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('allGames');
+};
