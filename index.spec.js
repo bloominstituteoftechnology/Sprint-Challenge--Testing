2e1704cafd1ca9c4c6baf5a72236d80cea072c1d
Matthew Wright <matthewbwright@gmail.com>
2018-12-07 09:46:22 -0700

    sets up test structure for GET

---
+++ index.spec.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const server = require('./api/server.js');
+
+describe('GET endpoint /games', () => {
+  it('', async () => {});
+  it('', async () => {});
+  it('', async () => {});
+});
