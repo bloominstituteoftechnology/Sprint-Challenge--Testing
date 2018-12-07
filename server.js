fee950887e5d3b027ba723eded7a954823609df4
Yanrong Xu <xuyanrong@icloud.com>
2018-12-07 15:24:48 -0500

    fix the problem that kam tell me

--- server.js
+++ server.js
@@ -4,21 +4,35 @@ const server = express();
 
 server.use(express.json());
 
+const db = [
+    {
+        title: 'Pacman',
+        genre: 'Arcade',
+        releaseYear: 1980
+    },
+    {
+        title: 'Super Mario',
+        gerre: 'Switch',
+        releaseYear: '2018'
+    }
+]
+
 server.get('/', (req, res) => {
-    res.send('server is running')
+    res.status(200).json({api: 'server is running'})
 });
 
 server.post('/games', (req, res) => {
-    const {title, genre} = req.body;
-    if(title && genre) {
-        res.status(201).json(req.body);
+    const game = req.body;
+    if(game.title && game.genre) {
+        db.push(game);
+        res.status(201).json({message: 'game created'});
     } else {
         res.status(422).json({message: 'either title or genre are missing'})
     }
 });
 
 server.get('/games', (req, res) => {
-    Object.keys(req.body).length ? res.status(200).json([req.body]) : res.status(200).json([])
+    res.status(200).json(db);
 });
 
 module.exports = server;
