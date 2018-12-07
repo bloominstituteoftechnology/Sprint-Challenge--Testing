7d3678ae338956b7edd3abb4dcb4d6c7ca75271e
Hargobind Atwal <ethaipath@gmail.com>
2018-12-07 10:37:59 -0800

    wrote endpoints that access an array.

--- games.js
+++ games.js
@@ -5,12 +5,27 @@ const router = express();
 let games = []
 
 router.get('/', (q, s) => {
-  s.status(200).json(games)
+  if (games.length !== 0) {
+    s.status(200).json(games)
+  } else {
+
+    s.status(200).json([])
+  }
 })
 
 router.post('/', (q, s) => {
-  // const { title, genre, releaseYear } = req.body;
-  s.status(200).json({message: 'server up!'})
+  const { title, genre, releaseYear } = req.body;
+  if (title !== undefined 
+   && genre !== undefined 
+   && releaseYear !== undefined) {
+     s.status(200).json(
+       [games.push({title, genre, releaseYear})]
+     )
+   } else {
+     s.status(422).json({
+       message: 'incomplete obj'
+     })
+   }
 })
 
 
