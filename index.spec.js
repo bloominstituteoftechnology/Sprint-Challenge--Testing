0f59aaae7fab624793edd11c795ed25fd1d1d98f
Axolotl <samuelmachat@gmail.com>
2018-12-07 12:27:57 -0500

    write tests for get route

--- index.spec.js
+++ index.spec.js
@@ -39,18 +39,45 @@ describe('server.js', () => {
     })
   })
 
-  describe('DELETE /games', () => {
+  describe('GET /games', () => {
     it('should return 200 if good request is sent', async () => {
-      const item = 'walrus'
-      let response = await request(server)
-        .delete(`/games/`)
-        .send({ item })
+      let response = await request(server).get('/games')
       expect(response.status).toBe(200)
     })
 
-    it('should return 400 if bad request is sent', async () => {
-      let response = await request(server).delete('/games')
-      expect(response.status).toBe(400)
+    it('should return an array, even an empty one', async () => {
+      let response = await request(server).get('/games')
+      expect(Array.isArray(response.data)).toBe(true)
+    })
+
+    it('should return game objects in the correct format', async () => {
+      const response = await request(server).get('/games')
+      const { title, genre } = response
+      const valid = title && genre
+      expect(valid).toBe(true)
     })
   })
+
+  // describe('DELETE /games/:id', () => {
+  //   it('should return 201 if good request is sent', async () => {
+  //     const newGame = {
+  //       title: 'Pacman',
+  //       genre: 'Arcade',
+  //       releaseYear: 1980
+  //     }
+
+  //     await request(server)
+  //       .post('/games')
+  //       .send({ ...newGame })
+
+  //     await request(server)
+  //       .de
+  //     expect(response.status).toBe(201)
+  //   })
+
+  //   it('should return 400 if bad request is sent', async () => {
+  //     let response = await request(server).delete('/games')
+  //     expect(response.status).toBe(400)
+  //   })
+  // })
 })
