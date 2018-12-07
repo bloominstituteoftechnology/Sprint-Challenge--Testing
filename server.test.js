cfa56ebe8dcd8974a586ab02106a527a52ed0964
Edward J <edjeong99@gmail.com>
2018-12-07 15:58:42 -0500

    fix bug in test

--- server.test.js
+++ server.test.js
@@ -3,9 +3,8 @@ const request = require("supertest");
 const server = require("./server.js");
 
 beforeEach(async () => {
- const temp = await request(server).delete("/games");
-   });
-   
+  const temp = await request(server).delete("/games");
+});
 
 describe("Sanity Check - server up", () => {
   test("should server Up ", async () => {
@@ -14,9 +13,6 @@ describe("Sanity Check - server up", () => {
     expect(response.body).toEqual({ message: "server up" });
   });
 });
-
-
-
 
 describe("/games route POST", () => {
   const input = {
@@ -31,22 +27,20 @@ describe("/games route POST", () => {
       .send();
     expect(response.status).toBe(422);
 
-    reponse = await request(server)
+    response = await request(server)
       .post("/games")
       .send({ title: "Pang" });
     expect(response.status).toBe(422);
 
-    reponse = await request(server)
-    .post("/games")
-    .send({ genre: "home" });
-  expect(response.status).toBe(422);
+    response = await request(server)
+      .post("/games")
+      .send({ genre: "home" });
+    expect(response.status).toBe(422);
 
-  reponse = await request(server)
-  .post("/games")
-  .send({ titl: "Pacman",
-  genre: "Arcade", });
-expect(response.status).toBe(422);
-
+    response = await request(server)
+      .post("/games")
+      .send({ titl: "Pacman", genre: "Arcade" });
+    expect(response.status).toBe(422);
   });
 
   test("should return posted object when correct input is given ", async () => {
@@ -58,48 +52,43 @@ expect(response.status).toBe(422);
   });
 });
 
-
-
 describe("/game route GET", () => {
-    const input = {
-        title: "Pacman",
-        genre: "Arcade",
-        releaseYear: 1980
-      };
-      const input2 = {
-        title: "Wing",
-        genre: "PC",
-        releaseYear: 2050
-      };
+  const input = {
+    title: "Pacman",
+    genre: "Arcade",
+    releaseYear: 1980
+  };
+  const input2 = {
+    title: "Wing",
+    genre: "PC",
+    releaseYear: 2050
+  };
 
+  test("should return empty array ", async () => {
+    // const temp = await request(server).delete("/games");
+    let response = await request(server).get("/games");
 
-   
-    test("should return empty array ", async () => {
-   
-        // const temp = await request(server).delete("/games");
-        let response = await request(server).get("/games");
-
-        expect(Array.isArray(response.body)).toBe(true);
+    expect(Array.isArray(response.body)).toBe(true);
+    // expect(typeof response.bogy).toBe('array');
     expect(response.body).toEqual([]);
   });
 
   test("should return array of games ", async () => {
-    
     await request(server)
       .post("/games")
       .send(input);
 
-      await request(server)
+    await request(server)
       .post("/games")
       .send(input2);
 
     //   const temp = await request(server).delete("/games");
-     
+
     let response = await request(server).get("/games");
 
     expect(Array.isArray(response.body)).toBe(true);
     expect(response.body).toEqual([
-        {
+      {
         title: "Pacman",
         genre: "Arcade",
         releaseYear: 1980
@@ -110,6 +99,5 @@ describe("/game route GET", () => {
         releaseYear: 2050
       }
     ]);
-  
   });
 });
