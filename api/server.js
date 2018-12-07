501f7f367ca55de29013f94ce82df9f69f6a1a60
Stublink <chrisrey95@gmail.com>
2018-12-07 13:32:40 -0500

    added support and test for unique titles + delete '/games/:id route

--- api/server.js
+++ api/server.js
@@ -4,7 +4,23 @@ const server = express();
 
 server.use(express.json());
 
-let games = [];
+let games = [
+    {
+        title: 'Not Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+    },
+    {
+        title: 'Not Not Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+    },
+    {
+        title: 'Not Not Not Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+    }
+];
 
 server.get('/', (req, res) => {
     res.status(200).json({server: 'is up'})
@@ -19,9 +35,33 @@ server.post('/games', (req, res) => {
         res.status(422).json({error: 'title and genre must be provided'})
         return;
     }
+
+    const gameTitles = games.map(game => game.title);
+
+    if(gameTitles.indexOf(req.body.title) !== -1) {
+        res.status(405).json({error: 'title already exists'})
+        return;
+    }
+
     games.push(req.body);
     const index = games.length - 1;
     res.status(201).json({index});
 })
+
+
+server.delete('/games/:id', (req, res) => {
+    const id = req.params.id
+    if (games.length <= id) {
+        res.status(404).json({error: 'name does not exist'})
+        return;
+    }
+
+    let copy = [...games];
+    copy.splice(id, 1);
+    games = copy;
+    
+    res.status(200).json({deleted: 'game'});
+})
+
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -15,6 +15,31 @@ describe('server', () => {
         });
     });
 
+
+    describe('/games get route', () => {
+        it('returns status 200', async () => {
+            let response = await request(server).get('/games');
+            expect(response.status).toBe(200);
+        });
+        
+        it('should return array', async () => {
+            let response = await request(server).get('/games');
+            const games = response.body;
+            expect(Array.isArray(games)).toEqual(true);
+        });
+
+        it('should return array of objects', async () => {
+            let response = await request(server).get('/games');
+            const games = response.body;
+            if (games.length > 0) {
+                expect(typeof games[0]).toEqual('object');
+            } else {
+                expect(Array.isArray(games)).toEqual(true);
+            }
+
+        });
+    });
+
     describe('/games post route', () => {
         const gameMock = {
             title: 'Pacman', // required
@@ -25,6 +50,36 @@ describe('server', () => {
         it('should return status code 201 if post is succesful', async () => {
             let response = await request(server).post('/games').send(gameMock);
             expect(response.status).toBe(201);
+            const index = response.body.index;
+            await request(server).delete(`/games/${index}`)
+        });
+
+        it('should return the index of new post added', async () => {
+            let response = await request(server).post('/games').send(gameMock);
+            
+            const index = response.body.index;
+            expect(typeof index).toEqual('number');
+            await request(server).delete(`/games/${index}`)
+        });
+
+        it('index returned matches index in db', async () => {
+            let response = await request(server).post('/games').send(gameMock);
+            expect(response.status).toBe(201);
+
+            const index = response.body.index;
+            response = await request(server).get('/games')
+            const games = response.body;
+
+            expect(games[index]).toEqual(gameMock);
+            await request(server).delete(`/games/${index}`)
+        });
+
+        it('should return status code 405 if title is not unique', async () => {
+            let response = await request(server).post('/games').send(gameMock);
+            const index = response.body.index;
+            response = await request(server).post('/games').send(gameMock);
+            expect(response.status).toBe(405);
+            await request(server).delete(`/games/${index}`)
         });
 
         it('should respond with status code 422 if title is not passed', async () => {
@@ -46,49 +101,33 @@ describe('server', () => {
 
             let response = await request(server).post('/games').send(releaseOmmited);
             expect(response.status).toBe(201);
-        });
-
-        it('should return the index of new post added', async () => {
-            let response = await request(server).post('/games').send(gameMock);
-            
             const index = response.body.index;
-            expect(typeof index).toEqual('number');
+            await request(server).delete(`/games/${index}`)
         });
-
-        it('index returned matches index in db', async () => {
-            let response = await request(server).post('/games').send(gameMock);
-            expect(response.status).toBe(201);
-
-            const index = response.body.index;
-            response = await request(server).get('/games')
-            const games = response.body;
-
-            expect(games[index]).toEqual(gameMock);
-        });
-
-        it('should return status code 405 if title is not unique', async () => {
-            let response = await request(server).post('/games').send(gameMock);
-            expect(response.status).toBe(405);
-        });
-
     });
 
-    describe('/games get route', () => {
-        it('returns status 200', async () => {
+    describe('/games delete route', () => {
+        it('returns status 200 when delete is succesful', async () => {
+            
             let response = await request(server).get('/games');
-            expect(response.status).toBe(200);
+            const index = response.body.length - 1;
+
+            response = await request(server).delete(`/games/${index}`);
+
+            expect(response.status).toEqual(200)
         });
         
-        it('should return array', async () => {
-            let response = await request(server).get('/games');
-            const games = response.body;
-            expect(Array.isArray(games)).toEqual(true);
-        });
-
-        it('should return array of objects', async () => {
+        it('should return 404 if index cant be found', async () => {
             let response = await request(server).get('/games');
 
-            expect(typeof response.body[0]).toEqual('object');
+            const length = response.body.length;
+            
+            response = await request(server).delete(`/games/${length+1}`);
+
+            expect(response.status).toEqual(404);
         });
+
+        // return 200 on succesful delete
+        // return 404 if index is not found
     });
 });
