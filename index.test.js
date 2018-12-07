7c9200ed387d9af84e1c4a0d76bd2ca185b940c3
lajawanti <myemail>
2018-12-07 12:21:04 -0500

    TWO TEST for POST ROUTE '/games'

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ const server = require('./server.js');
 //TESTS FOR SERVER.JS TO TEST ALL ROUTE FUNCTIONING...
 describe('server.js', () => {
     
-    //TEST FOR FIRST BASIC ROUTE...
+    //TEST FOR FIRST BASIC ROUTE...(THREE TEST CASES GROUPED UNDER THIS des)
     describe(" BASIC ROUTE '/' ", () => {
         it('should return status code 200', async () => {
              let response = await request(server).get('/');
@@ -21,5 +21,23 @@ describe('server.js', () => {
              let response = await request(server).get('/');
              expect(response.body).toEqual({message : "SERVER UP"});
         });
+    });
+
+    //TEST FOR FIRST POST ROUTE..'/games' (TWO TEST CASES GROUPED UNDER THIS des)
+    describe(" POST ROUTE '/games' ", () => {
+        it('should return status code 201 on success', async () => {
+             let response = await request(server).post('/games')
+                                                 .send({ title: 'Pacman', 
+                                                         genre: 'Arcade',
+                                                         releaseYear: 1980 });
+             expect(response.status).toBe(201);
+        })
+
+        it('should return status code 422 if data is not there', async () => {
+            let response = await request(server).post('/games')
+                                                .send({});
+            expect(response.status).toBe(422);
+       })
+        
     });
 });
--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const server = express();
 
 server.use(express.json());
 
-//BASIC ROUTE
+//BASIC ROUTE....
 server.get('/', (req, res) => {
     res.status(200).json({message : "SERVER UP"})
+})
+
+//POST ROUTE...
+server.post('/games', (req, res) => {
+     const {title, genre} = req.body;
+     if(!title || !genre) {
+          res.status(422).json({message : 'title and genre are required.'})
+     } else {
+          res.status(201).json({message : 'Successfully created.'})
+     }
 })
 
 module.exports = server;
