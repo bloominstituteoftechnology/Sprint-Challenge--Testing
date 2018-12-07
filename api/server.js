b54412aaf8e3d3a9f7ff14d145063b2e108a831b
Hunter315 <35641380+Hunter315@users.noreply.github.com>
2018-12-06 23:34:06 -0700

    Added games DB to server.js and fixed expecged test within GET /games test

--- api/server.js
+++ api/server.js
@@ -1,10 +1,40 @@
 const express = require('express');
+
 const server = express();
 
 server.use(express.json());
 
+// DATABASE
+let gamesDb = [
+    {
+        title: 'Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+    },
+    {
+        title: 'Rocket Knight Adventures', // required
+        genre: 'Platformer', // required
+        releaseYear: 1993 // not required
+    },
+    {
+        title: 'Metal Slug', // required
+        genre: 'Platformer', // required
+        releaseYear: 1996 // not required
+    },
+    {
+        title: 'Sonic R', // required
+        genre: 'Racing', // required
+        releaseYear: 1997 // not required
+    }
+    
+]
+
 server.get('/', (req, res) => {
     res.status(200).json({message: 'My server is working!'});
+});
+
+server.get('/games', (req, res) => {
+    res.status(200).json({data: gamesDb});
 });
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -1,60 +1,61 @@
 const request = require('supertest');
 const server = require('./api/server.js');
- describe('Server', () => {
+
+describe('Server', () => {
     describe('GET /', () => {
         it('returns status code 200(OK)', async () => {
             const res = await request(server).get('/');
             expect(res.status).toBe(200);
         });
-         it('returns JSON', async () => {
+
+        it('returns JSON', async () => {
             const res = await request(server).get('/');
             expect(res.type).toBe('application/json');
         });
-         it('returns {message: "Server is up!"', async () => {
+
+        it('returns {message: "Server is up!"', async () => {
             const res = await request(server).get('/');
             expect(res.body).toEqual({message: 'Server is up!'});
         });
     });
-     
-});
 
-describe('GET /games', () => {
-    it('returns status code 200(OK)', async () => {
-        const res = await request(server).get('/games');
-        expect(res.status).toBe(200);
-    });
-    
-    it('returns JSON', async () => {
-        const res = await request(server).get('/games');
-        expect(res.type).toBe('application/json');
-    });
-     it('returns an array of objects of games', async () => {
-        const expected = [
-            {
-                title: 'Pacman', // required
-                genre: 'Arcade', // required
-                releaseYear: 1980 // not required
-            },
-            {
-                title: 'Rocket Knight Adventures', // required
-                genre: 'Platformer', // required
-                releaseYear: 1993 // not required
-            },
-            {
-                title: 'Metal Slug', // required
-                genre: 'Platformer', // required
-                releaseYear: 1996 // not required
-            },
-            {
-                title: 'Sonic R', // required
-                genre: 'Racing', // required
-                releaseYear: 1997 // not required
-            }
-            
-        ]
-        const res = await request(server).get('/games');
-        expect(res.body).toBe(expected);
+    describe('GET /games', () => {
+        it('returns status code 200(OK)', async () => {
+            const res = await request(server).get('/games');
+            expect(res.status).toBe(200);
+        });
+        
+        it('returns JSON', async () => {
+            const res = await request(server).get('/games');
+            expect(res.type).toBe('application/json');
+        });
+
+        it('returns an array of objects of games', async () => {
+            const expected = [
+                {
+                    title: 'Pacman', // required
+                    genre: 'Arcade', // required
+                    releaseYear: 1980 // not required
+                },
+                {
+                    title: 'Rocket Knight Adventures', // required
+                    genre: 'Platformer', // required
+                    releaseYear: 1993 // not required
+                },
+                {
+                    title: 'Metal Slug', // required
+                    genre: 'Platformer', // required
+                    releaseYear: 1996 // not required
+                },
+                {
+                    title: 'Sonic R', // required
+                    genre: 'Racing', // required
+                    releaseYear: 1997 // not required
+                }
+                
+            ]
+            const res = await request(server).get('/games');
+            expect(res.body.data).toEqual(expected);
+        });
     });
 });
-}); 	
-}); 
