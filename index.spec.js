1d092386414df6f4288f5260d8f023c5a731fd5e
Hunter315 <35641380+Hunter315@users.noreply.github.com>
2018-12-06 23:17:16 -0700

    GET test in index.spec.js

---
+++ index.spec.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const server = require('./api/server.js');
+ describe('Server', () => {
+    describe('GET /', () => {
+        it('returns status code 200(OK)', async () => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+         it('returns JSON', async () => {
+            const res = await request(server).get('/');
+            expect(res.type).toBe('application/json');
+        });
+         it('returns {message: "Server is up!"', async () => {
+            const res = await request(server).get('/');
+            expect(res.body).toEqual({message: 'Server is up!'});
+        });
+    });
+     
+});
