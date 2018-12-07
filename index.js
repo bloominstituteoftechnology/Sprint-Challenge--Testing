bc824abf1d6b23bc703866f815f1a5114c162570
koennecker <koenneckerb@gmail.com>
2018-12-07 11:26:30 -0700

    Complete POST tests.

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ server.get("/games", (req, res) => {
   res.status(200).json(games);
 });
 
+server.post("/games", (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+  if (!title || !genre) {
+    res.status(422).json({ error: "Please provide a title and genre." });
+    return;
+  }
+  const newGame = { title, genre, releaseYear, id: gameId };
+  games.push(newGame);
+  res.status(201).json(newGame);
+  gameId++;
+});
+
 server.listen(9000, () => {
   console.log("Listening on port 9000.");
 });
--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,7 @@ const server = require("./index");
 const request = require("supertest");
 
 describe("index.js", () => {
-  describe("/games route", () => {
+  describe("GET /games route", () => {
     it("should return status code 200", async () => {
       let response = await request(server).get("/games");
       expect(response.status).toBe(200);
@@ -16,6 +16,44 @@ describe("index.js", () => {
     it("should return an array, even if there are no games stored", async () => {
       let response = await request(server).get("/games");
       expect(Array.isArray(response.body)).toBe(true);
+    });
+  });
+  describe("POST /games route", () => {
+    it("should return the added game", async () => {
+      let response = await request(server)
+        .post("/games")
+        .send({
+          title: "Pacman",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(response.body).toEqual({
+        title: "Pacman",
+        genre: "Arcade",
+        releaseYear: 1980,
+        id: 1
+      });
+    });
+    it("return a 201 success code if successful", async () => {
+      let response = await request(server)
+        .post("/games")
+        .send({
+          title: "Pacman",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(response.status).toBe(201);
+    });
+    it("should return an error if title and genre aren't both provided", async () => {
+      let response = await request(server)
+        .post("/games")
+        .send({
+          title: "Pacman"
+        });
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error: "Please provide a title and genre."
+      });
     });
   });
 });
