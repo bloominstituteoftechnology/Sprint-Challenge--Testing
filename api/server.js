164b4b8f71bf6ab9f3ef925d4b031325cbf09a89
Kat Fries <katherynejf@gmail.com>
2018-12-07 11:59:32 -0500

    added post request to /games endpoint for title, genre, and releaseYear

--- api/server.js
+++ api/server.js
@@ -6,6 +6,12 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'Ready!' })
 })
 
+server.post('/games', (req, res) => {
+  const { title, genre, releaseYear } = req.body;
+
+  res.status(200).json({ add: `${genre}: ${title}` })
+})
+
 const port = process.env.PORT || 7200;
 
 module.exports = server;
