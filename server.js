d7163a2f30b2e584c0e7ef6145288351dd7c4718
Brandon Strellis <bstrellis@gmail.com>
2018-12-07 11:58:03 -0500

    Added POST and GET requests to db in server.js. Yes, I know I'm doing this out of order.

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const db = require('./database/db');
 
 const server = express();
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up' });
+});
+
+server.post('/games', async (req, res) => {
+    try {
+        const newGame = req.body;
+        console.log('newGame', newGame);
+        const insertionCount = await db('games').insert(newGame);
+        res.status(201).json(insertionCount);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 server.get('/games', async (req, res) => {
     try {
-
+        const gamesArr = await db('games');
+        res.status(200).json(gamesArr);
     } catch(err) {
         console.log(err);
         res.status(500).json(err);
