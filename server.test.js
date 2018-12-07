a6689ab3dadba6ab5c3973cb39c6d61b94a88421
Edward J <edjeong99@gmail.com>
2018-12-07 11:52:56 -0500

    add test for GET /games

--- server.test.js
+++ server.test.js
@@ -2,19 +2,7 @@ const request = require("supertest");
 
 const server = require("./server.js");
 
-/*
-describe("/ route", () => {
 
-});
-
-
-it("should return json response type ", async () => {
-    let response = await request(server).get("/");
-
-    expect(response.type).toBe("application/json");
-  });
-
-*/
 
 describe("Sanity Check - server up", () => {
   test("should server Up ", async () => {
@@ -24,28 +12,45 @@ describe("Sanity Check - server up", () => {
   });
 });
 
-describe("/game route POST", () => {
+describe("/games route POST", () => {
   const input = {
     title: "Pacman",
     genre: "Arcade",
     releaseYear: 1980
   };
+  const input2 = {
+    title: "Wing",
+    genre: "PC",
+    releaseYear: 2050
+  };
 
-  test("incomplete input ", async () => {
+  test("should return 422 for given incomplete input ", async () => {
     let response = await request(server)
-      .post("/")
+      .post("/games")
       .send();
     expect(response.status).toBe(422);
 
     reponse = await request(server)
-      .post("/")
+      .post("/games")
       .send({ title: "Pang" });
     expect(response.status).toBe(422);
+
+    reponse = await request(server)
+    .post("/games")
+    .send({ genre: "home" });
+  expect(response.status).toBe(422);
+
+  reponse = await request(server)
+  .post("/games")
+  .send({ titl: "Pacman",
+  genre: "Arcade", });
+expect(response.status).toBe(422);
+
   });
 
-  test("correct input ", async () => {
+  test("should return posted object when correct input is given ", async () => {
     let response = await request(server)
-      .post("/")
+      .post("/games")
       .send(input);
     expect(response.status).toBe(200);
     expect(response.body.title).toBe("Pacman");
@@ -53,9 +58,37 @@ describe("/game route POST", () => {
 });
 
 describe("/game route GET", () => {
-  test("should server Up ", async () => {
-    let response = await request(server).get("/");
+  test("should return empty array ", async () => {
+    let response = await request(server).get("/games");
 
-    expect(response).toBe({ message: "server up" });
+    expect(typeof response.body).toBe(array);
+    expect(response.body).toBe([]);
+  });
+
+  test("should return array of games ", async () => {
+    // add 2 games info to test GET
+    await request(server)
+      .post("/games")
+      .send(input);
+
+      await request(server)
+      .post("/games")
+      .send(input2);
+
+    let response = await request(server).get("/games");
+
+    expect(typeof response.body).toBe(array);
+    expect(response.body).toBe([
+        {
+        title: "Pacman",
+        genre: "Arcade",
+        releaseYear: 1980
+      },
+      {
+        title: "Wing",
+        genre: "PC",
+        releaseYear: 2050
+      }
+    ]);
   });
 });
