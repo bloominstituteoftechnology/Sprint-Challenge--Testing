88eceeb645da6e581236718109558581683b1e2f
ToMMaaY <thuynh629@gmail.com>
2019-02-01 08:19:30 -0800

    created the schema for the games table

---
+++ data/migrations/20190201081409_games.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+   return knex.schema.createTable('games', tbl => {
+      tbl.string('title', 255).notNullable();
+      tbl.string('genre', 255).notNullable();
+      tbl.string('releaseYear', 4).notNullable();
+   });
+};
+
+exports.down = function(knex, Promise) {
+   return knex.schema.dropTableIfExists('games');
+};
