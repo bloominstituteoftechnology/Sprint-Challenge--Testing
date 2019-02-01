4740cb322af21019bce32c76f8eb4e3b1d596cf5
ToMMaaY <thuynh629@gmail.com>
2019-02-01 08:22:31 -0800

    updated schema to auto increment id

--- data/migrations/20190201081409_games.js
+++ data/migrations/20190201081409_games.js
@@ -1,5 +1,6 @@
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('games', tbl => {
+      tbl.increments();
       tbl.string('title', 255).notNullable();
       tbl.string('genre', 255).notNullable();
       tbl.string('releaseYear', 4).notNullable();
