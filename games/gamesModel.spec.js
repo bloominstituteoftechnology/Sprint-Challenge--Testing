5235fd74a2bb24c20f319c70891b9d8bae316599
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-01 13:14:33 -0500

    Added test for status code

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -1,12 +1,17 @@
 const request = require('supertest');
+const express = require('express');
 const db = require('../data/dbConfig');
 const gamesModel = require('./gamesModel');
+const server = require('../api/server');
+
+const app = express();
 
 afterEach(async () => {
   await db('gameTable').truncate();
+  jest.setTimeout(10000);
 });
 
-describe('Game model', () => {
+describe('Game post model', () => {
   it('Should insert provided game', async () => {
     const myGame = await gamesModel.insert({
       title: 'FIFA',
@@ -14,7 +19,26 @@ describe('Game model', () => {
       releaseYear: 2010
     });
     const games = await gamesModel.getAll();
+    // console.log('MY game', myGame);
+    // console.log('Games =', games);
+    // console.log('GAME STATUS', myGame.status);
     expect(games).toHaveLength(1);
     expect(games[0].title).toEqual('FIFA');
+    expect(games[0].genre).toEqual('Sports');
+    expect(games[0].releaseYear).toEqual(2010);
+  });
+
+  it('Check status code', async done => {
+    const myGame = {
+      title: 'FIFA',
+      genre: 'Sports',
+      releaseYear: 2010
+    };
+    myPort = 'http://localhost:4000';
+    const response = await request(myPort)
+      .post('/games')
+      .send(myGame);
+    expect(response.status).toBe(201);
+    done();
   });
 });
