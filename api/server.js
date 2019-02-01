6e536051defb4fc927a4c84f768c178eaf6e47ea
ToMMaaY <thuynh629@gmail.com>
2019-02-01 08:11:13 -0800

    created the endpoints for /GET and /POST

--- api/server.js
+++ api/server.js
@@ -5,4 +5,15 @@ const db = require('../data/dbConfig');
 
 configMiddleware(server);
 
+server.get('/games', async (res, req) => {
+   const games = await db('games');
+   res.statusCode(200).json(games);
+});
+
+server.post('/games', async (req, res) => {
+   const game = req.body;
+   const result = await db('games').insert(game);
+   res.status(201).json(result);
+});
+
 module.exports = server;
