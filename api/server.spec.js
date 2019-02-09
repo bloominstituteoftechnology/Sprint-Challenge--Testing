b9bc433ac39600f235ec023f4e3ddd5acb55f30c
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-09 09:53:50 -0800

    Wrapping up basic file structure setup. Initial commit.

---
+++ api/server.spec.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('Checking the get endpoint for /games', () => {
+
+});
+
+describe('Checking the post endpoint for /games', () => {
+    
+});
