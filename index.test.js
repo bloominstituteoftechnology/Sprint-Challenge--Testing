2932e605ec68ad5ed6b71d674e034205b2649774
Robert Billington <robertbillington@Roberts-MacBook-Pro.local>
2019-02-09 11:56:56 -0600

    Get games endpoint tests writrten

--- index.test.js
+++ index.test.js
@@ -2,3 +2,22 @@ const request = require('supertest');
 const sever = require('./server');
 
 const games = require('./gameModel');
+
+describe('Route handlers', () =>{
+    describe('Get endpoints for games', () =>{
+        it('Should return a 200 status code', async() =>{
+            const response = await request(server).get('/games');
+            expect(response.status).toBe(200);
+
+        });
+        it('Should return in JSON format', async()=>{
+            const response = await request(server).get('/games');
+            expect(response.type).toMatch(/json/i);
+        });
+        it('Should return in an array format', async() =>{
+            const response = await request(server).get('/games');
+            expect(Array.isArray(response.body)).toBeTruthy();
+        });
+
+    });
+});
--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const games = require('./gameModel');
 server.use(express.json());
 
 
+// The GET /games endpoint should return the list of games and HTTP status code 200.
+//  Write a test to make sure this endpoint always returns an array, even if there are no games stored. 
+//  If there are no games to return, the endpoint should return an empty array.
+
+
+
+
+// n the route handler, validate that the required fields are included inside the body. 
+// If the information is incomplete, return a 422 status code.
+
+//  Write tests to verify that the endpoint returns the correct HTTP status code when receiving 
+//  correct and incorrect game data.
+
 
 module.exports = {
     server
