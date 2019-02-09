2159ca55a30d898d66a2658530daa519b0c8b2d1
Rob Salzberg <robsalzberg@gmail.com>
2019-02-09 13:59:20 -0500

    Created GET /games endpoint

--- api/server.js
+++ api/server.js
@@ -2,4 +2,18 @@ const express = require("express");
 
 const server = express();
 
+const games = [
+  {
+    title: "Pac-man",
+    genre: "arcade",
+    releaseYear: 1980
+  }
+];
+
+server.use(express.json());
+
+server.get("/games", (req, res) => {
+  res.status(200).json(games);
+});
+
 module.exports = server;
