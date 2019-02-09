c62d07abd9dd6bb0bd4355d62a31ec28ed32be22
Emmanium <pradoe.ep@gmail.com>
2019-02-09 12:32:57 -0700

    made GET and POST endpoints to /games

--- api/server.js
+++ api/server.js
@@ -7,9 +7,32 @@ const server = express();
 server.use(express.json());
 
 server.get('/games', async (req, res) => {
+  try {
+    const rows = await games.getAll();
+
+    res.status(200).json(rows);
+  }
+
+  catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
 
 })
 
 server.post('/games', async (req, res) => {
-  
+  try {
+    const gameData = req.body;
+
+    if (gameData.name) {
+      const count = await games.insert(gameData);
+      res.status(201).json(ids);
+    } else {
+      res.status(400).json({ error: 'Body missing info' });
+    }
+  }
+
+  catch (err) {
+    res.status(500).json({ error: 'Database go boom' });
+  }
+
 })
---
+++ api/server.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+const db = require('../data/dbConfig');
+
+describe('the route handlers', () => {
+
+  describe('GET /games', () => {
+
+  })
+
+  describe('POST /games', () => {
+
+  })
+
+})
