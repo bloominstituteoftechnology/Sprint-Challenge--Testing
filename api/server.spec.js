ec2d4cffd61b5da07f7ac87b1f0ab0a9cf300a26
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-02-09 14:00:47 -0700

    Get endpoint tests written

--- api/server.spec.js
+++ api/server.spec.js
@@ -29,6 +29,71 @@ describe('server endpoints', () => {
 
     describe('get /games', () => {
 
+        afterEach(async () => {
+            data.clear();
+        });
+
+        test('responds with 200', async () => {
+            data.add({
+              title: 'Pacman',
+              genre: 'Arcade',
+              releaseYear: 1980
+            });
+            const response = await request(server).get(GET_GAMES_ENDPOINT);
+            expect(response.status).toBe(200);
+        });
+
+        test('responds with json', async () => {
+            data.add({
+              title: 'Pacman',
+              genre: 'Arcade',
+              releaseYear: 1980
+            });
+            const response = await request(server).get(GET_GAMES_ENDPOINT);
+            expect(response.type).toMatch(/json/i);
+        });
+
+        test('responds with array of teams', async () => {
+            data.add({
+              title: 'Pacman',
+              genre: 'Arcade',
+              releaseYear: 1980
+            });
+            data.add({
+              title: 'The Legend of Zelda',
+              genre: 'Action-Adventure',
+              releaseYear: 1986
+            });
+            data.add({
+              title: 'Sim City',
+              genre: 'Simulation',
+              releaseYear: 1989
+            });
+            const response = await request(server).get(GET_GAMES_ENDPOINT);
+            expect(response.body).toEqual([
+            {
+                title: 'Pacman',
+                genre: 'Arcade',
+                releaseYear: 1980
+              },
+            {
+                title: 'The Legend of Zelda',
+                genre: 'Action-Adventure',
+                releaseYear: 1986
+              },
+            {
+                title: 'Sim City',
+                genre: 'Simulation',
+                releaseYear: 1989
+              }
+            ]);
+        });
+
+        test('responds with empty array', async () => {
+            const response = await request(server).get(GET_GAMES_ENDPOINT);
+            expect(response.body).toEqual([]);
+        });
+
     });
 
     describe('post /games', () => {
