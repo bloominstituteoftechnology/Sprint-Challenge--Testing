315bce5499805f98cd9883a9a9c393c23ff7a2b9
Julie Jonak <juliejonak10@gmail.com>
2019-02-09 13:41:19 -0600

    Reconnected test.db3

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   testing: {
     client: 'sqlite3',
     connection: {
-      filename: './data/games.db3',
+      filename: './data/test.db3',
     },
     useNullAsDefault: true,
     migrations: {
