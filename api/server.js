8b74f5fac9e7e9d55d6900c2cb3c92484bb74ce1
cferrell2 <cferrell12182@gmail.com>
2019-02-09 13:25:23 -0500

    Create tests within index.spec.js for the GET api. Create GET route handler within server.js.  Verify that all tests pass.

--- api/server.js
+++ api/server.js
@@ -5,9 +5,18 @@ const server = express();
 //add builtin middleware
 server.use(express.json());
 
+//import .json data file
+const filename = '../data/games.json';
+const games = require(filename);
+
 //test server
 server.get('/', (req, res)=>{
   res.json({Server:'Up and running!'});
+})
+
+//GET Route Handler
+server.get('/games', (req, res)=>{
+  res.status(200).json(games);
 })
 
 
--- index.spec.js
+++ index.spec.js
@@ -4,6 +4,34 @@ const server = require('./api/server.js');
 //import supertest
 const request = require('supertest');
 
-describe('GET /', ()=>{
-  
-})
+
+describe('GET /games',()=>{
+
+  it('should respond with status code 200', async ()=>{
+    const response = await request(server).get('/games');
+    expect(response.status).toBe(200);
+  });
+
+  it('should return type JSON', async ()=>{
+    const response = await request(server).get('/games');
+    expect(response.type).toBe('application/json');
+  });
+
+  it('should return all games', async ()=>{
+    const response = await request(server).get('/games');
+    const expected = [
+      {
+        "title": "Pacman",
+        "genre": "Arcade",
+        "releaseYear": "1980"
+      },
+      {
+        "title": "Candy Land",
+        "genre": "Board",
+        "releaseYear": "1949"
+      }
+    ]
+    expect(response.body).toEqual(expected);
+  });
+
+});
