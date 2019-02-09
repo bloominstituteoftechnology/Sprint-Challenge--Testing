577431aa35e33c46be69cf926fff743848d91867
shannonhealy <sahealy525@gmail.com>
2019-02-09 15:35:02 -0500

    GET route testing

--- server.spec.js
+++ server.spec.js
@@ -1,15 +1,14 @@
 const request = require("supertest");
 const server = require("./server.js");
 
-describe("GET route for '/games'", () => {
+describe("GET route for '/games' while empty", () => {
     it("returns status 200", async () => {
-        
+        const response = await request(server).get("/games");
+        expect(response.status).toBe(200);
     });
     it("returns empty array if no games are stored", async () => {
-        
-    });
-    it("returns array of all games stored", async () => {
-        
+        const response = await request(server).get("/games");
+        expect(response.body).toBe([]);
     });
 });
 
@@ -25,5 +24,13 @@ describe("POST route for '/games'", () => {
     });
     it("returns new item id if data posts successfully", async () => {
 
+    });
+});
+
+describe("GET route for '/games' while popoulated", () => {
+    it("returns array of all games stored", async () => {
+        //should get back the games posted to it while testing post route (?)
+        const response = await request(server).get("/games");
+        expect(response.body.length).toBe(2);
     });
 });
