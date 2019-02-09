c4cce5e1d51208afcc26704275b2fa8a07f59945
paulapivat <paul.apivat@gmail.com>
2019-02-10 02:23:30 +0700

    wrote test for POST /games to verify endpoint returns correct status when receiving correct data

--- api/server.js
+++ api/server.js
@@ -15,4 +15,14 @@ server.get('/games', async (req , res) => {
     res.status(200).json(rows);
 })
 
+server.post('/games', async (req, res) => {
+    const gameData = req.body;
+    if (gameData.title){
+        const ids = await games.insert(gameData);
+        res.status(201).json(ids);
+    } else {
+        res.status(400).json({error: "missing title in body "});
+    }
+})
+
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -16,6 +16,26 @@ describe('the route handlers', () => {
             const response = await request(server).get('/games');
             expect(response.body).toEqual([{"genre": "Arcade", "id": 1, "releaseYear":1980, "title": "Pacman"}, {"genre": "Action", "id": 2, "releaseYear": 1985, "title": "Contra"}, {"genre": "Arcade", "id": 3, "releaseYear": 1983, "title": "Donkey Kong"}]);
         });
-    })
+    });
 
+    describe('post /games', () => {
+
+        // afterEach(async () => {
+        //     await db('games').truncate();
+        // });
+
+        it('responds with 201 when body is correct', async () => {
+            const body = { title: "Pacman", genre: "Arcade"};
+
+            const response = await request(server)
+                .post('/games')
+                .send(body);
+            
+            expect(response.status).toBe(201)
+
+        })
+
+        
+
+    })
 })
