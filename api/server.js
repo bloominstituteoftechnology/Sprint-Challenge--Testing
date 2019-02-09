985f0c5ff367269bce0f2f4fa8eb3e79c3e9618b
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-09 10:25:43 -0800

    Built out the tests so far. Going to start adding in some server code to make sure they will begin passing.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
+/* Framework and Library requires */
 const express = require('express');
+
+/* Local file requires */
+const dbHelpers = require('../data/helpers');
 
 const server = express();
 
 server.use(express.json());
 
+server.get('/games', async (req, res) => {
 
+});
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,22 +1,66 @@
 const request = require('supertest');
 const server = require('./server');
+const db = require('../data/dbConfig');
+
+/* Clear the DB after each test */
+afterEach(async () => {
+    db.truncate();
+});
 
 /* Check this endpoint for...
 *  The correct status code.
 *  That it is sending back a JSON object.
 */
-
 describe('Checking the get endpoint for /games', () => {
     it('Sends back the correct status code.', async () => {
         const response = request(server).get('/games');
+        // Expect the response to be the 200 OK server status code.
         expect(response.status).toBe(200);
     });
     it('Sends back the correct data type.', async () => {
         const response = request(server).get('/games');
+        // This endpoint must send back a JSON object.
         expect(response.type).toMatch(/json/i);
+    });
+    it('Sends back a JSON object containing nothing.', async () => {
+        const response = request(server).get('/games');
+        // Since there should be nothing in the database we expect it to return nothing.
+        expect(response.body).toEqual({});
     });
 });
 
+/* Check this endpoint for...
+*  The correct status code.
+*  That it is sending back a JSON object.
+*  Sending a malformed object sends back a 422 server status code.
+*/
 describe('Checking the post endpoint for /games', () => {
-
+    it('Posts correctly when the correct object is provided.', async () => {
+        const body = {
+            title: 'Final Fantasy 4',
+            genre: 'JRPG',
+            releaseYear: 1991
+        }
+        const response = request(server).post('/games').send(body);
+        // Expect the response status to be the 201 created server status code.
+        expect(response.status).toBe(201);
+    });
+    it('Sends back a JSON object.', () => {
+        const body = {
+            title: 'Final Fantasy 4',
+            genre: 'JRPG',
+            releaseYear: 1991
+        }
+        const response = request(server).post('/games').send(body);
+        expect(response.type).toMatch(/json/i);
+    });
+    it('Sends the added games ID.', () => {
+        const body = {
+            title: 'Final Fantasy 4',
+            genre: 'JRPG',
+            releaseYear: 1991
+        }
+        const response = request(server).post('/games').send(body);
+        expect(response.body).toEqual({id: 0});
+    });
 });
