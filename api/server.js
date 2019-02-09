0545be01f59ff22fbe435e059c5e9c19a5577947
Kendra Williams <kmonique.williams@gmail.com>
2019-02-09 12:51:46 -0500

    get / route handler passing

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.get("/", async (req, res) => {
+   res.status(200).json({api: "up and running"});
+});
+
+module.exports = server;
