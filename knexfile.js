aff47bf0be9bb02e9ffc0a65686bab270b0bda16
iepoch <staclem@live.com>
2019-02-09 10:42:22 -0700

    Boiler plate setup

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/pacman.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/test/migrations',
+    },
+    seeds: {
+      directory: './data/test/seeds',
+    },
+  },
+
+
+};
