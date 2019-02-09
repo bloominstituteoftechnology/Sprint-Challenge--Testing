e055d87236e79ca58616afcc322496e0cbc58f2e
unknown <kyleteeter12@gmail.com>
2019-02-09 13:18:03 -0600

    Server tests are now passing

--- api/server.js
+++ api/server.js
@@ -1,30 +1,30 @@
-// const express = require('express');
+const express = require('express');
 
-// const games = require('../helper/helper.js');
+const games = require('../helper/helper.js');
 
-// const server = express();
+const server = express();
 
-// server.use(express.json());
+server.use(express.json());
 
-// server.get('/', async (req, res) => {
-//   res.status(200).json({ api: 'up' });
-// });
+server.get('/', async (req, res) => {
+  res.status(200).json({ api: 'up' });
+});
 
-// server.get('/games', async (req, res) => {
-//   const rows = await games.getAll();
+server.get('/games', async (req, res) => {
+  const rows = await games.getAll();
 
-//   res.status(200).json(rows);
-// });
+  res.status(200).json(rows);
+});
 
-// server.post('/games', async (req, res) => {
-//     const gameData = req.body;
-//    console.log(gameData)
-//     if (gameData.title && gameData.genre) {
-//       const ids = await games.insert(gameData);
-//       res.status(201).json(ids);
-//     } else {
-//       res.status(422).json({error: 'missing important information'})
-//     }
+server.post('/games', async (req, res) => {
+    const gameData = req.body;
+   console.log(gameData)
+    if (gameData.title && gameData.genre) {
+      const ids = await games.insert(gameData);
+      res.status(201).json(ids);
+    } else {
+      res.status(422).json({error: 'missing important information'})
+    }
 
-// })
-// module.exports = server;
+})
+module.exports = server;
