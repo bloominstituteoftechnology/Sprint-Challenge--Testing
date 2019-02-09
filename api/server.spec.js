a4c70eddf8e0a4d1d25b6ad239f138833211a821
Jason Hedrick <jasonleehedrick@gmail.com>
2019-02-09 10:01:03 -0800

    Added tests for the get endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,10 +1,22 @@
 const request = require('supertest');
 const server = require('./server');
 
-describe('Checking the get endpoint for /games', () => {
+/* Check this endpoint for...
+*  The correct status code.
+*  That it is sending back a JSON object.
+*/
 
+describe('Checking the get endpoint for /games', () => {
+    it('Sends back the correct status code.', async () => {
+        const response = request(server).get('/games');
+        expect(response.status).toBe(200);
+    });
+    it('Sends back the correct data type.', async () => {
+        const response = request(server).get('/games');
+        expect(response.type).toMatch(/json/i);
+    });
 });
 
 describe('Checking the post endpoint for /games', () => {
-    
+
 });
