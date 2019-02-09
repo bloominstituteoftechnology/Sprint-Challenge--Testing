1117fb942b5152044936b6d151627af35dbc623d
Julie Jonak <juliejonak10@gmail.com>
2019-02-09 12:40:56 -0600

    gamesModel helper functions added

--- games/gamesModel.js
+++ games/gamesModel.js
@@ -1,11 +1,27 @@
 const db = require('../data/dbConfig');
 require('events').EventEmitter.defaultMaxListeners = 0;
 
-//fetch (id)
+const fetch = (id) => {
+    if(id){
+        return db('games').where('id', id)
+            .then(games => {
+                return games[0]
+            })
+    }
+    return db('games')
+        .then(games => {
+            return games
+        })
+}
 
-//insert
+const insert = (game) => {
+    return db('games').insert(game)
+        .then( ([id]) => fetch(id))
+}
 
-//remove
+const remove = (id) => {
+    return db('games').where('id', id).del(id)
+}
 
 module.exports = {
     fetch, insert, remove 
