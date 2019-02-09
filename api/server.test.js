c5eebf1ee595f614a369f6e90957d9deee3bc473
Kendra Williams <kmonique.williams@gmail.com>
2019-02-09 12:48:29 -0500

    first rount of test completed

---
+++ api/server.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const server = require("./server.js");
+const db = require("../data/dbConfig.js")
+
+describe("the route handlers", () => {
+   describe("get /", () => {
+      it("responds with 200", async () => {
+         const response = await request(server).get("/");
+
+         expect(response.status).toBe(200);
+      });
+      it("responds with json", async () => {
+         const response = await request(server).get("/");
+
+         expect(response.type).toMatch(/json/i);
+      });
+      it("sends correct response", async () => {
+         const response = await request(server).get("/");
+
+         expect(response.body).toEqual({api: "up and running"});
+      });
+   });
+});
--- knexfile.js
+++ knexfile.js
@@ -15,4 +15,18 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
 };
