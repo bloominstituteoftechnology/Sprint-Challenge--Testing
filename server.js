ef5bc30528044bcfd197389ca67741255c0e2000
shannonhealy <sahealy525@gmail.com>
2019-02-09 15:50:04 -0500

    POST endpoint implementation

--- server.js
+++ server.js
@@ -5,10 +5,29 @@ server.use(express.json());
 
 module.exports = server;
 
-server.post("/games", (req, res) => {
+let gamesDB = [];
+let nextId = 1;
 
+server.post("/games", (req, res) => {
+    if (req.body && req.body.title && req.body.genre) {
+        let newGame = {
+            id: nextId,
+            title: req.body.title,
+            genre: req.body.genre,
+        }
+
+        if (req.body.releaseYear)
+            newGame.releaseYear = req.body.releaseYear;
+        
+        gamesDB.push(newGame);
+        nextId++;
+
+        res.status(201).json(gamesDB[nextId-2].id);
+    } else {
+        res.status(422).json({ error: "malformed game data" });
+    }
 });
 
 server.get("/games", (req, res) => {
-    
+
 });
