4225fb5cfcec310b1860e7445ed60d28df93624b
Joe Mercado <jryel1970@gmail.com>
2019-02-11 22:33:23 -0500

    additional commit

--- index.spec.js
+++ index.spec.js
@@ -1,92 +1,67 @@
 const request = require('supertest');
-const server = require('./api/server');
+const server = require('./api/server.js');
+/*
+GET /games
+The GET /games endpoint should return the list of games and HTTP
+ status code 200.
+Write a test to make sure this endpoint always returns an array,
+ even if there are no games stored. If there are no games to return, 
+ the endpoint should return an empty array.
+ */
 
-describe('server', () => {
-    describe('GET /games', () => {
-
-      it('should return status code 200(OK)', async () => {
-        const response = await request(server).get('/games');
-
-        expect(response.status).toBe(200);
-      });
-
-      it('should return JSON', async () => {
-        const response = await request(server).get('/games');
-
-        expect(response.type).toBe('application/json');
-      });
-
-      it('should return empty array if no games', async () => {
-        const response = await request(server).get('/games');
-
-        expect(response.body).toEqual([]);
-      })
-    });
-
-    describe('POST /games', () => {
-        it('should add game to array', async () => {
-            const game = { title: 'PUBG', genre: 'battle royale'};
-            
-            
-            const response = await request(server)
-            .post(`/games`)
-            .send({ game });
-
-            expect(response.body).toBeTruthy();
+describe('server.js', () => {
+    describe('/ route', () => {
+        it('should return status code 200', async () => {
+            let response = await request(server).get('/');
+            expect(response.status).toBe(200);
         });
-
-        it('should return status of 201 if okay', () => {
-            const game = { title: 'PUBG', genre: 'battle royale' };
-            
-            
-            return request(server)
-            .post(`/games`)
-            .send(game)
-            .then(res => {
-                expect(res.status).toEqual(201)
-            })
-
-            // expect(response.status).toBe(201);
+    
+        it('should return JSON', async () => {
+            let response = await request(server).get('/');
+            expect(response.type).toBe('application/json');
         });
-
-        it('should return error if missing data', async () => {
-            const game = { title: 'Red Dead'};
-            const response = await request(server)
-            .post(`/games`)
-            .send({ game });
-            expect(response.status).toBe(422);
+        it('should return with a body like: { api: "working" }', async () => {
+            let response = await request(server).get('/');
+            expect(response.body).toEqual({ api: 'working' });
         });
     });
-
-    describe('DELETE /games/:id', () => {
-        
-        it('should return 404 if no game at id', async () => {
-            const id = 70;
-            
-            
-            const response = await request(server)
-            .delete(`/games/${id}`)
-            .send({ id });
-
-            expect(response.status).toEqual(404);
-        })
-
-        it('should return { message: No records found to update }', async () => {
-            const id = 70;
-            
-            
-            const response = await request(server)
-            .delete(`/games/${id}`)
-            .send({ id });
-
-            expect(response.body).toEqual({ message: 'No records found to delete' });
-        })
-    })
-    it('can run tests', () => {
-        expect(true).toBeTruthy();
+    describe('POST /games route success', () => {
+        it('should return status code 201', async () => {
+            let response = await request(server)
+            .post('/games')
+            .send({title: 'Pacman', genre: 'Arcade'});
+            expect(response.status).toBe(201)
+        });  
     });
+    describe('POST /games route error', () => {
 
-    it('can run more tests', () => {
-        expect(false).toBeFalsy();
+        it('genre is required, error 422', async () => {
+            let response = await request(server)
+            .post('/games')
+            .send({title: 'Pacman'});
+            expect(response.status).toBe(422)
+        });
+
+        it('title is required, error 422', async () => {
+            let response = await request(server)
+            .post('/games')
+            .send({genre: 'Arcade'});
+            expect(response.status).toBe(422)
+        });
     });
-})
+    describe('GET/games route success', () => {
+        it('should return status code 200', async () => {
+            const response = await request(server).get('/games');
+            expect(response.status).toBe(200)
+        });
+
+        it('the get is receiving an array even if it is empty', async () => {
+            const response = await request(server).get('/games');
+            expect(response.body).toEqual(expect.arrayContaining([]))
+        });
+        it('should return JSON', async () => {
+            let response = await request(server).get('/games');
+            expect(response.type).toBe('application/json');
+        });
+    });
+});
