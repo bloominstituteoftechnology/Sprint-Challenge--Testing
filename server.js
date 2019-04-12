99cc2670ded8c6baa82123f520dd4e6206feb97f
ItelSunday <iteldomingotech@gmail.com>
2019-04-12 12:27:24 -0700

    added get and post route

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const server = express();
 
 server.use(express.json());
 
-const count = 3;
-const users = [
+let count = 3;
+const games = [
     {
         title:'Defense Of The Ancients - DOTA',
         genre: 'Action Real Time Strategy',
         id: 1
     },
     {
-        name:'Kingdom Hearts',
+        title:'Kingdom Hearts',
         genre: 'Action/RPG',
         id: 2
     },
     {
-        name:'Ragnarok: Eternal Love',
+        title:'Ragnarok: Eternal Love',
         genre: 'Massive Multiplayer Online Role Playing Game',
         id: 3
     },
@@ -26,5 +26,34 @@ const users = [
 server.get('/', (req, res) => {
     res.send('<h1>Sanity Check!</h1>');
 });
+
+server.get('/games', (req, res) => {
+    res.json(games);
+});
+
+server.get('/games/:id', (req, res) => {
+    const id = req.params.id;
+    const selectGame = games.find(game => game.id == id);
+
+    if (selectGame) {
+        res.status(200).json(games);
+    } else {
+        res.status(404).json({message: 'contents not found'});
+    }
+});
+
+server.post('/games', (req, res) => {
+const { title } = req.body;
+  const newGame = { title, id: count };
+  if (!title) {
+    res.status(401).json({ message: 'Insert title field' });
+  }
+
+  games.push(newGame);
+  count++;
+  res.status(201).json(games);
+});
+
+
 
 module.exports = server;
--- server.test.js
+++ server.test.js
@@ -1,11 +1,22 @@
 const request = require('supertest');
 const server = request('./server');
 
-describe('users', () => {
-  describe('', () => {
-    it('', () => {
-        return
-    })
-  })
+//  // test away!
+// it('runs the test',() =>{
+//     expect(true).toBe(true);
+// }); 
+
+describe('games', () => {
+    describe('get', () => {
+      it('runs server with 200', () => {
+          return request(server)
+          .get('/')
+          .then( res => expect(res.status).toBe(200))
+          .catch();
+      });
   
-});
+  });
+  
+  });
+
+
