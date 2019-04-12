392dc12d43b4e9d35e2e6ad2df042581ceb444e2
Anthony Venturini <anthonydventurini@gmail.com>
2019-04-12 09:04:07 -0700

    adds server configuration

--- api/server.js
+++ api/server.js
@@ -1,11 +1,39 @@
-const express = require('express')
+const express = require("express");
+const Games = require("./games");
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-server.get('/', (req, res)=> {
-    res.send(`<h1>Hello World</h1>`)
-})
+server.get("/", (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
 
-module.exports = server
+server.get("/games", async (req, res) => {
+  try {
+    const games = await Games.getAll();
+
+    res.status(200).json(games);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+server.post("/games", async (req, res) => {
+  const { title, genre } = req.body;
+
+  if (!title || !genre) {
+    return res.status(422).json({ message: "Please supply the correct information" });
+  }
+
+  try {
+    const games = await Games.insert(req.body);
+
+    res.status(200).json(games);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+});
+
+module.exports = server;
