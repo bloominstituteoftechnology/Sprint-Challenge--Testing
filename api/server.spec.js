0cfcb42e9eb40cd85945abf1017498e6882f1c56
Kseniya Platonava <ksu2008@gmail.com>
2019-04-19 22:21:32 -0400

    Add GET endpoint tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,53 @@
-const server = require('./api/server.js');
+const server = require('./server.js');
 
 const request = require('supertest');
 
 describe('testing module', () => {
 
+  describe('GET /games endpoint test', () => {
+    it('should return status code 200', async () => {
+      const res = await request(server).get('/games')
 
+       expect(res.status).toBe(200);
+    });
+
+    it('should return JSON', async () => {
+      const res = await request(server).get('/games');
+
+       expect(res.type).toBe('application/json');
+    });
+
+    it('should send back the correct type', async () => {
+      const res = await request(server).get('/games');
+
+       expect(typeof res.body).toBe('object');
+    })
+    it('should return the array', async () => {
+      const res = await request(server).get('/games');
+
+      const expected = [
+        {
+            title: 'Pacman',
+            genre: 'Arcade',
+            releaseYear: 1980
+        },
+        {
+            title: 'World of Warcraft',
+            genre: 'RPG',
+            releaseYear: 2004
+        },
+        {
+            title: 'Tropico 5',
+            genre: 'CMS',
+            releaseYear: 2014
+        }
+      ];
+
+      expect(res.body).toEqual(expected);
+      expect(Array.isArray(res.body)).toBe(true);
+    });
+
+  });
+
+  
 });
