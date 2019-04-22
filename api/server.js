3954a390b59b7a9e706b9c50bc54cb34501c6f57
kammagik <kammagik@yahoo.com>
2019-04-21 21:35:54 -0400

    GET request created

--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 
 const server = express();
 
+const videgamesDB = require('../data/helpers/gamesDb');
+
 server.use(express.json());
 
-server.get('/games', (req, res) => {
-    res.status(200).json({})
+server.get('/', async (req, res) => {
+    const videogames = await videogamesDB.get();
+
+    res.status(200).send(videogames);
 })
 
 module.exports = server;
