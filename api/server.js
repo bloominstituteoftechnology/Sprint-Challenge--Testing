c269829575431aa878904b216bf94f2dd4623332
kammagik <kammagik@yahoo.com>
2019-05-02 22:02:21 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -2,31 +2,26 @@ const express = require('express')
 
 const server = express();
 
-const videogamesDB = require('../data/helpers/gamesDb');
+const db = require('../data/helpers/gamesDb');
 
 server.use(express.json());
 
 server.get('/', async (req, res) => {
     try {
-        const videogames = await videogamesDB.get();
+        const videogames = await db.get();
         res.status(200).send(videogames);
     } catch(err) {
         res.status(500).send(err);
     }   
 });
 
-server.post('/videogame', async(req, res) => {
+server.post('/games', async (req, res) => {
     const videogame = req.body;
-
-    if(videogame.title && videogame.genre) {
-        try {
-            const posted = await videogamesDB.add(videogame);
-            res.status(201).json(posted);
-        } catch(err) {
-            res.status(500).send(err);
-        }
+    if (videogame.title && videogame.genre) {
+        const ids = await db.insert(videogame)
+        res.status(201).json(ids);
     } else {
-        res.status(401).json({message: 'New videogame needs a title and a body'});
+        res.status(422).json({})
     }
 });
 
--- api/server.test.js
+++ api/server.test.js
@@ -4,40 +4,55 @@ const server = require('./server');
 
 const db = require('../data/dbConfig');
 
-describe('The route handlers', () => {
-    describe('get /', () => {
-        it('responds with 200', async () => {
-            const response = await request(server).get('/');
-
+describe('the route handler', () => {
+    describe('get /games', () => {
+        it('respond with 200 when get is successful', async () => {
+            const response = await request(server)
+                .get('/');
             expect(response.status).toBe(200);
-        });
+        })
+        it('should return an empty array when db is empty', async () => {
+            const response = await request(server)
+                .get('/');
+            
+                expect(response.status).toBe(200);
+        })
+        it('respond with 200', async () => {
+            const response = await request(server)
+                .get('/');
+            expect(response.status).toBe(200);
+        })
+    })
+})
 
-        it('responds with an array', async () => {
-            const response = await request(server).get('/');
-            expect(typeof response.body).toBe('array');
-        });
-    });
 
-    describe('post /videogame', () => {
+
+describe('create new games entry', () => {
+    describe('post /games', () => {
         afterEach(async () => {
             await db('videogames').truncate();
-            await db.seed.run();
-        });
+        })
 
-        it('resonds with 201 if body is correct', async () => {
-            const body = {title: 'Shining Force', genre: 'Role-playing'}
-            const response = await request(server).post('/videogame').send(body);
+        it('respond with 422 when the genre is missing', async () => {
+            const body = { title: 'test'};
+            const response = await request(server)
+                .post('/games').send(body);
+            expect(response.status).toBe(422);
 
-            expect(response.status).toBe(201);
-            db('videogames').truncate();
-        });
+        })
+        it('respond with 422 when the title is missing', async () => {
+            const body = { genre: 'testGenre'};
+            const response = await request(server)
+                .post('/games').send(body);
+            expect(response.status).toBe(422);
 
-        it('responds with 401 when body is missing data', async () => {
-            const body = { }
-            const response = await request(server).post('/videogame').send(body);
+        })
+        it('respond with 422 when post is incomplete', async () => {
+            const body = {};
+            const response = await request(server)
+                .post('/games').send(body);
+            expect(response.status).toBe(422);
 
-            expect(response.status).toBe(401)
-            db('videogames').truncate();
-        });
-    });
-});
+        })
+    })
+})
