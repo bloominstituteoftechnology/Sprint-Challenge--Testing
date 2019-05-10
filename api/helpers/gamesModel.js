5ac40a826162825197633ca0e294cbcecdc36205
unknown <jerrardsmith215@gmail.com>
2019-05-10 00:00:08 -0400

    gamesModel for POST/GET created

---
+++ api/helpers/gamesModel.js
@@ -0,0 +1,24 @@
+// intialize knex & database
+const knex = require('knex');
+const db = require('../../dbConfig'); // intialize database
+
+/* Functions for database searching, to be exported. */
+
+// CREATE
+const add = (newGame) => {
+    return db('games')
+        .insert(newGame)
+        .into('games');
+}
+
+// READ
+const find = () => {
+    return db('games');
+}
+
+/* Module exports for use in routers */
+module.exports = {
+    // CRUD operation exports
+        add, // [x] logic for CREATE/post
+        find, // [x] logic for READ/get
+}
