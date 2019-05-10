8aedbad4cc0f1cdb8217fbaf9ca53a15787c3e4f
unknown <jerrardsmith215@gmail.com>
2019-05-09 22:49:17 -0400

    GET /games endpoint tests written

--- api/server.spec.js
+++ api/server.spec.js
@@ -12,17 +12,38 @@ describe('GET ("/") ', () => {
     });
 
 // Returns JSON data
-    it('verifies that JSON data is received', async () => {
+    it('returns JSON data', async () => {
         const res = await request(server).get('/');
         expect(res.type).toBe('application/json'); // or .toMatch(/json/i)
     });
 
 // Functional (returns a 'Hello world' object)
-    it('verifies functionality', async () => {
+    it('is functional', async () => {
         const res = await request(server).get('/');
-        expect(res).toEqual({ message: 'Hello World'});
+        expect(res.body).toEqual({ message: 'Hello World'});
     });
 });
 
 /* Games GET testing */
+
+describe('GET ("/games")', () => {
+    it('returns status code 200', async () => {
+        let res = await request(server).get('/games');
+        expect(res.status).toBe(200);
+    });
+
+    it('returns an array', async () => {
+        let res = await request(server).get('/games');
+        expect(Array.isArray(res.body)).toBeTruthy();
+    });
+
+    it('returns an object array', async () => {
+        let res = await request(server).get('/games');
+        for (i = 0; i < res.body.length; i++) {
+            expect(typeof res.body[i]).toEqual('object');
+        }
+    });
+});
+
 /* Games POST testing */
+describe('POST ("/games")', () =>{});
