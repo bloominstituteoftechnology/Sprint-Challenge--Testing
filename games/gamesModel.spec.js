8cb8f75ede6c311d5ebe5ccb14a51bb3d1fbdb25
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-21 14:20:10 -0700

    endpoints tested

--- games/gamesModel.spec.js
+++ games/gamesModel.spec.js
@@ -1,6 +1,6 @@
 const db = require('../data/dbConfig');
 
-const { insert } = require('./gamesModel');
+const { insert, get } = require('./gamesModel');
 
 describe('games model', () => {
     beforeEach(async () => {
@@ -10,6 +10,23 @@ describe('games model', () => {
     it('should set environment to development', () => {
         expect(process.env.DB_ENV).toBe('development');
     });
+
+    describe('get()', async () => {
+        it('should return all games', async () => {
+            await insert({ title: 'Street Fighter', genre: 'Action', releaseYear: '1997' })
+
+            const games = await get();
+
+            expect(games).toHaveLength(1)
+        })
+
+        it('should return empty array', async () => {
+            
+            const games = await db('games');
+
+            expect(games).toEqual([]);
+        })
+    })
 
     describe('insert()', () => {
         it('should insert the provided animal', async () => {
--- games/gamesRouter.spec.js
+++ games/gamesRouter.spec.js
@@ -13,16 +13,44 @@ describe('games router', () => {
             .get('/')
             .expect('Content-Type', /json/i)
         })
+
+        it('responds with 500 ERROR', () => {
+            supertest(router).get('/1').expect(500);
+        })
+
+        
     })
 
 
     describe('POST /', () => {
-        
+
         it('responds with 200 OK', () => {
-            supertest(router).post('/').expect(200)
+            supertest(router).post('/').expect(200);
         })
 
+        // it('responds with 200 success', async () => {
+        //     const data = {
+        //         title: 'Street Fighter', 
+        //         genre: 'Action', 
+        //         releaseYear: '1997'
+        //     }
 
+        //     const response = await supertest(router)
+        //     .post('/games')
+        //     .send(data)
+
+        //     expect(response.status).toEqual(200)
+        // })
+
+        it('responds with 200 OK', () => {
+            supertest(router)
+            .post('/')
+            .expect('Content-Type', /json/i)
+        })
+
+        it('responds with 500 ERROR', () => {
+            supertest(router).post('/1').expect(500)
+        })
     })
 
 })
