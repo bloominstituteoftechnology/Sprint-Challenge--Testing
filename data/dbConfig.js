90208a434eebc5475e6affd3eb5a7b6e59fcf6e0
Trip4077 <bj2336@email.vccs.edu>
2019-06-29 12:08:26 -0400

    Test Database and DB Config created

---
+++ data/dbConfig.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const knex = require('knex');
+const config = require('../knexfile');
+
+const dbEnv = process.env.DB_ENV || 'development';
+
+module.exports = knex(config[dbEnv]);
