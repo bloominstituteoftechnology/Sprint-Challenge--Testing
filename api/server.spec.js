d246343214967ee5b3302e14d71b250e9b1c414f
Cody Brewer <brewer.cody.m@gmail.com>
2019-06-30 23:33:36 -0600

    first test created & failing that's a success?

---
+++ api/server.spec.js
@@ -0,0 +1,11 @@
+const require('supertest');
+
+const server = require('./server');
+const db = require('../data/dbConfig.js');
+
+describe('server.js', () => {
+    it('should return a 200 on the basic route', async () => {
+        const res = await request(server).get('/');
+        expect(res.status).toBe(200);
+    })
+})
